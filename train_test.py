32149d90f2b9d0130fc70928b9c4125707d6b770
Ziyi Zhu <zhu.ziyi@outlook.com>
2022-07-04 23:16:12 +0100

    Add test trainer

---
+++ train_test.py
@@ -0,0 +1,151 @@
+import torch
+import wandb
+import time
+import os
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+
+from datasets import load_dataset
+from transformers import AutoTokenizer, pipeline
+
+from trl.gpt2 import GPT2HeadWithValueModel, respond_to_batch
+from trl.ppo import PPOTrainer
+from trl.core import build_bert_batch_from_txt, listify_batch
+
+config = {
+    "model_name": "gpt2",
+    "cls_model_name": "lvwerra/distilbert-imdb",
+    "steps": 20000,
+    "batch_size": 256,
+    "forward_batch_size": 16,
+    "ppo_epochs": 4,
+    "input_size": 1024,
+    "output_size": 64,
+    "lr": 1.41e-5,
+    "init_kl_coef": 0.2,
+    "target": 6,
+    "horizon": 10000,
+    "gamma": 1,
+    "lam": 0.95,
+    "cliprange": 0.2,
+    "cliprange_value": 0.2,
+    "vf_coef": 0.1,
+}
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+pipe_device = 0 if torch.cuda.is_available() else -1
+
+wandb.init(name="run-test", project="gpt2-test", config=config)
+
+ds = load_dataset(
+    "ChaiML/user_model_inputs",
+    split="train",
+    use_auth_token="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",
+)
+
+gpt2_model = GPT2HeadWithValueModel.from_pretrained(config["model_name"])
+gpt2_model_ref = GPT2HeadWithValueModel.from_pretrained(config["model_name"])
+
+gpt2_tokenizer = AutoTokenizer.from_pretrained(config["model_name"])
+gpt2_tokenizer.pad_token = gpt2_tokenizer.eos_token
+
+wandb.watch(gpt2_model, log="all")
+
+gpt2_model.to(device)
+gpt2_model_ref.to(device)
+
+
+def tokenize(sample):
+    sample["tokens"] = gpt2_tokenizer.encode(sample["text"])[-config["input_size"] :]
+    sample["query"] = gpt2_tokenizer.decode(sample["tokens"])
+    return sample
+
+
+ds = ds.map(tokenize, batched=False)
+
+gen_kwargs = {
+    "min_length": -1,
+    "top_k": 0.0,
+    "top_p": 1.0,
+    "do_sample": True,
+    "pad_token_id": gpt2_tokenizer.eos_token_id,
+}
+
+
+def collater(data):
+    return dict((key, [d[key] for d in data]) for key in data[0])
+
+
+dataloader = torch.utils.data.DataLoader(
+    ds, batch_size=config["batch_size"], collate_fn=collater
+)
+
+
+def calculate_reward(pipe_output):
+    output_dict = {}
+    for result_dict in pipe_output:
+        output_dict[result_dict["label"]] = result_dict["score"]
+    return logit(output_dict["POSITIVE"])
+
+
+def logit(p):
+    return np.log(p) - np.log(1 - p)
+
+
+ppo_trainer = PPOTrainer(gpt2_model, gpt2_model_ref, gpt2_tokenizer, **config)
+
+total_ppo_epochs = int(np.ceil(config["steps"] / config["batch_size"]))
+
+for epoch, batch in tqdm(zip(range(total_ppo_epochs), iter(dataloader))):
+    logs, timing = dict(), dict()
+    t0 = time.time()
+    query_tensors = [torch.tensor(t).long().to(device) for t in batch["tokens"]]
+
+    #### Get response from gpt2
+    t = time.time()
+    response_tensors = []
+    for i in range(config["batch_size"]):
+        gen_len = config["output_size"]
+        response = gpt2_model.generate(
+            query_tensors[i].unsqueeze(dim=0), max_new_tokens=gen_len, **gen_kwargs
+        )
+        response_tensors.append(response.squeeze()[-gen_len:])
+    batch["response"] = [gpt2_tokenizer.decode(r.squeeze()) for r in response_tensors]
+    timing["time/get_response"] = time.time() - t
+
+    #### Compute reward score
+    t = time.time()
+    rewards = torch.tensor(
+        [logit(r.count("a") / len(r)) for r in batch["response"]]
+    ).to(device)
+    timing["time/get_reward_preds"] = time.time() - t
+
+    #### Run PPO step
+    t = time.time()
+    stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
+    timing["time/optimization"] = time.time() - t
+
+    #### Log everything
+    timing["time/epoch"] = time.time() - t0
+    table_rows = [
+        list(r) for r in zip(batch["query"], batch["response"], rewards.cpu().tolist())
+    ]
+    logs.update(
+        {
+            "game_log": wandb.Table(
+                columns=["query", "response", "reward"], rows=table_rows
+            )
+        }
+    )
+    logs.update(timing)
+    logs.update(stats)
+    logs["env/reward_mean"] = torch.mean(rewards).cpu().numpy()
+    logs["env/reward_std"] = torch.std(rewards).cpu().numpy()
+    logs["env/reward_dist"] = rewards.cpu().numpy()
+
+    for key in logs:
+        if isinstance(logs[key], list):
+            if isinstance(logs[key][0], torch.Tensor):
+                logs[key] = [array.cpu().numpy() for array in logs[key]]
+    wandb.log(logs)
