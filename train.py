4e90ef4be761d1dd7932eb782ca017d7d9540fac
Ziyi Zhu <zhu.ziyi@outlook.com>
2022-07-06 10:20:05 +0100

    Add more logs

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForSequenceClassification 
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 from trl.gpt2 import GPT2HeadWithValueModel, respond_to_batch
 from trl.ppo import PPOTrainer
@@ -15,8 +15,8 @@ from trl.core import build_bert_batch_from_txt, listify_batch
 
 config = {
     "model_name": "gpt2",
-    "cls_model_name": "RobertIrv938/rewardModel60kEpoch3Balanced",
-    "cls_tokenizer_name": "roberta-large",
+    "cls_model_name": "ChaiML/rewardModel90kEpoch2K1M3",
+    "cls_tokenizer_name": "roberta-large-mnli",
     "auth_token": "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",
     "wandb_key": "f3c2ba6991e7af7c6225908adad8f098296d7433",
     "steps": 20000,
@@ -39,7 +39,7 @@ config = {
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 wandb.login(key=config["wandb_key"])
-wandb.init(name="run-balanced", project="gpt2-ppo", config=config)
+wandb.init(name="run-test", project="gpt2-ppo", config=config)
 
 ds = load_dataset(
     "ChaiML/user_model_inputs",
@@ -60,8 +60,7 @@ gpt2_model_ref.to(device)
 
 
 reward_model = AutoModelForSequenceClassification.from_pretrained(
-    config["cls_model_name"],
-    use_auth_token=config["auth_token"]
+    config["cls_model_name"], use_auth_token=config["auth_token"]
 ).to(device)
 
 reward_tokenizer = AutoTokenizer.from_pretrained(config["cls_tokenizer_name"])
@@ -71,6 +70,7 @@ def tokenize(sample):
     sample["tokens"] = gpt2_tokenizer.encode(sample["text"])[-config["input_size"] :]
     sample["query"] = gpt2_tokenizer.decode(sample["tokens"])
     return sample
+
 
 # ds = ds.filter(lambda x: np.random.uniform() < 0.1)
 ds = ds.map(tokenize, batched=False)
@@ -96,17 +96,85 @@ dataloader = torch.utils.data.DataLoader(
 
 def calculate_reward(query, response):
     if response == "<|endoftext|>" or not response.strip():
-        return torch.tensor(-4.0).to(device)
-    encoded_input = reward_tokenizer(query + response, return_tensors='pt').to(device)
+        return torch.tensor(2.0).to(device)
+    encoded_input = reward_tokenizer(query + response, return_tensors="pt").to(device)
     output = reward_model(**encoded_input)
     return output.logits[0, 1]
+
+
+def evaluate(eval_batch):
+    game_data = dict()
+    game_data["query"] = eval_batch["query"]
+    query_tensors = [torch.tensor(t).long().to(device) for t in eval_batch["tokens"]]
+
+    #### get response from gpt2 and gpt2_ref
+    response_tensors_ref, response_tensors = [], []
+    for i in range(len(query_tensors)):
+        query_len = len(query_tensors[i])
+        output_ref = gpt2_model_ref.generate(
+            query_tensors[i].unsqueeze(dim=0).to(device), **gen_kwargs
+        ).squeeze()
+        response_tensors_ref.append(clip_response(output_ref, query_len))
+        output = gpt2_model.generate(
+            query_tensors[i].unsqueeze(dim=0).to(device), **gen_kwargs
+        ).squeeze()
+        response_tensors.append(clip_response(output, query_len))
+
+    #### decode responses
+    game_data["response_ref"] = [gpt2_tokenizer.decode(r) for r in response_tensors_ref]
+    game_data["response"] = [gpt2_tokenizer.decode(r) for r in response_tensors]
+
+    #### sentiment analysis of query/response pairs before/after
+    game_data["rewards_ref"] = [
+        calculate_reward(q, r)
+        for q, r in zip(game_data["query"], game_data["response_ref"])
+    ]
+    game_data["rewards"] = [
+        calculate_reward(q, r)
+        for q, r in zip(game_data["query"], game_data["response"])
+    ]
+
+    # store results in a dataframe
+    df_results = pd.DataFrame(game_data)
+
+    logs = dict()
+    logs.update({"evaluation/comparison_table": wandb.Table(dataframe=df_results)})
+    logs.update(
+        {
+            "evaluation/mean_reward_ref": torch.mean(
+                torch.tensor(game_data["rewards_ref"])
+            )
+            .cpu()
+            .numpy()
+        }
+    )
+    logs.update(
+        {
+            "evaluation/mean_reward": torch.mean(torch.tensor(game_data["rewards"]))
+            .cpu()
+            .numpy()
+        }
+    )
+
+    return logs
+
+
+def clip_response(response, query_len):
+    response = response[query_len:]
+    stop_idx = (response == torch.tensor(198)).nonzero().flatten()
+    if len(stop_idx) > 0:
+        response = response[: stop_idx[0] + 1]
+    return response
 
 
 ppo_trainer = PPOTrainer(gpt2_model, gpt2_model_ref, gpt2_tokenizer, **config)
 
 total_ppo_epochs = int(np.ceil(config["steps"] / config["batch_size"]))
 
-for epoch, batch in tqdm(zip(range(total_ppo_epochs), iter(dataloader))):
+dataloader_iter = iter(dataloader)
+eval_batch = dataloader_iter.next()
+
+for epoch, batch in tqdm(zip(range(total_ppo_epochs), dataloader_iter)):
     logs, timing = dict(), dict()
     t0 = time.time()
     query_tensors = [torch.tensor(t).long().to(device) for t in batch["tokens"]]
@@ -117,22 +185,23 @@ for epoch, batch in tqdm(zip(range(total_ppo_epochs), iter(dataloader))):
     for i in range(len(query_tensors)):
         query_len = len(query_tensors[i])
         response = gpt2_model.generate(
-            query_tensors[i].unsqueeze(dim=0), max_length=query_len + config["output_size"], **gen_kwargs
-        ).squeeze()[query_len:]
-
-        stop_idx = (response == torch.tensor(198)).nonzero().flatten()
-        if len(stop_idx) > 0:
-            response = response[:stop_idx[0] + 1]
-        response_tensors.append(response)
+            query_tensors[i].unsqueeze(dim=0),
+            max_length=query_len + config["output_size"],
+            **gen_kwargs
+        ).squeeze()
+        response_tensors.append(clip_response(response, query_len))
 
     batch["response"] = [gpt2_tokenizer.decode(r) for r in response_tensors]
     timing["time/get_response"] = time.time() - t
 
     #### Compute reward score
     t = time.time()
-    rewards = torch.tensor([
-        calculate_reward(q, r) for q, r in zip(batch["reward_input"], batch["response"])
-    ]).to(device)
+    rewards = torch.tensor(
+        [
+            calculate_reward(q, r)
+            for q, r in zip(batch["reward_input"], batch["response"])
+        ]
+    ).to(device)
     timing["time/get_reward_preds"] = time.time() - t
 
     #### Run PPO step
@@ -162,4 +231,6 @@ for epoch, batch in tqdm(zip(range(total_ppo_epochs), iter(dataloader))):
         if isinstance(logs[key], list):
             if isinstance(logs[key][0], torch.Tensor):
                 logs[key] = [array.cpu().numpy() for array in logs[key]]
+
+    logs.update(evaluate(eval_batch))
     wandb.log(logs)
