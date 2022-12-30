d59e553567604560ea225f6032b635db7c46c4c9
younesbelkada <younesbelkada@gmail.com>
2022-12-30 10:53:50 +0000

    make style

--- trl/models/__init__.py
+++ trl/models/__init__.py
@@ -13,8 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .modeling_value_head import AutoModelForCausalLMWithValueHead
 from .modeling_base import PreTrainedModelWrapper, create_reference_model
+from .modeling_value_head import AutoModelForCausalLMWithValueHead
 
 
 SUPPORTED_ARCHITECTURES = (AutoModelForCausalLMWithValueHead,)
