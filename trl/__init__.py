48fcd0bd36bded83dbda16d3f09d7180cc5b474e
Leandro von Werra <lvwerra@users.noreply.github.com>
2022-12-30 09:42:30 +0100

    add create_reference_model (#61)
    * add create_reference_model
    
    * Update tests/test_modeling.py
    
    Co-authored-by: Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>
    
    * rename variable
    
    Co-authored-by: Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>

--- trl/__init__.py
+++ trl/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.1.1"
 
-from .models import AutoModelForCausalLMWithValueHead
+from .models import AutoModelForCausalLMWithValueHead, create_reference_model
 from .trainer import PPOTrainer, PPOConfig
