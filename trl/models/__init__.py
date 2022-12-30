bb488ffe9a964cebad7e4381d6e0c370d957f770
younesbelkada <younesbelkada@gmail.com>
2022-12-30 10:53:17 +0000

    fix init

--- trl/models/__init__.py
+++ trl/models/__init__.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from .modeling_value_head import AutoModelForCausalLMWithValueHead
+from .modeling_base import PreTrainedModelWrapper, create_reference_model
 
 
 SUPPORTED_ARCHITECTURES = (AutoModelForCausalLMWithValueHead,)
