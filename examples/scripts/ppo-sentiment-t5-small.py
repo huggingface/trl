008a81c29c15b360cb45d0189fd17e6ab6ae90b3
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-01-23 11:00:39 +0100

    Encoder-Decoder models support (#93)
    * v1
    
    * add example script
    
    * still not working
    
    * update trainer
    
    * update script
    
    * fix example and clean code
    
    * fix style
    
    * fix style
    
    * fix typo
    
    * add post_init method
    
    * add testing suite for seq2seq
    
    - refactor testing suite
    - push tiny models
    - add testing suite for `seq2seq` models
    
    * attempt to fix CI mem issue
    
    * attempt to fix CI mem issue
    
    * forward contrib credits from enc-dec PR
    
    * correct script
    
    * correct `fbs`
    
    * address comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Leandro von Werra <lvwerra@users.noreply.github.com>
    
    * Update examples/scripts/ppo-sentiment-t5-small.py
    
    Co-authored-by: Leandro von Werra <lvwerra@users.noreply.github.com>
    
    Co-authored-by: leandro <leandro.vonwerra@spoud.io>
    Co-authored-by: SSamDav <SSamDav@users.noreply.github.com>
    Co-authored-by: Leandro von Werra <lvwerra@users.noreply.github.com>

---
+++ examples/scripts/ppo-sentiment-t5-small.py
@@ -0,0 +1,135 @@
+# coding=utf-8
+# Copyright 2022 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import torch
+from tqdm import tqdm
+tqdm.pandas()
+from transformers import pipeline, AutoTokenizer
+from datasets import load_dataset
+
+from trl import PPOTrainer, PPOConfig, AutoModelForSeq2SeqLMWithValueHead
+from trl.core import LengthSampler
+
+########################################################################
+# This is a fully working simple example to use trl with accelerate.
+#
+# This example fine-tunes a T5 model on the IMDB dataset using PPO 
+# (proximal policy optimization).
+# in any of the following settings (with the same script):
+#   - single CPU or single GPU
+#   - multi GPUS (using PyTorch distributed mode)
+#   - multi GPUS (using DeepSpeed ZeRO-Offload stages 1 & 2)
+#   - fp16 (mixed-precision) or fp32 (normal precision)
+#
+# To run it in each of these various modes, first initialize the accelerate
+# configuration with `accelerate config` then run the script with 
+# `accelerate launch ppo-sentiment-t5-small.py`
+#
+########################################################################
+
+# We first define the configuration of the experiment, defining the model, the dataset,
+# the training parameters, and the PPO parameters.
+# Check the default arguments in the `PPOConfig` class for more details.
+config = PPOConfig(
+    model_name="lvwerra/t5-imdb",
+    learning_rate=5e-5,
+    batch_size=256,
+    forward_batch_size=1
+)
+# We then define the arguments to pass to the sentiment analysis pipeline.
+# We set `return_all_scores` to True to get the sentiment score for each token.
+sent_kwargs = {
+    "return_all_scores": True,
+    "function_to_apply": "none",
+    "batch_size": config.forward_batch_size
+}
+# Below is an example function to build the dataset. In our case, we use the IMDB dataset
+# from the `datasets` library. One should customize this function to train the model on
+# its own dataset.
+def build_imdb_dataset(tokenizer, input_min_text_length=2, input_max_text_length=8):
+    # load imdb with datasets
+    ds = load_dataset("imdb", split='train')
+    ds = ds.rename_columns({'text': 'review'})
+    ds = ds.filter(lambda x: len(x["review"])>200, batched=False)
+
+    input_size = LengthSampler(input_min_text_length, input_max_text_length)
+
+    def tokenize(sample):
+        sample["input_ids"] = tokenizer.encode(sample["review"])[:input_size()] + [tokenizer.eos_token_id]
+        sample["query"] = tokenizer.decode(sample["input_ids"])
+        return sample
+
+    ds = ds.map(tokenize, batched=False)
+    ds.set_format(type='torch')
+    return ds
+
+def collater(data):
+    return dict((key, [d[key] for d in data]) for key in data[0])
+
+# Now let's build the model, the reference model, and the tokenizer.
+model = AutoModelForSeq2SeqLMWithValueHead.from_pretrained(config.model_name)
+ref_model = AutoModelForSeq2SeqLMWithValueHead.from_pretrained(config.model_name)
+tokenizer = AutoTokenizer.from_pretrained(config.model_name)
+
+# We retrieve the dataloader by calling the `build_dataset` function.
+dataset = build_imdb_dataset(tokenizer)
+
+query = tokenizer("I really liked this movie because", return_tensors="pt")["input_ids"]
+
+generation_kwargs = {
+    "top_k": 0.0,
+    "top_p": 1.0,
+    "do_sample": True,
+    "eos_token_id": -1
+}
+
+
+# We then build the PPOTrainer, passing the model, the reference model, the tokenizer
+ppo_trainer = PPOTrainer(config, model, ref_model, tokenizer, dataset=dataset, data_collator=collater)
+
+# We then build the sentiment analysis pipeline, passing the model name and the
+# sentiment analysis pipeline arguments. Let's also make sure to set the device
+# to the same device as the PPOTrainer.
+device = ppo_trainer.accelerator.device
+if ppo_trainer.accelerator.num_processes == 1:
+   device = 0 if torch.cuda.is_available() else "cpu" # to avoid a `pipeline` bug
+sentiment_pipe = pipeline("sentiment-analysis", "lvwerra/distilbert-imdb", device=device)
+
+# We then define the arguments to pass to the `generate` function. These arguments
+# are passed to the `generate` function of the PPOTrainer, which is a wrapper around 
+# the `generate` function of the trained model.
+output_min_length = 16
+output_max_length = 32
+output_length_sampler = LengthSampler(output_min_length, output_max_length)
+
+for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
+    query_tensors = batch['input_ids']
+
+    #### Get response from gpt2
+    response_tensors = []
+    for query in query_tensors:
+        gen_len = output_length_sampler()
+        generation_kwargs["max_new_tokens"] = gen_len
+        response = ppo_trainer.generate(query, **generation_kwargs)
+        response_tensors.append(response.squeeze())
+    batch['response'] = [tokenizer.decode(r[1:].squeeze()) for r in response_tensors]
+
+    #### Compute sentiment score
+    texts = [q + r for q,r in zip(batch['query'], batch['response'])]
+    pipe_outputs = sentiment_pipe(texts, **sent_kwargs)
+    rewards = [torch.tensor(output[1]["score"]).to(device) for output in pipe_outputs]
+
+    #### Run PPO step 
+    stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
+    ppo_trainer.log_stats(stats, batch, rewards)
--- trl/__init__.py
+++ trl/__init__.py
@@ -2,5 +2,10 @@
 
 __version__ = "0.1.1"
 
-from .models import AutoModelForCausalLMWithValueHead, PreTrainedModelWrapper, create_reference_model
+from .models import (
+    AutoModelForCausalLMWithValueHead,
+    AutoModelForSeq2SeqLMWithValueHead,
+    PreTrainedModelWrapper,
+    create_reference_model,
+)
 from .trainer import PPOConfig, PPOTrainer
