156efa87ccc179e95cf20cf0b2d36e13b0d4fb57
younesbelkada <younesbelkada@gmail.com>
2023-01-24 18:04:13 +0000

    make style

--- trl/trainer/ppo_config.py
+++ trl/trainer/ppo_config.py
@@ -11,11 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import logging
 from dataclasses import dataclass
 from typing import Optional
 
 import numpy as np
-import logging
 
 from ..core import flatten_dict
 
