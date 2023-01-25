8976efb0f660b8e180abf484e0d0c84c3b34ba8f
Leandro von Werra <lvwerra@users.noreply.github.com>
2023-01-25 11:58:21 +0100

    restructure examples (#107)

--- examples/sentiment/scripts/gpt2-sentiment.py
+++ examples/sentiment/scripts/gpt2-sentiment.py
@@ -59,36 +59,7 @@ sent_kwargs = {
 # Below is an example function to build the dataset. In our case, we use the IMDB dataset
 # from the `datasets` library. One should customize this function to train the model on
 # its own dataset.
-def build_dataset(config, dataset_name="imdb", input_min_text_length=2, input_max_text_length=8):
-    """
-    Build dataset for training. This builds the dataset from `load_dataset`, one should 
-    customize this function to train the model on its own dataset.
-    
-    Args:
-        dataset_name (`str`): 
-            The name of the dataset to be loaded.
-    
-    Returns:
-        dataloader (`torch.utils.data.DataLoader`):
-            The dataloader for the dataset.
-    """
-    tokenizer = AutoTokenizer.from_pretrained(config.model_name)
-    tokenizer.pad_token = tokenizer.eos_token
-    # load imdb with datasets
-    ds = load_dataset(dataset_name, split='train')
-    ds = ds.rename_columns({'text': 'review'})
-    ds = ds.filter(lambda x: len(x["review"])>200, batched=False)
 
-    input_size = LengthSampler(input_min_text_length, input_max_text_length)
-
-    def tokenize(sample):
-        sample["input_ids"] = tokenizer.encode(sample["review"])[:input_size()]
-        sample["query"] = tokenizer.decode(sample["input_ids"])
-        return sample
-
-    ds = ds.map(tokenize, batched=False)
-    ds.set_format(type='torch')
-    return ds
 
 # We retrieve the dataloader by calling the `build_dataset` function.
 dataset = build_dataset(config)
