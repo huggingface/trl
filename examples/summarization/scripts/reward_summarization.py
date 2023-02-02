e60d9f89a7f2ca8e9bfb850f5adfa2240632891e
TristanThrush <tristan@huggingface.co>
2023-02-02 06:27:15 +0000

    formatted according to style

--- examples/summarization/scripts/reward_summarization.py
+++ examples/summarization/scripts/reward_summarization.py
@@ -1,5 +1,12 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, PreTrainedTokenizerBase, HfArgumentParser
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    TrainingArguments,
+    Trainer,
+    PreTrainedTokenizerBase,
+    HfArgumentParser,
+)
 from transformers.utils import PaddingStrategy
 from typing import Optional, Union, List, Dict, Any
 import evaluate
@@ -14,14 +21,32 @@ class ScriptArguments:
     """
 
     local_rank: Optional[int] = field(default=0, metadata={"help": "Used for multi-gpu"})
-    resume_from_checkpoint: Optional[bool] = field(default=False, metadata={"help": "If you want to resume training where it left off."})
-    deepspeed: Optional[str] = field(default=None, metadata={"help": "Path to deepspeed config if using deepspeed. You may need this if the model that you want to train doesn't fit on a single GPU."})
+    resume_from_checkpoint: Optional[bool] = field(
+        default=False, metadata={"help": "If you want to resume training where it left off."}
+    )
+    deepspeed: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "Path to deepspeed config if using deepspeed. You may need this if the model that you want to train doesn't fit on a single GPU."
+        },
+    )
     per_device_train_batch_size: Optional[int] = field(default=16)
     per_device_eval_batch_size: Optional[int] = field(default=16)
     gradient_accumulation_steps: Optional[int] = field(default=4)
     learning_rate: Optional[int] = field(default=2e-5)
-    model_name: Optional[str] = field(default="gpt2", metadata={"help": "The model that you want to train from the Hugging Face hub. E.g. gpt2, gpt2-xl, bert, etc."})
-    bf16: Optional[bool] = field(default=False, metadata={"help": "This essentially cuts the training time in half if you want to sacrifice a little precision and have a supported GPU."})
+    model_name: Optional[str] = field(
+        default="gpt2",
+        metadata={
+            "help": "The model that you want to train from the Hugging Face hub. E.g. gpt2, gpt2-xl, bert, etc."
+        },
+    )
+    bf16: Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": "This essentially cuts the training time in half if you want to sacrifice a little precision and have a supported GPU."
+        },
+    )
+
 
 parser = HfArgumentParser(ScriptArguments)
 script_args = parser.parse_args_into_dataclasses()[0]
@@ -61,14 +86,21 @@ def turn_into_text_classification_format(examples):
     new_examples = {"text_j": [], "text_k": []}
     for info, summaries, choice in zip(examples["info"], examples["summaries"], examples["choice"]):
         assert len(summaries) == 2
-        assert choice in (0,1)
+        assert choice in (0, 1)
         original_text_field = "post" if info["post"] is not None else "article"
-        new_examples["text_j"].append(summaries[choice]["text"] + " " + tokenizer.bos_token + " " + info[original_text_field])
-        new_examples["text_k"].append(summaries[0 if choice==1 else 1]["text"] + " " + tokenizer.bos_token + " " + info[original_text_field])
+        new_examples["text_j"].append(
+            summaries[choice]["text"] + " " + tokenizer.bos_token + " " + info[original_text_field]
+        )
+        new_examples["text_k"].append(
+            summaries[0 if choice == 1 else 1]["text"] + " " + tokenizer.bos_token + " " + info[original_text_field]
+        )
 
     return new_examples
 
-num_proc = 100 # Can adjust to be higher if you have more processors. Should work even if you don't have 100 CPUs, though.
+
+num_proc = (
+    100
+)  # Can adjust to be higher if you have more processors. Should work even if you don't have 100 CPUs, though.
 original_columns = ds["train"].column_names
 ds = ds.map(turn_into_text_classification_format, batched=True, num_proc=num_proc, remove_columns=original_columns)
 
@@ -76,7 +108,13 @@ ds = ds.map(turn_into_text_classification_format, batched=True, num_proc=num_pro
 def preprocess_function(examples):
     tokenized_j = tokenizer(examples["text_j"], truncation=True)
     tokenized_k = tokenizer(examples["text_k"], truncation=True)
-    return {"input_ids_j": tokenized_j["input_ids"], "attention_mask_j": tokenized_j["attention_mask"], "input_ids_k": tokenized_k["input_ids"], "attention_mask_k": tokenized_k["attention_mask"]}
+    return {
+        "input_ids_j": tokenized_j["input_ids"],
+        "attention_mask_j": tokenized_j["attention_mask"],
+        "input_ids_k": tokenized_k["input_ids"],
+        "attention_mask_k": tokenized_k["attention_mask"],
+    }
+
 
 tokenized_ds = ds.map(preprocess_function, batched=True, num_proc=num_proc, remove_columns=["text_j", "text_k"])
 
@@ -110,11 +148,20 @@ class RewardDataCollatorWithPadding:
             pad_to_multiple_of=self.pad_to_multiple_of,
             return_tensors=self.return_tensors,
         )
-        batch = {"input_ids_j": batch_j["input_ids"], "attention_mask_j": batch_j["attention_mask"], "input_ids_k": batch_k["input_ids"], "attention_mask_k": batch_k["attention_mask"], "return_loss": True}
+        batch = {
+            "input_ids_j": batch_j["input_ids"],
+            "attention_mask_j": batch_j["attention_mask"],
+            "input_ids_k": batch_k["input_ids"],
+            "attention_mask_k": batch_k["attention_mask"],
+            "return_loss": True,
+        }
         return batch
+
 
 # Define the metric that we'll use for validation.
 accuracy = evaluate.load("accuracy")
+
+
 def compute_metrics(eval_pred):
     predictions, _ = eval_pred
     # Here, predictions is rewards_j and rewards_k.
@@ -123,8 +170,9 @@ def compute_metrics(eval_pred):
     labels = np.zeros(predictions.shape)
     return accuracy.compute(predictions=predictions, references=labels)
 
+
 class RewardTrainer(Trainer):
-    # Define how to compute the reward loss. 
+    # Define how to compute the reward loss.
     def compute_loss(self, model, inputs, return_outputs=False):
         rewards_j = model(input_ids=inputs["input_ids_j"], attention_mask=inputs["attention_mask_j"])[0]
         rewards_k = model(input_ids=inputs["input_ids_k"], attention_mask=inputs["attention_mask_k"])[0]
@@ -133,6 +181,7 @@ class RewardTrainer(Trainer):
             return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
         return loss
 
+
 # Train the model, woohoo.
 trainer = RewardTrainer(
     model=model,
@@ -140,7 +189,7 @@ trainer = RewardTrainer(
     train_dataset=tokenized_ds["train"],
     eval_dataset=tokenized_ds["validation"],
     compute_metrics=compute_metrics,
-    data_collator=RewardDataCollatorWithPadding(tokenizer=tokenizer)
+    data_collator=RewardDataCollatorWithPadding(tokenizer=tokenizer),
 )
 
 trainer.train(script_args.resume_from_checkpoint)
