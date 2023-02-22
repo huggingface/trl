1ec326378f04fc9b6cb9188928802e8e431d92f1
Edward Beeching <edbeeching@gmail.com>
2023-02-22 13:59:06 +0100

    debugging issues with unstable training

--- trl/trainer/ppo_trainer.py
+++ trl/trainer/ppo_trainer.py
@@ -438,6 +438,7 @@ class PPOTrainer(BaseTrainer):
         timing["time/ppo/compute_rewards"] = time.time() - t
 
         self.model.train()
+        self.model.gradient_checkpointing_enable()
         t = time.time()
         all_stats = []
         idxs = list(range(bs))
