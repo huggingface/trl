ebdc4b409db50018bdd6aedca3cf4fdcdd9c4a63
gaurav.vi <gaurav.vi@media.net>
2023-03-23 15:37:12 +0530

    Reformatting the file using black(jupyter)

--- trl/trainer/ppo_config.py
+++ trl/trainer/ppo_config.py
@@ -24,33 +24,100 @@ from ..core import flatten_dict
 class PPOConfig(object):
     """
     Configuration class for PPOTrainer
-   """
+    """
 
-    model_name: Optional[str] = field(default=None, metadata={"help": "Name of model to use - used only for tracking purposes"})
-    steps: Optional[int] = field(default=20000, metadata={"help": "Number of training steps"})
-    learning_rate: Optional[float] = field(default=1e-5, metadata={"help": "Adam learning rate"})
-    adap_kl_ctrl: Optional[bool] = field(default=True, metadata={"help": "Use adaptive KL control, otherwise linear"})
-    init_kl_coef: Optional[float] = field(default=0.2, metadata={"help": "Initial KL penalty coefficient (used for adaptive and linear control)"})
-    target: Optional[float] = field(default=6, metadata={"help": "Target KL value for adaptive KL control"})
-    horizon: Optional[float] = field(default=10000, metadata={"help": "Horizon for adaptive KL control"})
-    gamma: Optional[float] = field(default=1, metadata={"help": "Gamma parameter for advantage calculation"})
-    lam: Optional[float] = field(default=0.95, metadata={"help": "Lambda parameter for advantage calculation"})
-    cliprange: Optional[float] = field(default=0.2, metadata={"help": "Range for clipping in PPO policy gradient loss"})
-    cliprange_value: Optional[float] = field(default=0.2, metadata={"help": "Range for clipping values in loss calculation"})
-    vf_coef: Optional[float] = field(default=0.1, metadata={"help": "Scaling factor for value loss"})
-    batch_size: Optional[int] = field(default=256, metadata={"help": "Number of samples per optimisation step"})
-    forward_batch_size: Optional[int] = field(default=None, metadata={"help": "Number of samples forward passed through model at a time"})
-    mini_batch_size: Optional[int] = field(default=1, metadata={"help": "Number of samples optimized inside PPO together"})
-    gradient_accumulation_steps: Optional[int] = field(default=1, metadata={"help": "The number of gradient accumulation steps"})
-    ppo_epochs: Optional[int] = field(default=4, metadata={"help": "Number of optimisation epochs per batch of samples"})
-    remove_unused_columns: Optional[bool] = field(default=True, metadata={"help": "Remove unused columns from the dataset if `datasets.Dataset` is used"})
-    log_with: Optional[str] = field(default=None, metadata={"help": "Log with either 'wandb' or 'tensorboard', check  https://huggingface.co/docs/accelerate/usage_guides/tracking for more details"})
-    tracker_kwargs: Optional[dict] = field(default_factory=dict, metadata={"help": "Keyword arguments for the accelerator (e.g. `logging_dir`)"})
-    accelerator_kwargs: Optional[dict] = field(default_factory=dict, metadata={"help": "Keyword arguments for the tracker (e.g. wandb_project)"})
-    tracker_project_name: Optional[str] = field(default="trl", metadata={"help": "Name of project to use for tracking"})
-    max_grad_norm: Optional[float] = field(default=None, metadata={"help": "Maximum gradient norm for gradient clipping"})
-    seed: Optional[int] = field(default=0, metadata={"help": "Seed value for random generations"})
-    optimize_cuda_cache: Optional[bool] = field(default=False, metadata={"help": "Optimize CUDA cache for slightly more memory-effcient training"})
+    model_name: Optional[str] = field(
+        default=None,
+        metadata={"help": "Name of model to use - used only for tracking purposes"},
+    )
+    steps: Optional[int] = field(
+        default=20000, metadata={"help": "Number of training steps"}
+    )
+    learning_rate: Optional[float] = field(
+        default=1e-5, metadata={"help": "Adam learning rate"}
+    )
+    adap_kl_ctrl: Optional[bool] = field(
+        default=True, metadata={"help": "Use adaptive KL control, otherwise linear"}
+    )
+    init_kl_coef: Optional[float] = field(
+        default=0.2,
+        metadata={
+            "help": "Initial KL penalty coefficient (used for adaptive and linear control)"
+        },
+    )
+    target: Optional[float] = field(
+        default=6, metadata={"help": "Target KL value for adaptive KL control"}
+    )
+    horizon: Optional[float] = field(
+        default=10000, metadata={"help": "Horizon for adaptive KL control"}
+    )
+    gamma: Optional[float] = field(
+        default=1, metadata={"help": "Gamma parameter for advantage calculation"}
+    )
+    lam: Optional[float] = field(
+        default=0.95, metadata={"help": "Lambda parameter for advantage calculation"}
+    )
+    cliprange: Optional[float] = field(
+        default=0.2, metadata={"help": "Range for clipping in PPO policy gradient loss"}
+    )
+    cliprange_value: Optional[float] = field(
+        default=0.2, metadata={"help": "Range for clipping values in loss calculation"}
+    )
+    vf_coef: Optional[float] = field(
+        default=0.1, metadata={"help": "Scaling factor for value loss"}
+    )
+    batch_size: Optional[int] = field(
+        default=256, metadata={"help": "Number of samples per optimisation step"}
+    )
+    forward_batch_size: Optional[int] = field(
+        default=None,
+        metadata={"help": "Number of samples forward passed through model at a time"},
+    )
+    mini_batch_size: Optional[int] = field(
+        default=1, metadata={"help": "Number of samples optimized inside PPO together"}
+    )
+    gradient_accumulation_steps: Optional[int] = field(
+        default=1, metadata={"help": "The number of gradient accumulation steps"}
+    )
+    ppo_epochs: Optional[int] = field(
+        default=4,
+        metadata={"help": "Number of optimisation epochs per batch of samples"},
+    )
+    remove_unused_columns: Optional[bool] = field(
+        default=True,
+        metadata={
+            "help": "Remove unused columns from the dataset if `datasets.Dataset` is used"
+        },
+    )
+    log_with: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "Log with either 'wandb' or 'tensorboard', check  https://huggingface.co/docs/accelerate/usage_guides/tracking for more details"
+        },
+    )
+    tracker_kwargs: Optional[dict] = field(
+        default_factory=dict,
+        metadata={"help": "Keyword arguments for the accelerator (e.g. `logging_dir`)"},
+    )
+    accelerator_kwargs: Optional[dict] = field(
+        default_factory=dict,
+        metadata={"help": "Keyword arguments for the tracker (e.g. wandb_project)"},
+    )
+    tracker_project_name: Optional[str] = field(
+        default="trl", metadata={"help": "Name of project to use for tracking"}
+    )
+    max_grad_norm: Optional[float] = field(
+        default=None, metadata={"help": "Maximum gradient norm for gradient clipping"}
+    )
+    seed: Optional[int] = field(
+        default=0, metadata={"help": "Seed value for random generations"}
+    )
+    optimize_cuda_cache: Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": "Optimize CUDA cache for slightly more memory-effcient training"
+        },
+    )
 
     def __post_init__(self):
         if self.forward_batch_size is not None:
