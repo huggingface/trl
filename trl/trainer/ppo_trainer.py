833828f06022a56207f06787f98a226c13aa4bb0
younesbelkada <younesbelkada@gmail.com>
2023-03-27 11:37:10 +0000

    wrap in a private method

--- trl/trainer/ppo_trainer.py
+++ trl/trainer/ppo_trainer.py
@@ -627,25 +627,7 @@ class PPOTrainer(BaseTrainer):
                 )
                 if self.config.early_stopping:
                     policykl = train_stats["policy/policykl"]
-
-                    if not self.is_distributed and policykl > 1.5 * self.config.target_kl:
-                        early_stop = True
-                        self.optimizer.zero_grad()
-                        break
-                    elif self.is_distributed:
-                        import torch.distributed as dist
-
-                        # Wait for all processes to finish
-                        dist.barrier()
-
-                        # all gather the policykl
-                        dist.all_reduce(policykl, dist.ReduceOp.SUM)
-                        policykl /= self.accelerator.num_processes
-
-                        if policykl > 1.5 * self.config.target_kl:
-                            early_stop = True
-                            self.optimizer.zero_grad()
-                            break
+                    early_stop = self._early_stop(policykl)
 
                 all_stats.append(train_stats)
 
@@ -690,6 +672,41 @@ class PPOTrainer(BaseTrainer):
             self.lr_scheduler.step()
 
         return stats
+
+    def _early_stop(self, policykl):
+        r"""
+        Handles the early stopping logic. If the policy KL is greater than the target KL, then the gradient is zeroed and
+        the optimization step is skipped.
+        This also handles the multi-gpu case where the policy KL is averaged across all processes.
+
+        Args:
+            policy_kl (torch.Tensor):
+                the policy KL
+
+        Returns:
+            `bool`: whether to early stop or not
+        """
+        early_stop = False
+        if not self.config.early_stopping:
+            return early_stop
+
+        if not self.is_distributed and policykl > 1.5 * self.config.target_kl:
+            self.optimizer.zero_grad()
+            early_stop = True
+        elif self.is_distributed:
+            import torch.distributed as dist
+
+            # Wait for all processes to finish
+            dist.barrier()
+
+            # all gather the policykl
+            dist.all_reduce(policykl, dist.ReduceOp.SUM)
+            policykl /= self.accelerator.num_processes
+
+            if policykl > 0 * self.config.target_kl:
+                self.optimizer.zero_grad()
+                early_stop = True
+        return early_stop
 
     def gather_stats(self, stats):
         """
