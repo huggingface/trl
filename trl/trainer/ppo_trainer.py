2387ae83c3703cd1edf6f9f6361ab2a985e6c136
Edward Beeching <edbeeching@gmail.com>
2023-04-04 11:49:17 +0200

    adds non-score reward clamping

--- trl/trainer/ppo_trainer.py
+++ trl/trainer/ppo_trainer.py
@@ -927,7 +927,7 @@ class PPOTrainer(BaseTrainer):
         for score, logprob, ref_logprob, mask in zip(scores, logprobs, ref_logprobs, masks):
             # compute KL penalty (from difference in logprobs)
             kl = logprob - ref_logprob
-            non_score_reward = -self.kl_ctl.value * kl
+            non_score_reward = torch.clamp(-self.kl_ctl.value * kl, -20.0, 0.0)
             non_score_rewards.append(non_score_reward)
             reward = non_score_reward.clone()
             last_non_masked_index = mask.nonzero()[-1]
