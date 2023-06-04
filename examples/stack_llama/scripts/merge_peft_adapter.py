c586a55152c6cda46f485e21880eb1cb243ae191
Michael Noukhovitch <mnoukhov@gmail.com>
2023-06-04 03:13:58 +0000

    style, black line length 119

--- examples/stack_llama/scripts/merge_peft_adapter.py
+++ examples/stack_llama/scripts/merge_peft_adapter.py
@@ -5,12 +5,7 @@ import peft
 import torch
 from peft import PeftConfig, PeftModel
 from peft.utils import _get_submodules
-from transformers import (
-    AutoModelForCausalLM,
-    AutoModelForSequenceClassification,
-    AutoTokenizer,
-    HfArgumentParser,
-)
+from transformers import AutoModelForCausalLM, AutoModelForSequenceClassification, AutoTokenizer, HfArgumentParser
 
 
 @dataclass
@@ -19,28 +14,16 @@ class ScriptArguments:
     The name of the Casual LM model we wish to fine with PPO
     """
 
-    adapter_model_name: Optional[str] = field(
-        default=None, metadata={"help": "the model name"}
-    )
-    base_model_name: Optional[str] = field(
-        default=None, metadata={"help": "the model name"}
-    )
-    output_name: Optional[str] = field(
-        default=None, metadata={"help": "the model name"}
-    )
+    adapter_model_name: Optional[str] = field(default=None, metadata={"help": "the model name"})
+    base_model_name: Optional[str] = field(default=None, metadata={"help": "the model name"})
+    output_name: Optional[str] = field(default=None, metadata={"help": "the model name"})
 
 
 parser = HfArgumentParser(ScriptArguments)
 script_args = parser.parse_args_into_dataclasses()[0]
-assert (
-    script_args.adapter_model_name is not None
-), "please provide the name of the Adapter you would like to merge"
-assert (
-    script_args.base_model_name is not None
-), "please provide the name of the Base model"
-assert (
-    script_args.base_model_name is not None
-), "please provide the output name of the merged model"
+assert script_args.adapter_model_name is not None, "please provide the name of the Adapter you would like to merge"
+assert script_args.base_model_name is not None, "please provide the name of the Base model"
+assert script_args.base_model_name is not None, "please provide the output name of the merged model"
 
 peft_config = PeftConfig.from_pretrained(script_args.adapter_model_name)
 if peft_config.task_type == "SEQ_CLS":
