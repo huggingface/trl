5fda3b0898f9edeeeabd36b89865a74159dd0ad6
Kashif Rasul <kashif.rasul@gmail.com>
2023-06-09 13:38:23 +0200

    just call the train instead of step

--- examples/dpo.py
+++ examples/dpo.py
@@ -3,7 +3,12 @@ from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional, Union
 
 import torch
-from transformers import GPT2Tokenizer, AutoModelForCausalLM, PreTrainedTokenizerBase
+from transformers import (
+    GPT2Tokenizer,
+    AutoModelForCausalLM,
+    PreTrainedTokenizerBase,
+    TrainingArguments,
+)
 from transformers.utils import PaddingStrategy
 from datasets import load_dataset
 
@@ -53,34 +58,25 @@ train_dataset = train_dataset.map(
     remove_columns=train_dataset.column_names,
 )
 
-# 2. initialize trainer
-dpo_config = {
-    "batch_size": 1,
-}
-# TODO
+# 2. initialize training arguments:
+training_args = TrainingArguments(
+    per_device_train_batch_size=2,
+    max_steps=3,
+    remove_unused_columns=False,
+    gradient_accumulation_steps=4,
+    learning_rate=1e-3,
+    evaluation_strategy="steps",
+)
+
+
+# 3. initialize the DPO trainer
 dpo_trainer = DPOTrainer(
     model,
     model_ref,
     beta=0.1,
+    training_args=training_args,
+    train_dataset=train_dataset,
 )
 
-# 3. encode a query
-query_txt = "This morning I went to the "
-query_tensor = tokenizer.encode(query_txt, return_tensors="pt").to(model.device)
-
-# 4. generate model response
-generation_kwargs = {
-    "min_length": -1,
-    "top_k": 0.0,
-    "top_p": 1.0,
-    "do_sample": True,
-    "pad_token_id": tokenizer.eos_token_id,
-    "max_new_tokens": 20,
-}
-response_tensor = dpo_trainer.generate(
-    [item for item in query_tensor], return_prompt=False, **generation_kwargs
-)
-response_txt = tokenizer.decode(response_tensor[0])
-
-# 5. train model with dpo which uses the reward implicitly defined by the model and model_ref
-train_stats = dpo_trainer.step([query_tensor[0]], [response_tensor[0]])
+# 4. train
+dpo_trainer.train()
