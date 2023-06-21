daf65e91203a274b4221789dfe98e959ea93ca23
TevenLeScao <teven.lescao@gmail.com>
2023-06-21 11:43:39 -0400

    arguments and style clean-up

--- examples/dpo.py
+++ examples/dpo.py
@@ -1,7 +1,7 @@
 # 0. imports
 import os
 from dataclasses import dataclass, field
-from typing import Any, Dict, List, Optional, Union
+from typing import Optional
 
 import torch
 from datasets import load_dataset
@@ -9,10 +9,8 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     HfArgumentParser,
-    PreTrainedTokenizerBase,
     TrainingArguments,
 )
-from transformers.utils import PaddingStrategy
 
 from trl import DPOTrainer
 
@@ -24,26 +22,33 @@ class ScriptArguments:
     The name of the Casual LM model we wish to fine with DPO
     """
 
-    model_name: Optional[str] = field(default="gpt2", metadata={"help": "the model name"})
-    log_with: Optional[str] = field(default=None, metadata={"help": "use 'wandb' to log with wandb"})
-    learning_rate: Optional[float] = field(default=1e-3, metadata={"help": "the learning rate"})
-    batch_size: Optional[int] = field(default=64, metadata={"help": "the PPO minibatch size"})
+    # training parameters
+    model_name_or_path: Optional[str] = field(default="gpt2", metadata={"help": "the model name"})
+    learning_rate: Optional[float] = field(default=1e-3, metadata={"help": "optimizer learning rate"})
+    per_device_train_batch_size: Optional[int] = field(default=64, metadata={"help": "batch size per device"})
     gradient_accumulation_steps: Optional[int] = field(
         default=1, metadata={"help": "the number of gradient accumulation steps"}
     )
-    max_length: Optional[int] = field(default=512, metadata={"help": "max length of each samples"})
+    max_length: Optional[int] = field(default=512, metadata={"help": "max length of each sample"})
     label_pad_token_id: Optional[int] = field(default=-100, metadata={"help": "label for non response tokens"})
-    broadcast_buffers: Optional[bool] = field(default=False, metadata={"help": "ddp broadcast buffer argument"})
-    ignore_bias_buffers: Optional[bool] = field(default=False, metadata={
-        "help": "fix for DDP issues with LM bias/mask buffers - invalid scalar type,`inplace operation. See"
-                "https://github.com/huggingface/transformers/issues/22482#issuecomment-1595790992"
-    })
-
-
-parser = HfArgumentParser(ScriptArguments)
-script_args = parser.parse_args_into_dataclasses()[0]
-
-SANITY = True
+    # instrumentation
+    sanity_check: Optional[bool] = field(default=False, metadata={"help": "only train on 1000 samples"})
+    report_to: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": 'The list of integrations to report the results and logs to. Supported platforms are `"azure_ml"`,'
+            '`"comet_ml"`, `"mlflow"`, `"neptune"`, `"tensorboard"`,`"clearml"` and `"wandb"`. '
+            'Use `"all"` to report to all integrations installed, `"none"` for no integrations.'
+        },
+    )
+    # debug argument for distributed training
+    ignore_bias_buffers: Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": "fix for DDP issues with LM bias/mask buffers - invalid scalar type,`inplace operation. See"
+            "https://github.com/huggingface/transformers/issues/22482#issuecomment-1595790992"
+        },
+    )
 
 
 # Turn the dataset into pairs of post + summaries, where text_j is the preferred question + answer and text_k is the other.
@@ -82,50 +87,55 @@ def preprocess_function(examples, tokenizer, max_length, label_pad_token_id):
     return new_examples
 
 
-# 1. load a pretrained model
-model = AutoModelForCausalLM.from_pretrained(script_args.model_name)
+if __name__ == "__main__":
+    parser = HfArgumentParser(ScriptArguments)
+    script_args = parser.parse_args_into_dataclasses()[0]
 
-if script_args.ignore_bias_buffers:
-    # torch distributed hack
-    model._ddp_params_and_buffers_to_ignore = [name for name, buffer in model.named_buffers() if buffer.dtype == torch.bool]
+    # 1. load a pretrained model
+    model = AutoModelForCausalLM.from_pretrained(script_args.model_name_or_path)
 
-model_ref = AutoModelForCausalLM.from_pretrained(script_args.model_name)
-tokenizer = AutoTokenizer.from_pretrained(script_args.model_name)
-tokenizer.pad_token = tokenizer.eos_token
+    if script_args.ignore_bias_buffers:
+        # torch distributed hack
+        model._ddp_params_and_buffers_to_ignore = [
+            name for name, buffer in model.named_buffers() if buffer.dtype == torch.bool
+        ]
 
-# 2. Load the human stack-exchange paired dataset
-train_dataset = load_dataset("lvwerra/stack-exchange-paired", data_dir="data/reward", split="train")
+    model_ref = AutoModelForCausalLM.from_pretrained(script_args.model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(script_args.model_name_or_path)
+    tokenizer.pad_token = tokenizer.eos_token
 
-if SANITY:
-    train_dataset = train_dataset.select(range(1000))
+    # 2. Load the human stack-exchange paired dataset
+    train_dataset = load_dataset("lvwerra/stack-exchange-paired", data_dir="data/reward", split="train")
 
-train_dataset = train_dataset.map(
-    preprocess_function,
-    batched=True,
-    remove_columns=train_dataset.column_names,
-    num_proc=os.cpu_count(),
-    fn_kwargs=dict(
-        tokenizer=tokenizer, max_length=script_args.max_length, label_pad_token_id=script_args.label_pad_token_id
-    ),
-)
+    if script_args.sanity_check:
+        train_dataset = train_dataset.select(range(min(len(train_dataset), 1000)))
 
-# 2. initialize training arguments:
-training_args = TrainingArguments(
-    per_device_train_batch_size=script_args.batch_size,
-    max_steps=10,
-    remove_unused_columns=False,
-    gradient_accumulation_steps=script_args.gradient_accumulation_steps,
-    learning_rate=script_args.learning_rate,
-    evaluation_strategy="steps",
-    output_dir="./test",
-    report_to=script_args.log_with,
-    ddp_broadcast_buffers=script_args.broadcast_buffers
-)
+    train_dataset = train_dataset.map(
+        preprocess_function,
+        batched=True,
+        remove_columns=train_dataset.column_names,
+        num_proc=os.cpu_count(),
+        fn_kwargs=dict(
+            tokenizer=tokenizer, max_length=script_args.max_length, label_pad_token_id=script_args.label_pad_token_id
+        ),
+    )
 
-# 3. initialize the DPO trainer
-dpo_trainer = DPOTrainer(
-    model, model_ref, args=training_args, beta=0.1, train_dataset=train_dataset, tokenizer=tokenizer
-)
+    # 2. initialize training arguments:
+    training_args = TrainingArguments(
+        per_device_train_batch_size=script_args.per_device_train_batch_size,
+        max_steps=10,
+        remove_unused_columns=False,
+        gradient_accumulation_steps=script_args.gradient_accumulation_steps,
+        learning_rate=script_args.learning_rate,
+        evaluation_strategy="steps",
+        output_dir="./test",
+        report_to=script_args.report_to,
+    )
 
-# 4. train
-dpo_trainer.train()
+    # 3. initialize the DPO trainer
+    dpo_trainer = DPOTrainer(
+        model, model_ref, args=training_args, beta=0.1, train_dataset=train_dataset, tokenizer=tokenizer
+    )
+
+    # 4. train
+    dpo_trainer.train()
