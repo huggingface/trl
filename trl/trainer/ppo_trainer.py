7af3239d09f4160789ae74b0c4cde94e5b3039fd
edbeeching <edbeeching@gmail.com>
2023-07-03 13:50:23 +0200

    ppo_config -> config

--- trl/trainer/ppo_trainer.py
+++ trl/trainer/ppo_trainer.py
@@ -983,13 +983,13 @@ class PPOTrainer(BaseTrainer):
         return torch.stack(rewards), torch.stack(non_score_rewards)
 
     def _kl_penalty(self, logprob: torch.FloatTensor, ref_logprob: torch.FloatTensor) -> torch.FloatTensor:
-        if self.ppo_config.kl_penalty == "kl":
+        if self.config.kl_penalty == "kl":
             return logprob - ref_logprob
 
-        if self.ppo_config.kl_penalty == "abs":
+        if self.config.kl_penalty == "abs":
             return (logprob - ref_logprob).abs()
 
-        if self.ppo_config.kl_penalty == "mse":
+        if self.config.kl_penalty == "mse":
             return 0.5 * (logprob - ref_logprob).square()
 
         raise NotImplementedError
