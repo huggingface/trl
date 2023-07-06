5839babe3455eb3ea8472beeb3e42634d3d0da33
Kashif Rasul <kashif.rasul@gmail.com>
2023-07-06 09:59:22 +0200

    added dpo authors

--- trl/trainer/dpo_trainer.py
+++ trl/trainer/dpo_trainer.py
@@ -1,3 +1,4 @@
+# DPO Authors: Rafael Rafailov, Archit Sharma, Eric Mitchell, Stefano Ermon, Christopher D. Manning, and Chelsea Finn 2023
 # Copyright 2023 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
