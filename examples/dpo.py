2c75a042781854261d46104150cbbadf8cc17a0b
Kashif Rasul <kashif.rasul@gmail.com>
2023-07-19 13:26:59 +0200

    removed unused import

--- examples/dpo.py
+++ examples/dpo.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass, field
 from typing import Dict, Optional
 
 import torch
-import tqdm
 from datasets import Dataset, load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer, HfArgumentParser, TrainingArguments
 
