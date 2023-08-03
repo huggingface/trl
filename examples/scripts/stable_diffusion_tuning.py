92412c3e887a53557f755cce6cfb5147fbd8f1bd
metric-space <functor.soup@gmail.com>
2023-08-03 00:50:51 -0400

    Add recent run params

--- examples/scripts/stable_diffusion_tuning.py
+++ examples/scripts/stable_diffusion_tuning.py
@@ -127,9 +127,12 @@ if __name__ == "__main__":
     config = DDPOConfig(
         num_epochs=200,
         train_gradient_accumulation_steps=1,
-        sample_num_batches_per_epoch=2,
-        sample_num_steps=30,
-        sample_batch_size=1,
+        sample_num_steps=50,
+        sample_batch_size=6,
+        train_batch_size=3,
+        sample_num_batches_per_epoch=4,
+        per_prompt_stat_tracking=True,
+        per_prompt_stat_tracking_buffer_size=32,
         tracker_project_name="stable_diffusion_training",
         log_with="wandb",
         project_kwargs={
