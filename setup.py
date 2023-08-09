25fd6f2313b36ae3648073a659ef0b229f560f7b
Leandro von Werra <lvwerra@users.noreply.github.com>
2023-08-09 17:48:25 +0200

    Move repo (#628)
    * update actions
    
    * update references

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
-    url="https://github.com/lvwerra/trl",
+    url="https://github.com/huggingface/trl",
     packages=find_packages(),
     include_package_data=True,
     install_requires=REQUIRED_PKGS,
