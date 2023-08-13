747e64e15894cc2e450ca654ca0ee86bb755161a
metric-space <functor.soup@gmail.com>
2023-08-13 12:07:14 -0400

    Renamed modelling_sd_base to modeling_sd_base

--- trl/models/__init__.py
+++ trl/models/__init__.py
@@ -26,7 +26,7 @@ from ..import_utils import is_diffusers_available
 
 
 if is_diffusers_available():
-    from .modelling_sd_base import (
+    from .modeling_sd_base import (
         DDPOPipelineOutput,
         DDPOSchedulerOutput,
         DDPOStableDiffusionPipeline,
