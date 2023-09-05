ac9c06087d022d2fac8d02720581b0b74a7b0c42
lewtun <lewis.c.tunstall@gmail.com>
2023-09-05 09:05:42 +0200

    Refactor RewardTrainer hyperparameters into dedicated dataclass (#726)
    * Refactor RewardTrainer hyperparameters into dedicated dataclass
    
    * Revert
    
    * Add doc string
    
    * Fix warning
    
    * Handle backwards compat
    
    * Fix tests
    
    * Add docs
    
    * Refactor to RewardConfig
    
    * Fix case conditions
    
    * Fix

--- trl/trainer/__init__.py
+++ trl/trainer/__init__.py
@@ -40,3 +40,4 @@ from .ppo_config import PPOConfig
 from .ppo_trainer import PPOTrainer
 from .reward_trainer import RewardTrainer, compute_accuracy
 from .sft_trainer import SFTTrainer
+from .training_configs import RewardConfig
