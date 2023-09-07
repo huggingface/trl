4f81e7736d66b5c4bf542c6a98b1b9b04f7ebe55
Gaetan LOPEZ LATOUCHE <66413927+gaetanlop@users.noreply.github.com>
2023-09-07 12:03:10 -0400

    Seq2Seq model support for DPO (#586)
    * dpo_collator for seq2seq models
    
    * dpo trainer support
    
    * refactoring
    
    * update collator
    
    * computes decoder input ids if possible
    
    * decoder input ids for dpo trainer
    
    * added test for seq2seq
    
    * quality
    
    * fixed typo
    
    * fixed string padding for seq2seq
    
    * fixed minor issues in padding
    
    * fixed typo in dpo.py
    
    * add docstring
    
    * run all precommit
    
    * fixed gradient accumulation steps in test
    
    * reformatting
    
    * fixing dpo tests
    
    * update .mdx

--- trl/trainer/utils.py
+++ trl/trainer/utils.py
@@ -22,7 +22,7 @@ import numpy as np
 import torch
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import IterableDataset
-from transformers import DataCollatorForLanguageModeling, PreTrainedTokenizerBase, TrainerCallback
+from transformers import DataCollatorForLanguageModeling, PreTrainedModel, PreTrainedTokenizerBase, TrainerCallback
 
 
 class AdaptiveKLController:
@@ -250,6 +250,9 @@ class DPODataCollatorWithPadding:
     Args:
         tokenizer (`PreTrainedTokenizerBase`):
             The tokenizer used for encoding the data.
+        model (Optional[`PreTrainedModel`]):
+            The model that is being trained. If set and has the *prepare_decoder_input_ids_from_labels*, use it to
+            prepare the *decoder_input_ids*.
         padding (`Union[bool, str, `PaddingStrategy`]`, `optional`, defaults to `True`):
             padding_strategy to pass to the tokenizer.
         max_length (`Optional[int]`, `optional`, defaults to `None`):
@@ -260,16 +263,23 @@ class DPODataCollatorWithPadding:
             The label used for masking.
         padding_value (`int`, defaults to 0):
             The value used for padding.
+        is_encoder_decoder (`Optional[bool]`, `optional`, defaults to `None`):
+            Whether or not you model has an encoder_decoder architecture.
+        max_target_length (`Optional[int]`, `optional`, defaults to `None`):
+            The maximum length of the target to be processed. Only useful for encoder-decoder architectures.
         truncation_mode: (`str`, defaults to "keep_end"):
             The truncation mode to use when truncating the prompt.
     """
     tokenizer: PreTrainedTokenizerBase
+    model: Optional[PreTrainedModel] = None
     padding: Union[bool, str] = True
     max_length: Optional[int] = None
     max_prompt_length: Optional[int] = None
     label_pad_token_id: int = -100
     padding_value: int = 0
     truncation_mode: str = "keep_end"
+    is_encoder_decoder: Optional[bool] = False
+    max_target_length: Optional[int] = None
 
     def tokenize_batch_element(
         self,
@@ -287,69 +297,99 @@ class DPODataCollatorWithPadding:
             the sum of the length of the prompt and the chosen/rejected response, with
             label_pad_token_id  for the prompt tokens.
         """
-        chosen_tokens = self.tokenizer(chosen, add_special_tokens=False)
-        rejected_tokens = self.tokenizer(rejected, add_special_tokens=False)
-        prompt_tokens = self.tokenizer(prompt, add_special_tokens=False)
-
-        assert self.tokenizer.eos_token_id not in prompt_tokens["input_ids"], f"Prompt contains EOS token: {prompt}"
-        assert (
-            self.tokenizer.eos_token_id not in chosen_tokens["input_ids"]
-        ), f"Chosen response contains EOS token: {chosen}"
-        assert (
-            self.tokenizer.eos_token_id not in rejected_tokens["input_ids"]
-        ), f"Rejected response contains EOS token: {rejected}"
-
-        chosen_tokens["input_ids"].append(self.tokenizer.eos_token_id)
-        chosen_tokens["attention_mask"].append(1)
-
-        rejected_tokens["input_ids"].append(self.tokenizer.eos_token_id)
-        rejected_tokens["attention_mask"].append(1)
-
-        longer_response_length = max(len(chosen_tokens["input_ids"]), len(rejected_tokens["input_ids"]))
-
-        # if combined sequence is too long, truncate the prompt
-        if len(prompt_tokens["input_ids"]) + longer_response_length > self.max_length:
-            if self.truncation_mode == "keep_start":
-                prompt_tokens = {k: v[: self.max_prompt_length] for k, v in prompt_tokens.items()}
-            elif self.truncation_mode == "keep_end":
-                prompt_tokens = {k: v[-self.max_prompt_length :] for k, v in prompt_tokens.items()}
-            else:
-                raise ValueError(f"Unknown truncation mode: {self.truncation_mode}")
-
-        # if that's still too long, truncate the response
-        if len(prompt_tokens["input_ids"]) + longer_response_length > self.max_length:
-            chosen_tokens = {k: v[: self.max_length - self.max_prompt_length] for k, v in chosen_tokens.items()}
-            rejected_tokens = {k: v[: self.max_length - self.max_prompt_length] for k, v in rejected_tokens.items()}
-
-        # Create labels
-        chosen_sequence_tokens = {k: prompt_tokens[k] + chosen_tokens[k] for k in chosen_tokens}
-        rejected_sequence_tokens = {k: prompt_tokens[k] + rejected_tokens[k] for k in rejected_tokens}
-        chosen_sequence_tokens["labels"] = chosen_sequence_tokens["input_ids"][:]
-        chosen_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [self.label_pad_token_id] * len(
-            prompt_tokens["input_ids"]
-        )
-        rejected_sequence_tokens["labels"] = rejected_sequence_tokens["input_ids"][:]
-        rejected_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [self.label_pad_token_id] * len(
-            prompt_tokens["input_ids"]
-        )
 
         batch = {}
+
+        if not self.is_encoder_decoder:
+            chosen_tokens = self.tokenizer(chosen, add_special_tokens=False)
+            rejected_tokens = self.tokenizer(rejected, add_special_tokens=False)
+            prompt_tokens = self.tokenizer(prompt, add_special_tokens=False)
+
+            assert (
+                self.tokenizer.eos_token_id not in prompt_tokens["input_ids"]
+            ), f"Prompt contains EOS token: {prompt}"
+            assert (
+                self.tokenizer.eos_token_id not in chosen_tokens["input_ids"]
+            ), f"Chosen response contains EOS token: {chosen}"
+            assert (
+                self.tokenizer.eos_token_id not in rejected_tokens["input_ids"]
+            ), f"Rejected response contains EOS token: {rejected}"
+
+            chosen_tokens["input_ids"].append(self.tokenizer.eos_token_id)
+            chosen_tokens["attention_mask"].append(1)
+
+            rejected_tokens["input_ids"].append(self.tokenizer.eos_token_id)
+            rejected_tokens["attention_mask"].append(1)
+
+            longer_response_length = max(len(chosen_tokens["input_ids"]), len(rejected_tokens["input_ids"]))
+
+            # if combined sequence is too long, truncate the prompt
+            if len(prompt_tokens["input_ids"]) + longer_response_length > self.max_length:
+                if self.truncation_mode == "keep_start":
+                    prompt_tokens = {k: v[: self.max_prompt_length] for k, v in prompt_tokens.items()}
+                elif self.truncation_mode == "keep_end":
+                    prompt_tokens = {k: v[-self.max_prompt_length :] for k, v in prompt_tokens.items()}
+                else:
+                    raise ValueError(f"Unknown truncation mode: {self.truncation_mode}")
+
+            # if that's still too long, truncate the response
+            if len(prompt_tokens["input_ids"]) + longer_response_length > self.max_length:
+                chosen_tokens = {k: v[: self.max_length - self.max_prompt_length] for k, v in chosen_tokens.items()}
+                rejected_tokens = {
+                    k: v[: self.max_length - self.max_prompt_length] for k, v in rejected_tokens.items()
+                }
+
+            # Create labels
+            chosen_sequence_tokens = {k: prompt_tokens[k] + chosen_tokens[k] for k in chosen_tokens}
+            rejected_sequence_tokens = {k: prompt_tokens[k] + rejected_tokens[k] for k in rejected_tokens}
+            chosen_sequence_tokens["labels"] = chosen_sequence_tokens["input_ids"][:]
+            chosen_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [self.label_pad_token_id] * len(
+                prompt_tokens["input_ids"]
+            )
+            rejected_sequence_tokens["labels"] = rejected_sequence_tokens["input_ids"][:]
+            rejected_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [self.label_pad_token_id] * len(
+                prompt_tokens["input_ids"]
+            )
+
+            for k, toks in {
+                "chosen": chosen_sequence_tokens,
+                "rejected": rejected_sequence_tokens,
+                "prompt": prompt_tokens,
+            }.items():
+                for type_key, tokens in toks.items():
+                    if type_key == "token_type_ids":
+                        continue
+                    batch[f"{k}_{type_key}"] = tokens
+
+        else:
+            chosen_tokens = self.tokenizer(
+                chosen, truncation=True, max_length=self.max_target_length, add_special_tokens=True
+            )
+            rejected_tokens = self.tokenizer(
+                rejected, truncation=True, max_length=self.max_target_length, add_special_tokens=True
+            )
+            prompt_tokens = self.tokenizer(
+                prompt, truncation=True, max_length=self.max_prompt_length, add_special_tokens=True
+            )
+
+            batch["chosen_labels"] = chosen_tokens["input_ids"]
+            batch["rejected_labels"] = rejected_tokens["input_ids"]
+            batch["prompt_input_ids"] = prompt_tokens["input_ids"]
+            batch["prompt_attention_mask"] = prompt_tokens["attention_mask"]
+
+            if self.model is not None and hasattr(self.model, "prepare_decoder_input_ids_from_labels"):
+                batch["rejected_decoder_input_ids"] = self.model.prepare_decoder_input_ids_from_labels(
+                    labels=batch["rejected_labels"]
+                )
+                batch["chosen_decoder_input_ids"] = self.model.prepare_decoder_input_ids_from_labels(
+                    labels=batch["chosen_labels"]
+                )
 
         batch["prompt"] = prompt
         batch["chosen"] = prompt + chosen
         batch["rejected"] = prompt + rejected
         batch["chosen_response_only"] = chosen
         batch["rejected_response_only"] = rejected
-
-        for k, toks in {
-            "chosen": chosen_sequence_tokens,
-            "rejected": rejected_sequence_tokens,
-            "prompt": prompt_tokens,
-        }.items():
-            for type_key, tokens in toks.items():
-                if type_key == "token_type_ids":
-                    continue
-                batch[f"{k}_{type_key}"] = tokens
 
         return batch
 
@@ -358,24 +398,37 @@ class DPODataCollatorWithPadding:
         padded_batch = {}
         for k in batch[0].keys():
             if k.endswith("_input_ids") or k.endswith("_attention_mask") or k.endswith("_labels"):
-                # adapted from https://stackoverflow.com/questions/73256206
-                if "prompt" in k:
-                    to_pad = [torch.LongTensor(ex[k][::-1]) for ex in batch]
-                else:
+                if self.is_encoder_decoder:
                     to_pad = [torch.LongTensor(ex[k]) for ex in batch]
-                if k.endswith("_input_ids"):
-                    padding_value = self.tokenizer.pad_token_id
-                elif k.endswith("_labels"):
-                    padding_value = self.label_pad_token_id
-                elif k.endswith("_attention_mask"):
-                    padding_value = self.padding_value
-                else:
-                    raise ValueError(f"Unexpected key in batch '{k}'")
 
-                padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
-                # for the prompt, flip back so padding is on left side
-                if "prompt" in k:
-                    padded_batch[k] = padded_batch[k].flip(dims=[1])
+                    if (k.startswith("prompt")) and (k.endswith("input_ids")):
+                        padding_value = self.tokenizer.pad_token_id
+                    elif k.endswith("_attention_mask"):
+                        padding_value = 0
+                    elif (k.startswith("chosen")) or (k.startswith("rejected")) or ("decoder" in k):
+                        padding_value = self.label_pad_token_id
+                    else:
+                        raise ValueError(f"Unexpected key in batch '{k}'")
+                    padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
+                else:
+                    # adapted from https://stackoverflow.com/questions/73256206
+                    if "prompt" in k:
+                        to_pad = [torch.LongTensor(ex[k][::-1]) for ex in batch]
+                    else:
+                        to_pad = [torch.LongTensor(ex[k]) for ex in batch]
+                    if k.endswith("_input_ids"):
+                        padding_value = self.tokenizer.pad_token_id
+                    elif k.endswith("_labels"):
+                        padding_value = self.label_pad_token_id
+                    elif k.endswith("_attention_mask"):
+                        padding_value = self.padding_value
+                    else:
+                        raise ValueError(f"Unexpected key in batch '{k}'")
+
+                    padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
+                    # for the prompt, flip back so padding is on left side
+                    if "prompt" in k:
+                        padded_batch[k] = padded_batch[k].flip(dims=[1])
             else:
                 padded_batch[k] = [ex[k] for ex in batch]
 
