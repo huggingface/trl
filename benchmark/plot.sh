7a4e276dc650358326efd8fef6fba600504febef
Costa Huang <costa.huang@outlook.com>
2023-09-08 09:16:38 -0400

    push changes

--- benchmark/plot.sh
+++ benchmark/plot.sh
@@ -1,10 +1,10 @@
 # pip install openrlbenchmark==0.2.1a5
-PR_662_TAG=v0.4.7-55-g110e672
-PR_662_NAME=PR-662
+PR_TAG=v0.4.7-55-g110e672
+PR_NAME=PR-662
 
 python -m openrlbenchmark.rlops_multi_metrics \
     --filters '?we=huggingface&wpn=trl&xaxis=_step&ceik=trl_ppo_trainer_config.value.reward_model&cen=trl_ppo_trainer_config.value.exp_name&metrics=env/reward_mean&metrics=objective/kl' \
-        "sentiment_tuning?tag=$PR_662_TAG&cl=sentiment lvwerra/gpt2-imdb ($PR_662_NAME)" \
+        "sentiment_tuning?tag=$PR_TAG&cl=sentiment lvwerra/gpt2-imdb ($PR_NAME)" \
     --env-ids sentiment-analysis:lvwerra/distilbert-imdb \
     --no-check-empty-runs \
     --pc.ncols 2 \
@@ -14,8 +14,8 @@ python -m openrlbenchmark.rlops_multi_metrics \
 
 python -m openrlbenchmark.rlops_multi_metrics \
     --filters '?we=huggingface&wpn=trl&xaxis=_step&ceik=trl_ppo_trainer_config.value.reward_model&cen=trl_ppo_trainer_config.value.exp_name&metrics=env/reward_mean&metrics=objective/kl' \
-        "sentiment_tuning?tag=$PR_662_TAG&cl=sentiment lvwerra/gpt2-imdb ($PR_662_NAME)" \
-        "sentiment_tuning_step_grad_accu?tag=$PR_662_TAG&cl=sentiment lvwerra/gpt2-imdb gradient accumulation ($PR_662_NAME)" \
+        "sentiment_tuning?tag=$PR_TAG&cl=sentiment lvwerra/gpt2-imdb ($PR_NAME)" \
+        "sentiment_tuning_step_grad_accu?tag=$PR_TAG&cl=sentiment lvwerra/gpt2-imdb gradient accumulation ($PR_NAME)" \
     --env-ids sentiment-analysis:lvwerra/distilbert-imdb \
     --no-check-empty-runs \
     --pc.ncols 2 \
@@ -25,10 +25,10 @@ python -m openrlbenchmark.rlops_multi_metrics \
 
 python -m openrlbenchmark.rlops_multi_metrics \
     --filters '?we=huggingface&wpn=trl&xaxis=_step&ceik=trl_ppo_trainer_config.value.reward_model&cen=trl_ppo_trainer_config.value.exp_name&metrics=env/reward_mean&metrics=objective/kl' \
-        "sentiment_tuning?tag=$PR_662_TAG&cl=sentiment lvwerra/gpt2-imdb ($PR_662_NAME)" \
-        "sentiment_tuning_gpt2?tag=$PR_662_TAG&cl=sentiment gpt2 ($PR_662_NAME)" \
-        "sentiment_tuning_falcon_rw_1b?tag=$PR_662_TAG&cl=sentiment tiiuae/falcon-rw-1b ($PR_662_NAME)" \
-        "sentiment_tuning_gpt2xl_grad_accu?tag=$PR_662_TAG&cl=sentiment gpt2xl ($PR_662_NAME)" \
+        "sentiment_tuning?tag=$PR_TAG&cl=sentiment lvwerra/gpt2-imdb ($PR_NAME)" \
+        "sentiment_tuning_gpt2?tag=$PR_TAG&cl=sentiment gpt2 ($PR_NAME)" \
+        "sentiment_tuning_falcon_rw_1b?tag=$PR_TAG&cl=sentiment tiiuae/falcon-rw-1b ($PR_NAME)" \
+        "sentiment_tuning_gpt2xl_grad_accu?tag=$PR_TAG&cl=sentiment gpt2xl ($PR_NAME)" \
     --env-ids sentiment-analysis:lvwerra/distilbert-imdb \
     --no-check-empty-runs \
     --pc.ncols 2 \
@@ -38,8 +38,8 @@ python -m openrlbenchmark.rlops_multi_metrics \
 
 python -m openrlbenchmark.rlops_multi_metrics \
     --filters '?we=huggingface&wpn=trl&xaxis=_step&ceik=trl_ppo_trainer_config.value.reward_model&cen=trl_ppo_trainer_config.value.exp_name&metrics=env/reward_mean&metrics=objective/kl' \
-        "sentiment_tuning?tag=$PR_662_TAG&cl=sentiment lvwerra/gpt2-imdb ($PR_662_NAME)" \
-        "sentiment_tuning_peft?tag=$PR_662_TAG&cl=sentiment lvwerra/gpt2-imdb w/ peft ($PR_662_NAME)" \
+        "sentiment_tuning?tag=$PR_TAG&cl=sentiment lvwerra/gpt2-imdb ($PR_NAME)" \
+        "sentiment_tuning_peft?tag=$PR_TAG&cl=sentiment lvwerra/gpt2-imdb w/ peft ($PR_NAME)" \
     --env-ids sentiment-analysis:lvwerra/distilbert-imdb \
     --no-check-empty-runs \
     --pc.ncols 2 \
