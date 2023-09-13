ace60961d16ef73dd6476fd048d733bdd9bcb190
Costa Huang <costa.huang@outlook.com>
2023-09-13 20:36:42 +0000

    use temporary dir for benchmark CI

--- benchmark/benchmark_and_report.sh
+++ benchmark/benchmark_and_report.sh
@@ -1,9 +1,20 @@
+#### Step 1: create a work directory:
+# this is necessary because another github action job will remove
+# the entire directory, which slurm depends on.
+# https://stackoverflow.com/questions/4632028/how-to-create-a-temporary-directory
+MY_SLURM_TMP_DIR=/fsx/costa/slurm_tmpdir
+mkdir -p $MY_SLURM_TMP_DIR
+WORK_DIR=`mktemp -d -p "$MY_SLURM_TMP_DIR"`
+cp -r "$PWD" "$WORK_DIR"
+cd "$WORK_DIR/$(basename "$PWD")"
+echo WORK_DIR: $WORK_DIR
+
+#### Step 2: actual work starts:
 echo PATH is $PATH
 echo PYTHONPATH is $PYTHONPATH
 echo whcih python is $(which python)
 
 export WANDB_ENTITY=huggingface
-
 bash benchmark/benchmark_core.sh > output.txt
 
 # Extract Job IDs into an array
@@ -27,4 +38,4 @@ done
 echo "TAGS_STRING: $TAGS_STRING"
 echo "FOLDER_STRING: $FOLDER_STRING"
 
-TAGS_STRING=$TAGS_STRING FOLDER_STRING=$FOLDER_STRING sbatch --dependency=afterany:$job_ids benchmark/post_github_comment.sbatch
+TAGS_STRING=$TAGS_STRING FOLDER_STRING=$FOLDER_STRING WORK_DIR=$WORK_DIR sbatch --dependency=afterany:$job_ids benchmark/post_github_comment.sbatch
--- benchmark/benchmark_aux.sh
+++ benchmark/benchmark_aux.sh
@@ -1,7 +1,19 @@
 ## w/ and w/o gradient accumulation
 python benchmark/benchmark.py \
     --command "python examples/scripts/sentiment_tuning.py --ppo_config.exp_name sentiment_tuning_step_grad_accu --ppo_config.mini_batch_size 1 --ppo_config.gradient_accumulation_steps 128 --ppo_config.log_with wandb" \
-    --num-seeds 5 \
+    --num-seeds 3 \
+    --start-seed 1 \
+    --workers 10 \
+    --slurm-nodes 1 \
+    --slurm-gpus-per-task 1 \
+    --slurm-ntasks 1 \
+    --slurm-total-cpus 12 \
+    --slurm-template-path benchmark/trl.slurm_template
+
+# compound
+python benchmark/benchmark.py \
+    --command "python examples/scripts/sentiment_tuning.py --ppo_config.exp_name sentiment_tuning_gpt2xl_grad_accu --ppo_config.model_name gpt2-xl --ppo_config.mini_batch_size 16 --ppo_config.gradient_accumulation_steps 8 --ppo_config.log_with wandb" \
+    --num-seeds 3 \
     --start-seed 1 \
     --workers 10 \
     --slurm-nodes 1 \
@@ -13,7 +25,7 @@ python benchmark/benchmark.py \
 ## w/ different models (gpt2, gpt2-xl, falcon, llama2)
 python benchmark/benchmark.py \
     --command "python examples/scripts/sentiment_tuning.py --ppo_config.exp_name sentiment_tuning_gpt2 --ppo_config.log_with wandb" \
-    --num-seeds 5 \
+    --num-seeds 3 \
     --start-seed 1 \
     --workers 10 \
     --slurm-nodes 1 \
@@ -23,7 +35,7 @@ python benchmark/benchmark.py \
     --slurm-template-path benchmark/trl.slurm_template
 python benchmark/benchmark.py \
     --command "python examples/scripts/sentiment_tuning.py --ppo_config.exp_name sentiment_tuning_falcon_rw_1b --ppo_config.model_name tiiuae/falcon-rw-1b --ppo_config.log_with wandb" \
-    --num-seeds 5 \
+    --num-seeds 3 \
     --start-seed 1 \
     --workers 10 \
     --slurm-nodes 1 \
@@ -36,7 +48,7 @@ python benchmark/benchmark.py \
 ## w/ and w/o PEFT
 python benchmark/benchmark.py \
     --command "python examples/scripts/sentiment_tuning.py --ppo_config.exp_name sentiment_tuning_peft --use_peft --ppo_config.log_with wandb" \
-    --num-seeds 5 \
+    --num-seeds 3 \
     --start-seed 1 \
     --workers 10 \
     --slurm-nodes 1 \
