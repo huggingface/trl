0821f905a8f9f794f7bcf4fd056c0c4a460fe7fc
Costa Huang <costa.huang@outlook.com>
2023-09-18 11:16:16 -0400

    Temp benchmark ci dir (#765)
    * Support fork in benchmark CI
    
    * use temporary dir for benchmark CI
    
    * debug
    
    * revert back
    
    * dependency fix
    
    * refactor script

--- benchmark/benchmark_level3.sh
+++ benchmark/benchmark_level3.sh
@@ -1,7 +1,7 @@
 ## w/ and w/o gradient accumulation
 python benchmark/benchmark.py \
     --command "python examples/scripts/sentiment_tuning.py --ppo_config.exp_name sentiment_tuning_step_grad_accu --ppo_config.mini_batch_size 1 --ppo_config.gradient_accumulation_steps 128 --ppo_config.log_with wandb" \
-    --num-seeds 5 \
+    --num-seeds 3 \
     --start-seed 1 \
     --workers 10 \
     --slurm-nodes 1 \
@@ -13,7 +13,7 @@ python benchmark/benchmark.py \
 ## w/ different models (gpt2, gpt2-xl, falcon, llama2)
 python benchmark/benchmark.py \
     --command "python examples/scripts/sentiment_tuning.py --ppo_config.exp_name sentiment_tuning_gpt2 --ppo_config.log_with wandb" \
-    --num-seeds 5 \
+    --num-seeds 3 \
     --start-seed 1 \
     --workers 10 \
     --slurm-nodes 1 \
@@ -23,7 +23,7 @@ python benchmark/benchmark.py \
     --slurm-template-path benchmark/trl.slurm_template
 python benchmark/benchmark.py \
     --command "python examples/scripts/sentiment_tuning.py --ppo_config.exp_name sentiment_tuning_falcon_rw_1b --ppo_config.model_name tiiuae/falcon-rw-1b --ppo_config.log_with wandb" \
-    --num-seeds 5 \
+    --num-seeds 3 \
     --start-seed 1 \
     --workers 10 \
     --slurm-nodes 1 \
@@ -36,7 +36,7 @@ python benchmark/benchmark.py \
 ## w/ and w/o PEFT
 python benchmark/benchmark.py \
     --command "python examples/scripts/sentiment_tuning.py --ppo_config.exp_name sentiment_tuning_peft --use_peft --ppo_config.log_with wandb" \
-    --num-seeds 5 \
+    --num-seeds 3 \
     --start-seed 1 \
     --workers 10 \
     --slurm-nodes 1 \
