d608fea0d107d4359f9c03a9d6dd434d292a9f50
Dev Aggarwal <devxpy@gmail.com>
2023-09-26 15:08:30 +0530

    Allow passing the token_ids as instruction_template in DataCollatorForCompletionOnlyLM (#749)
    * Update utils.py
    
    * correctly assign instruction_template in DataCollatorForCompletionOnlyLM
    
    * correctly use instruction_token_ids in DataCollatorForCompletionOnlyLM
    
    * DataCollatorForCompletionOnlyLM: fix instruction_template / response_template type check: handle cases where instruction_template is None
    
    * make precommit
    
    * Test DataCollatorForCompletionOnlyLM with pre-tokenized instruction_template

--- trl/trainer/utils.py
+++ trl/trainer/utils.py
@@ -75,21 +75,31 @@ class DataCollatorForCompletionOnlyLM(DataCollatorForLanguageModeling):
     def __init__(
         self,
         response_template: Union[str, List[int]],
-        instruction_template: Optional[str] = None,
+        instruction_template: Union[str, List[int]] = None,
         *args,
         mlm: bool = False,
         ignore_index: int = -100,
         **kwargs,
     ):
         super().__init__(*args, mlm=mlm, **kwargs)
+
         self.instruction_template = instruction_template
+        if isinstance(instruction_template, str):
+            # The user provides a string, must tokenize
+            self.instruction_token_ids = self.tokenizer.encode(self.instruction_template, add_special_tokens=False)
+        else:
+            # The user already provides the token ids
+            self.instruction_token_ids = instruction_template
+
         self.response_template = response_template
-        self.ignore_index = ignore_index
-        if type(response_template) == list:
+        if isinstance(response_template, str):
+            # The user provides a string, must tokenize
+            self.response_token_ids = self.tokenizer.encode(self.response_template, add_special_tokens=False)
+        else:
             # The user already provides the token ids
             self.response_token_ids = response_template
-        else:
-            self.response_token_ids = self.tokenizer.encode(self.response_template, add_special_tokens=False)
+
+        self.ignore_index = ignore_index
 
     def torch_call(self, examples: List[Union[List[int], Any, Dict[str, Any]]]) -> Dict[str, Any]:
         batch = super().torch_call(examples)
@@ -142,7 +152,7 @@ class DataCollatorForCompletionOnlyLM(DataCollatorForLanguageModeling):
                     )
                     batch["labels"][i, :] = self.ignore_index
 
-                human_token_ids = self.tokenizer.encode(self.instruction_template, add_special_tokens=False)
+                human_token_ids = self.instruction_token_ids
                 for human_idx in np.where(batch["labels"][i] == human_token_ids[0])[0]:
                     # find the indexes of the start of a human answer.
                     if human_token_ids == batch["labels"][i][human_idx : human_idx + len(human_token_ids)].tolist():
