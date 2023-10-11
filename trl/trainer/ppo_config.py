1752ae4f96eb12ce7311a2074469364cab984d6d
abhilash1910 <abhilash.majumder@intel.com>
2023-10-10 20:47:42 -0700

    review commit

--- trl/trainer/ppo_config.py
+++ trl/trainer/ppo_config.py
@@ -100,7 +100,9 @@ class PPOConfig:
     max_grad_norm: Optional[float] = None
     """Maximum gradient norm for gradient clipping"""
     optimize_cuda_cache: bool = False
-    """Optimize CUDA cache for slightly more memory-efficient training"""
+    """DEPRECATED: use `optimize_device_cache` instead, which does the same thing."""
+    optimize_device_cache: bool = False
+    """Optimize device cache for slightly more memory-efficient training"""
     early_stopping: bool = False
     """Whether to stop the PPO optimization loop early is the KL too high"""
     target_kl: float = 1
