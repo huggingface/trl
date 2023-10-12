58056704af8eae88d29858f4c011e4643e9b3ac1
abhilash1910 <abhilash.majumder@intel.com>
2023-10-11 22:39:33 -0700

    upstream

--- trl/models/modeling_base.py
+++ trl/models/modeling_base.py
@@ -24,7 +24,7 @@ from huggingface_hub.utils import EntryNotFoundError, HFValidationError, LocalEn
 from transformers import PreTrainedModel
 from transformers.integrations.deepspeed import is_deepspeed_zero3_enabled
 
-from ..import_utils import is_peft_available, is_xpu_available
+from ..import_utils import is_peft_available, is_xpu_available, is_transformers_greater_than
 
 
 if is_peft_available():
@@ -39,6 +39,11 @@ if is_peft_available():
         prepare_model_for_kbit_training,
     )
     from peft.peft_model import set_peft_model_state_dict
+
+if is_transformers_greater_than("4.33.0"):
+    from transformers.integrations.deepspeed import is_deepspeed_zero3_enabled
+else:
+    from transformers.deepspeed import is_deepspeed_zero3_enabled
 
 LAYER_PATTERNS = [
     "transformer.h.{layer}",
