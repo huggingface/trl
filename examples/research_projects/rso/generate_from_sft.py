962e0fb58f97c84a91e08a40de83e7cfd1376cc1
Gaetan LOPEZ <gaetanloplat@gmail.com>
2023-10-22 17:01:23 -0400

    adding ranking to rso script

--- examples/research_projects/rso/generate_from_sft.py
+++ examples/research_projects/rso/generate_from_sft.py
@@ -1,49 +1,58 @@
-import os
-import warnings
-from dataclasses import dataclass, field
-from typing import List, Optional, Tuple
+# coding=utf-8
+# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import numpy as np
+from dataclasses import dataclass, field
+from typing import Optional
+
 import torch
 from accelerate import Accelerator
-from datasets import Dataset, concatenate_datasets, load_dataset
+from datasets import Dataset, load_dataset
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import (
     AutoModelForCausalLM,
-    AutoModelForSequenceClassification,
     AutoTokenizer,
     DataCollatorForSeq2Seq,
-    DataCollatorWithPadding,
     HfArgumentParser,
     PreTrainedModel,
-    PreTrainedTokenizerBase,
-    Trainer,
-    TrainingArguments,
+    PreTrainedTokenizerBase
 )
+
 
 @dataclass
 class ScriptArguments:
+    # model parameters
     model_name_or_path: Optional[str] = field(default=None, metadata={"help": "the model name"})
     mixed_precision: Optional[str] = field(default="fp16", metadata={"help": "the model dtype"})
     # data parameters
     dataset_name: Optional[str] = field(default="Dahoas/full-hh-rlhf", metadata={"help": "the HF data path"})
-    split: Optional[bool] = field(default=True, metadata={"help": "the dataset split to use for generation"})
+    split: Optional[str] = field(default="train", metadata={"help": "the dataset split to use for generation"})
     batch_size: Optional[int] = field(default=8, metadata={"help": "the generation batch size"})
     max_prompt_length: Optional[int] = field(default=512, metadata={"help": "the maximum prompt length"})
-    save_dataset_path: Optional[str] = field(default="sft_gen_dataset", metadata={"help": "thepath to save the generated dataset"})
-
+    save_dataset_path: Optional[str] = field(default="sft_gen_dataset", metadata={"help": "the path for saving the generated dataset"})
     # generation parameters
     max_new_tokens: Optional[int] = field(
         default=128, metadata={"help": "the maximum number of tokens generated per sample"}
     )
-    temperature: Optional[float] = field(default=1.0)
-    top_p: Optional[float] = field(default=1.0)
-    top_k: Optional[float] = field(default=50)
-    num_return_sequences: Optional[int] = field(default=8)
-    
+    temperature: Optional[float] = field(default=1.0, metadata={"help": "the sampling temperature"})
+    top_p: Optional[float] = field(default=1.0, metadata={"help": "top_p sampling argument"})
+    top_k: Optional[float] = field(default=50, metadata={"help": "top_k sampling argument"})
+    num_return_sequences: Optional[int] = field(default=64, metadata={"help": "the number of return sequences"})
     # instrumentation
     sanity_check: Optional[bool] = field(default=False)
+    
     
 @torch.no_grad()
 def generate(
@@ -68,11 +77,10 @@ def generate(
         )
 
         generated_tokens = all_tokens[:, sequence_length:]
+        prompt_tokens = all_tokens[:,:sequence_length]
 
         generated_tokens = accelerator.pad_across_processes(generated_tokens, dim=1, pad_index=tokenizer.pad_token_id)
-        input_ids = torch.repeat_interleave(batch["input_ids"], generation_kwargs["num_return_sequences"], dim=0)
-
-        prompt_tokens = accelerator.pad_across_processes(input_ids, dim=1, pad_index=tokenizer.pad_token_id)
+        prompt_tokens = accelerator.pad_across_processes(prompt_tokens, dim=1, pad_index=tokenizer.pad_token_id)
 
         generated_tokens = accelerator.gather(generated_tokens)
         generated_tokens = generated_tokens.cpu()
@@ -90,6 +98,7 @@ def generate(
     accelerator.wait_for_everyone()
 
     all_predictions = tokenizer.batch_decode(all_predictions, skip_special_tokens=True)
+    all_prompts = tokenizer.batch_decode(all_prompts, skip_special_tokens=True)
 
     def filter_text(text):
         """
@@ -99,11 +108,11 @@ def generate(
 
     # postprocessing
     all_predictions = [filter_text(generated_text) for generated_text in all_predictions]
-    all_prompts = tokenizer.batch_decode(all_prompts, skip_special_tokens=True)
-
+    
     generated_dataset = Dataset.from_dict({"prompt": all_prompts, "response": all_predictions})
 
     return generated_dataset
+
 
 if __name__ == "__main__":
     parser = HfArgumentParser(ScriptArguments)
@@ -138,7 +147,7 @@ if __name__ == "__main__":
     dataset = load_dataset(script_args.dataset_name)[script_args.split]
 
     if script_args.sanity_check:
-        dataset = dataset.dataset(range(min(len(dataset), 100)))
+        dataset = dataset.select(range(min(len(dataset), 100)))
 
     def tokenize_fn(samples):
         model_inputs = tokenizer(samples["prompt"])
@@ -148,13 +157,16 @@ if __name__ == "__main__":
         }
 
     dataset = dataset.map(tokenize_fn, batched=True, remove_columns=list(dataset.features))
+    dataset = dataset.filter(lambda x: len(x["input_ids"])<script_args.max_prompt_length)
 
-    data_collator = DataCollatorForSeq2Seq(tokenizer, max_length=script_args.max_length, pad_to_multiple_of=8)
+    data_collator = DataCollatorForSeq2Seq(tokenizer, max_length=script_args.max_prompt_length, pad_to_multiple_of=8)
 
     dataloader = DataLoader(dataset, batch_size=script_args.batch_size, shuffle=False, collate_fn=data_collator)
 
     model, dataloader = accelerator.prepare(model, dataloader)
 
+    # generate responses from sft policy
     generated_dataset = generate(model, dataloader, tokenizer, accelerator, **generation_kwargs)
     
+    # save the generated dataset
     generated_dataset.save_to_disk(script_args.save_dataset_path)
--- examples/research_projects/rso/rso.py
+++ examples/research_projects/rso/rso.py
@@ -1,4 +1,19 @@
-import os
+# coding=utf-8
+# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
 import warnings
 from dataclasses import dataclass, field
 from typing import List, Optional, Tuple
@@ -14,8 +29,7 @@ from transformers import (
     AutoTokenizer,
     DataCollatorWithPadding,
     HfArgumentParser,
-    PreTrainedModel,
-    PreTrainedTokenizerBase,
+    PreTrainedModel
 )
 
 @dataclass
@@ -23,8 +37,9 @@ class ScriptArguments:
     reward_model_name_or_path: Optional[str] = field(default=None, metadata={"help": "the model name"})
     mixed_precision: Optional[str] = field(default="fp16", metadata={"help": "the model dtype"})
     # data parameters
-    dataset_name: Optional[str] = field(default="Dahoas/full-hh-rlhf", metadata={"help": "the HF data path"})
-    batch_size: Optional[int] = field(default=8, metadata={"help": "the generation batch size"})
+    dataset_name: Optional[str] = field(default=None, metadata={"help": "the generated dataset path"})
+    batch_size: Optional[int] = field(default=32, metadata={"help": "the scoring batch size"})
+    save_dataset_path: Optional[str] = field(default="sft_gen_dataset_ranked", metadata={"help": "the path for saving the dataset"})
     
     # rejection sampling 
     num_samples: Optional[int] = field(default=8, metadata={"help": "the number of samples to keep after rejection sampling"})
@@ -35,7 +50,7 @@ class ScriptArguments:
     sanity_check: Optional[bool] = field(default=False)
     
   
-# taken from https://arxiv.org/pdf/2309.06657.pdf 
+# modified from https://arxiv.org/pdf/2309.06657.pdf 
 def conduct_rejection_sampling(
     response_candidates: List[str],
     response_rewards: List[float],
@@ -51,10 +66,12 @@ def conduct_rejection_sampling(
         beta: beta parameter in KL-constrained reward maximization objective.
         
     Returns:
-        Rejection sampled sequences from the optimal policy.
+        accepted: Accepted rejection sampled sequences from the optimal policy.
+        rewards: the rewards associated to the accepted samples.
     """
     candidates = {c: r for c, r in zip(response_candidates, response_rewards)}
     accepted = []
+    rewards = []
     while len(accepted) < num_samples:
         max_reward = max(candidates.values())
         to_remove = []
@@ -63,21 +80,85 @@ def conduct_rejection_sampling(
             if u >= np.exp((r - max_reward) / beta):
                 continue
             accepted.append(c)
+            rewards.append(r)
             to_remove.append(c)
             if len(accepted) == num_samples:
                 break
         for c in to_remove:
             candidates.pop(c)
-    return accepted
+    return accepted, rewards
+
+
+def first_round_ranking(responses: List[str], rewards: List[float]) -> Tuple[List[str], List[str]]:
+    """Conducts first round ranking. Starts from n responses and construct n/2 pairs to be assigned
+    to chosen or rejected based on there rewards.
+    
+    Args:
+        responses: accecpted candidates from rejection sampling
+        rewards: response rewards.
+        
+    Returns:
+        chosen: chosen samples.
+        rejected: rejected samples.
+    """
+    
+    chosen = []
+    rejected = []
+    
+    def pick(responses):
+        selected = random.randrange(len(responses))
+        return responses.pop(selected)
+    
+    responses = [(response, reward) for response, reward in zip(responses,rewards)]
+    while responses:
+        selected1 = pick(responses)
+        selected2 = pick(responses)
+        if selected1[1]>selected2[1]:
+            chosen.append(selected1[0])
+            rejected.append(selected2[0])
+        else:
+            chosen.append(selected2[0])
+            rejected.append(selected1[0])
+            
+    return chosen, rejected
+
+
+def tournament_ranking(responses: List[str], rewards: List[float]):
+    """Conducts tournament ranking. Starts from n responses and construct n-1 pairs to be assigned
+    to chosen or rejected based on there rewards.
+    
+    Args:
+        responses: accecpted candidates from rejection sampling.
+        rewards: response rewards.
+        
+    Returns:
+        chosen: chosen samples.
+        rejected: rejected samples.
+    """
+    sorted_responses = [response for _, response in sorted(zip(rewards, responses), reverse=True)]
+    
+    chosen = [sorted_responses[i] for i in range(0, len(responses), 2)]
+    rejected =[sorted_responses[i] for i in range(1, len(responses), 2)]
+    
+    return chosen, rejected
 
       
 @torch.no_grad()
 def score(
     model: PreTrainedModel,
     dataloader: DataLoader,
-    accelerator: Accelerator,
-    dataset: Dataset
-) -> Dataset:
+    accelerator: Accelerator
+) -> List[float]:
+    """Score the generated dataset based on a reward model.
+    
+    Args:
+        model: the model used to score samples.
+        dataloader: the dataloader containing batches of elements from the generated dataset.
+        accelerator: the accelerator object.
+        
+    Returns:
+        rewards: rewards assigned to each sample of the generated dataset.
+    """
 
     rewards = []
     pbar = tqdm(total=len(dataloader), disable=not accelerator.is_local_main_process)
@@ -87,17 +168,22 @@ def score(
         scores = accelerator.gather(scores)
         rewards.extend(scores)
         pbar.update(1)
-
-    rewards = rewards[: len(dataset)]
+        
     rewards = [reward.item() for reward in rewards]
 
-    dataset = dataset.add_column("rewards", rewards)
-
-    return dataset
+    return rewards
 
 if __name__ == "__main__":
     parser = HfArgumentParser(ScriptArguments)
     script_args = parser.parse_args_into_dataclasses()[0]
+    
+    
+    if script_args.num_samples%2!=0:
+        warnings.warn(
+            "Creating pairs requires an even number for num_samples."
+            f"Setting num_samples to {script_args.num_samples+1} instead of {script_args.num_samples}"
+        )
+        script_args.num_samples += 1
 
     accelerator = Accelerator(
         mixed_precision=script_args.mixed_precision
@@ -105,13 +191,13 @@ if __name__ == "__main__":
     
     # load reward model and tokenizer
     model = AutoModelForSequenceClassification.from_pretrained(script_args.reward_model_name_or_path)
-    tokenizer = AutoTokenizer.from_pretrained(script_args.model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(script_args.reward_model_name_or_path)
 
     # load and preprocess the dataset
     dataset = load_from_disk(script_args.dataset_name)
 
     if script_args.sanity_check:
-        dataset = dataset.dataset(range(min(len(dataset), 100)))
+        dataset = dataset.dataset(range(min(len(dataset), 500)))
 
     def tokenize_fn(samples):
         # create the text column first
@@ -126,34 +212,43 @@ if __name__ == "__main__":
 
     data_collator = DataCollatorWithPadding(tokenizer)
 
-    dataloader = DataLoader(dataset, batch_size=script_args.batch_size, shuffle=False, collate_fn=data_collator)
+    dataloader = DataLoader(reward_dataset, batch_size=script_args.batch_size, shuffle=False, collate_fn=data_collator)
 
     model, dataloader = accelerator.prepare(model, dataloader)
     
-    dataset = score(model, dataloader, accelerator, dataset)
+    rewards = score(model, dataloader, accelerator)
+    
+    rewards = rewards[: len(dataset)]
+
+    dataset = dataset.add_column("rewards", rewards)
     
     # perform rejection sampling
+    df = dataset.to_pandas()
+    df = df.groupby("prompt").agg({"response":lambda x: list(x), "rewards":lambda x: list(x)}).reset_index()
     
-    dataset = dataset.to_pandas()
-    
-    dataset = dataset.groupby("prompt").agg({'response':lambda x: list(x), 'scores':lambda x: list(x)}).reset_index()
-    
-    dataset["accepted"] = dataset.apply(
-        lambda x: conduct_rejection_sampling(
-            x["response"], 
-            x["rewards"], 
-            script_args.num_samples, 
-            script_args.beta
+    # conduct rejected sampling algorithm as in https://arxiv.org/pdf/2309.06657.pdf
+    df["accepted"], df["rewards"] = zip(*df.apply(
+            lambda x: conduct_rejection_sampling(
+                x["response"], 
+                x["rewards"], 
+                script_args.num_samples, 
+                script_args.beta
+            ),
+            axis=1
         )
     )
     
-    # sort the list first
-    # then do tournament or first_round ranking
+    # perform ranking
+    ranking_fn = tournament_ranking if "tournament" in script_args.ranking_method else first_round_ranking
     
+    df["chosen"], df["rejected"] = zip(*df.apply(lambda x: ranking_fn(x["accepted"], x["rewards"]), axis=1))
+    df = df.filter(["prompt", "chosen", "rejected"])
+    df = df.explode(["chosen", "rejected"])
     
+    dataset = Dataset.from_pandas(df)
     
     # save the dataset for later finetuning with DPO
-    
+    dataset.save_to_disk(script_args.save_dataset_path)
     
     
     
