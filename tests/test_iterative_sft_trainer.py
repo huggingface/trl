71c28fd336bab86c9a40c849e8ee433bdfec62e5
Gaetan LOPEZ <gaetanloplat@gmail.com>
2023-11-01 21:51:07 -0400

    fix testing

--- tests/test_iterative_sft_trainer.py
+++ tests/test_iterative_sft_trainer.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import tempfile
 import unittest
 
 import torch
@@ -65,36 +66,41 @@ class IterativeTrainerTester(unittest.TestCase):
             ["gpt2", "tensor"],
             ["gpt2", "text"],
             ["t5", "tensor"],
-            # ["t5", "text"],
+            ["t5", "text"],
         ]
     )
     def test_iterative_step_from_tensor(self, model_name, input_name):
-        # initialize dataset
-        if input_name == "tensor":
-            dummy_dataset = self._init_tensor_dummy_dataset()
-            inputs = {
-                "input_ids": dummy_dataset["input_ids"],
-                "attention_mask": dummy_dataset["attention_mask"],
-                "labels": dummy_dataset["labels"],
-            }
-        else:
-            dummy_dataset = self._init_textual_dummy_dataset()
-            inputs = {
-                "texts": dummy_dataset["texts"],
-                "texts_labels": dummy_dataset["texts_labels"],
-            }
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            # initialize dataset
+            if input_name == "tensor":
+                dummy_dataset = self._init_tensor_dummy_dataset()
+                inputs = {
+                    "input_ids": dummy_dataset["input_ids"],
+                    "attention_mask": dummy_dataset["attention_mask"],
+                    "labels": dummy_dataset["labels"],
+                }
+            else:
+                dummy_dataset = self._init_textual_dummy_dataset()
+                inputs = {
+                    "texts": dummy_dataset["texts"],
+                    "texts_labels": dummy_dataset["texts_labels"],
+                }
 
-        if model_name == "gpt2":
-            model = self.model
-            tokenizer = self.tokenizer
-        else:
-            model = self.t5_model
-            tokenizer = self.t5_tokenizer
+            if model_name == "gpt2":
+                model = self.model
+                tokenizer = self.tokenizer
+            else:
+                model = self.t5_model
+                tokenizer = self.t5_tokenizer
 
-        args = TrainingArguments(max_steps=2)
-        iterative_trainer = IterativeSFTTrainer(model=model, args=args, tokenizer=tokenizer)
+            args = TrainingArguments(
+                output_dir=tmp_dir,
+                per_device_train_batch_size=2,
+                max_steps=2,
+            )
+            iterative_trainer = IterativeSFTTrainer(model=model, args=args, tokenizer=tokenizer)
 
-        iterative_trainer.step(**inputs)
+            iterative_trainer.step(**inputs)
 
-        for param in iterative_trainer.model.parameters():
-            assert param.grad is not None
+            for param in iterative_trainer.model.parameters():
+                assert param.grad is not None
