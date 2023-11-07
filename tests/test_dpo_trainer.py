3c082993fb803b0dc2e951e942c39ed7e9ebb753
Kashif Rasul <kashif.rasul@gmail.com>
2023-11-07 12:16:22 +0100

    undo test refactor

--- tests/test_dpo_trainer.py
+++ tests/test_dpo_trainer.py
@@ -28,24 +28,17 @@ from .testing_utils import require_no_wandb, require_peft
 class DPOTrainerTester(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.models = {
-            "gpt2": {
-                "model_id": "trl-internal-testing/dummy-GPT2-correct-vocab",
-                "model_type": AutoModelForCausalLM,
-                "tokenizer_type": AutoTokenizer,
-            },
-            "t5": {
-                "model_id": "trl-internal-testing/tiny-T5ForConditionalGeneration-correct-vocab",
-                "model_type": AutoModelForSeq2SeqLM,
-                "tokenizer_type": AutoTokenizer,
-            },
-            # add more models here if needed
-        }
-        for key, model_info in cls.models.items():
-            model_info["model"] = model_info["model_type"].from_pretrained(model_info["model_id"])
-            model_info["ref_model"] = model_info["model_type"].from_pretrained(model_info["model_id"])
-            model_info["tokenizer"] = model_info["tokenizer_type"].from_pretrained(model_info["model_id"])
-        cls.models["gpt2"]["tokenizer"].pad_token = cls.models["gpt2"]["tokenizer"].eos_token
+        cls.model_id = "trl-internal-testing/dummy-GPT2-correct-vocab"
+        cls.model = AutoModelForCausalLM.from_pretrained(cls.model_id)
+        cls.ref_model = AutoModelForCausalLM.from_pretrained(cls.model_id)
+        cls.tokenizer = AutoTokenizer.from_pretrained(cls.model_id)
+        cls.tokenizer.pad_token = cls.tokenizer.eos_token
+
+        # get t5 as seq2seq example:
+        model_id = "trl-internal-testing/tiny-T5ForConditionalGeneration-correct-vocab"
+        cls.t5_model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+        cls.t5_ref_model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+        cls.t5_tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     def _init_dummy_dataset(self):
         # fmt: off
@@ -81,12 +74,8 @@ class DPOTrainerTester(unittest.TestCase):
         # fmt: on
         return Dataset.from_dict(dummy_dataset_dict)
 
-    def _get_models_by_name(self, name):
-        return self.models[name]["model"], self.models[name]["ref_model"], self.models[name]["tokenizer"]
-
     @parameterized.expand([["gpt2", "sigmoid", True], ["t5", "hinge", False]])
-    def test_dpo_trainer(self, name, loss_type, precompute_ref_log_probs):
-        model, ref_model, tokenizer = self._get_models_by_name(name)
+    def test_dpo_trainer(self, name, loss_type, pre_compute):
         with tempfile.TemporaryDirectory() as tmp_dir:
             training_args = TrainingArguments(
                 output_dir=tmp_dir,
@@ -100,6 +89,15 @@ class DPOTrainerTester(unittest.TestCase):
 
             dummy_dataset = self._init_dummy_dataset()
 
+            if name == "gpt2":
+                model = self.model
+                ref_model = self.ref_model
+                tokenizer = self.tokenizer
+            elif name == "t5":
+                model = self.t5_model
+                ref_model = self.t5_ref_model
+                tokenizer = self.t5_tokenizer
+
             trainer = DPOTrainer(
                 model=model,
                 ref_model=ref_model,
@@ -109,7 +107,7 @@ class DPOTrainerTester(unittest.TestCase):
                 tokenizer=tokenizer,
                 train_dataset=dummy_dataset,
                 eval_dataset=dummy_dataset,
-                precompute_ref_log_probs=precompute_ref_log_probs,
+                precompute_ref_log_probs=pre_compute,
             )
 
             previous_trainable_params = {n: param.clone() for n, param in trainer.model.named_parameters()}
@@ -125,9 +123,7 @@ class DPOTrainerTester(unittest.TestCase):
                 if param.sum() != 0:
                     self.assertFalse(torch.equal(param, new_param))
 
-    @parameterized.expand([["gpt2", True], ["t5", True]])
-    def test_dpo_trainer_without_providing_ref_model(self, name, precompute_ref_log_probs):
-        model, _, tokenizer = self._get_models_by_name(name)
+    def test_dpo_trainer_without_providing_ref_model(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
             training_args = TrainingArguments(
                 output_dir=tmp_dir,
@@ -142,14 +138,13 @@ class DPOTrainerTester(unittest.TestCase):
             dummy_dataset = self._init_dummy_dataset()
 
             trainer = DPOTrainer(
-                model=model,
+                model=self.model,
                 ref_model=None,
                 beta=0.1,
                 args=training_args,
-                tokenizer=tokenizer,
+                tokenizer=self.tokenizer,
                 train_dataset=dummy_dataset,
                 eval_dataset=dummy_dataset,
-                precompute_ref_log_probs=precompute_ref_log_probs,
             )
 
             previous_trainable_params = {n: param.clone() for n, param in trainer.model.named_parameters()}
@@ -167,9 +162,7 @@ class DPOTrainerTester(unittest.TestCase):
 
     @require_peft
     @mark.peft_test
-    @parameterized.expand([["gpt2", True], ["t5", True]])
-    def test_dpo_trainer_without_providing_ref_model_with_lora(self, name, precompute_ref_log_probs):
-        model, _, tokenizer = self._get_models_by_name(name)
+    def test_dpo_trainer_without_providing_ref_model_with_lora(self):
         from peft import LoraConfig
 
         lora_config = LoraConfig(
@@ -194,15 +187,14 @@ class DPOTrainerTester(unittest.TestCase):
             dummy_dataset = self._init_dummy_dataset()
 
             trainer = DPOTrainer(
-                model=model,
+                model=self.model,
                 ref_model=None,
                 beta=0.1,
                 args=training_args,
-                tokenizer=tokenizer,
+                tokenizer=self.tokenizer,
                 train_dataset=dummy_dataset,
                 eval_dataset=dummy_dataset,
                 peft_config=lora_config,
-                precompute_ref_log_probs=precompute_ref_log_probs,
             )
 
             previous_trainable_params = {n: param.clone() for n, param in trainer.model.named_parameters()}
@@ -219,10 +211,8 @@ class DPOTrainerTester(unittest.TestCase):
                     if param.sum() != 0:
                         self.assertFalse(torch.equal(param, new_param))
 
-    @parameterized.expand([["gpt2", False], ["t5", True]])
     @require_no_wandb
-    def test_dpo_trainer_generate_during_eval_no_wandb(self, name, precompute_ref_log_probs):
-        model, _, tokenizer = self._get_models_by_name(name)
+    def test_dpo_trainer_generate_during_eval_no_wandb(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
             training_args = TrainingArguments(
                 output_dir=tmp_dir,
@@ -242,15 +232,14 @@ class DPOTrainerTester(unittest.TestCase):
                 " Please install `wandb` to resolve.",
             ):
                 DPOTrainer(
-                    model=model,
+                    model=self.model,
                     ref_model=None,
                     beta=0.1,
                     args=training_args,
-                    tokenizer=tokenizer,
+                    tokenizer=self.tokenizer,
                     train_dataset=dummy_dataset,
                     eval_dataset=dummy_dataset,
                     generate_during_eval=True,
-                    precompute_ref_log_probs=precompute_ref_log_probs,
                 )
 
     @require_peft
@@ -266,8 +255,8 @@ class DPOTrainerTester(unittest.TestCase):
             task_type="CAUSAL_LM",
         )
 
-        # gpt2 lora model
-        model, _, tokenizer = self._get_models_by_name("gpt2")
+        # lora model
+        model = AutoModelForCausalLM.from_pretrained(self.model_id)
         model_peft = get_peft_model(model, lora_config)
 
         with tempfile.TemporaryDirectory() as tmp_dir:
@@ -289,7 +278,7 @@ class DPOTrainerTester(unittest.TestCase):
                 ref_model=None,
                 beta=0.1,
                 args=training_args,
-                tokenizer=tokenizer,
+                tokenizer=self.tokenizer,
                 train_dataset=dummy_dataset,
                 eval_dataset=dummy_dataset,
                 peft_config=lora_config,
