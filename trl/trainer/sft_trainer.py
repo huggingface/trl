c7bea2c46e919c46d077b10fa294656870275e2c
younesbelkada <younesbelkada@gmail.com>
2023-11-16 17:27:57 +0000

    style

--- trl/trainer/sft_trainer.py
+++ trl/trainer/sft_trainer.py
@@ -181,10 +181,10 @@ class SFTTrainer(Trainer):
                     if hasattr(model, "enable_input_require_grads"):
                         model.enable_input_require_grads()
                     else:
-    
+
                         def make_inputs_require_grad(module, input, output):
                             output.requires_grad_(True)
-    
+
                         model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
 
                 model = get_peft_model(model, peft_config)
