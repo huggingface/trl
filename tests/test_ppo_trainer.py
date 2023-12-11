bf0f7052e85a35d292b68cc3d67fd710bbbc904d
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-12-11 16:39:15 +0100

    Update test_ppo_trainer.py

--- tests/test_ppo_trainer.py
+++ tests/test_ppo_trainer.py
@@ -595,7 +595,7 @@ class PPOTrainerTester(unittest.TestCase):
             returns[idx].unsqueeze(0),
         )
 
-        self.assertAlmostEqual(pg_loss.item(), 2.2868, 4)
+        self.assertAlmostEqual(pg_loss.item(), 2.0494, 4)
         self.assertAlmostEqual(v_loss.item(), 0.09950, 4)
 
         # check if we get same results with masked parts removed
