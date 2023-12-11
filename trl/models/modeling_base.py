cb7819e627d40d2e66eee84ca304692df0b2c607
Wang, Yi <yi.a.wang@intel.com>
2023-12-11 23:37:01 +0800

    add local folder support as input for rl_training. (#1078)
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- trl/models/modeling_base.py
+++ trl/models/modeling_base.py
@@ -20,7 +20,12 @@ import torch
 import torch.nn as nn
 from accelerate import PartialState
 from huggingface_hub import hf_hub_download
-from huggingface_hub.utils import EntryNotFoundError, HFValidationError, LocalEntryNotFoundError
+from huggingface_hub.utils import (
+    EntryNotFoundError,
+    HFValidationError,
+    LocalEntryNotFoundError,
+    RepositoryNotFoundError,
+)
 from safetensors.torch import load_file as safe_load_file
 from transformers import PreTrainedModel
 
@@ -177,7 +182,7 @@ class PreTrainedModelWrapper(nn.Module):
                         "adapter_config.json",
                         token=token,
                     )
-                except (EntryNotFoundError, LocalEntryNotFoundError, HFValidationError):
+                except (EntryNotFoundError, LocalEntryNotFoundError, HFValidationError, RepositoryNotFoundError):
                     remote_adapter_config = None
             else:
                 remote_adapter_config = None
@@ -353,7 +358,7 @@ class PreTrainedModelWrapper(nn.Module):
                 token=token,
             )
         # sharded
-        except (EntryNotFoundError, LocalEntryNotFoundError, HFValidationError):
+        except (EntryNotFoundError, LocalEntryNotFoundError, HFValidationError, RepositoryNotFoundError):
             if os.path.exists(index_filename):
                 index_file_name = index_filename
             else:
@@ -363,7 +368,7 @@ class PreTrainedModelWrapper(nn.Module):
                         model_index_name,
                         token=token,
                     )
-                except (EntryNotFoundError, LocalEntryNotFoundError, HFValidationError):
+                except (EntryNotFoundError, LocalEntryNotFoundError, HFValidationError, RepositoryNotFoundError):
                     # not continue training, do not have v_head weight
                     is_resuming_training = False
                     logging.warning(
