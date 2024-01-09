5de03db56868e299b41e0384a039d5c572ad3900
Kashif Rasul <kashif.rasul@gmail.com>
2024-01-09 15:53:55 +0100

    Update examples/scripts/kto.py
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>

--- examples/scripts/kto.py
+++ examples/scripts/kto.py
@@ -129,7 +129,7 @@ def get_hh(split: str, sanity_check: bool = False, silent: bool = False, cache_d
 
 
 if __name__ == "__main__":
-    parser = HfArgumentParser(ScriptArguments)
+    parser = HfArgumentParser(ScriptArguments, KTOConfig)
     script_args = parser.parse_args_into_dataclasses()[0]
 
     # 1. load a pretrained model
