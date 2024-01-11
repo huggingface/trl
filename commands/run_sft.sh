cc15412e6330ac2ff4c04e015e2bd30333754c8d
younesbelkada <younesbelkada@gmail.com>
2024-01-11 09:17:19 +0000

    fix comment

--- commands/run_sft.sh
+++ commands/run_sft.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# This script runs an SFT example end-to-end on a tiny model using QLoRA & PEFT
-
+# This script runs an SFT example end-to-end on a tiny model using different possible configurations
+# but defaults to QLoRA + PEFT
 OUTPUT_DIR="test_sft/"
 MODEL_NAME="HuggingFaceM4/tiny-random-LlamaForCausalLM"
 DATASET_NAME="imdb"
