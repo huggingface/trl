a6addfd0d20e1d6cada94ad9165ea447cecf945a
Costa Huang <costa.huang@outlook.com>
2024-01-11 19:01:35 +0000

    quick change

--- trl/trainer/utils.py
+++ trl/trainer/utils.py
@@ -20,13 +20,16 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 import numpy as np
 import torch
 from accelerate import Accelerator
-from peft import LoraConfig, PeftConfig
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import IterableDataset
 from transformers import BitsAndBytesConfig, DataCollatorForLanguageModeling, PreTrainedTokenizerBase
 
-from ..import_utils import is_unsloth_available, is_xpu_available
+from ..import_utils import is_peft_available, is_unsloth_available, is_xpu_available
 from ..trainer.model_config import ModelConfig
+
+
+if is_peft_available():
+    from peft import LoraConfig, PeftConfig
 
 
 class AdaptiveKLController:
