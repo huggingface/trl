348faf002f3e25b8d972f66fa4353fd8b59e9914
Costa Huang <costa.huang@outlook.com>
2024-01-12 09:48:37 -0500

    Update trl/trainer/model_config.py
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>

--- trl/trainer/model_config.py
+++ trl/trainer/model_config.py
@@ -18,7 +18,7 @@ class ModelConfig:
         default=None,
         metadata={
             "help": (
-                "The model checkpoint for weights initialization. Don't set if you want to train a model from scratch."
+                "The model checkpoint for weights initialization."
             )
         },
     )
