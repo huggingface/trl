8f60b2b40a5da916fced83cfba4fd516df28fc30
philschmid <schmidphilipp1995@gmail.com>
2024-01-18 14:50:57 +0000

    fix template & add test

--- tests/test_dataset_formatting.py
+++ tests/test_dataset_formatting.py
@@ -147,3 +147,20 @@ class SetupChatFormatTestCase(unittest.TestCase):
         self.assertTrue(len(modified_tokenizer) == original_tokenizer_len + 2)
         self.assertTrue(self.model.get_input_embeddings().weight.shape[0] % 64 == 0)
         self.assertTrue(self.model.get_input_embeddings().weight.shape[0] == original_tokenizer_len + 64)
+
+    def test_example_with_setup_model(self):
+        modified_model, modified_tokenizer = setup_chat_format(
+            self.model,
+            self.tokenizer,
+        )
+        messages = [
+            {"role": "system", "content": "You are helpful"},
+            {"role": "user", "content": "Hello"},
+            {"role": "assistant", "content": "Hi, how can I help you?"},
+        ]
+        prompt = modified_tokenizer.apply_chat_template(messages, tokenize=False)
+
+        self.assertEqual(
+            prompt,
+            "<|im_start|>system\nYou are helpful<|im_end|>\n<|im_start|>user\nHello<|im_end|>\n<|im_start|>assistant\nHi, how can I help you?<|im_end|>\n",
+        )
