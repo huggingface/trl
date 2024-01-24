2a9a13db52fa510c10cd626038926b6b45ae924d
John Giorgi <johnmgiorgi@gmail.com>
2024-01-24 16:59:44 -0500

    fix: use context manager instead of attribute
    Co-authored-by: Younes Belkada <49240599+younesbelkada@users.noreply.github.com>

--- trl/trainer/sft_trainer.py
+++ trl/trainer/sft_trainer.py
@@ -254,7 +254,7 @@ class SFTTrainer(Trainer):
                 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
         # Pre-process the datasets only once per node. The remaining processes will use the cache.
-        with PartialState().is_local_main_process():
+        with PartialState().is_local_main_process:
             if dataset_kwargs is None:
                 dataset_kwargs = {}
             if train_dataset is not None:
