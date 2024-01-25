cd75d5cf843c0038ca70537a3b3729cbb363e3d6
Kashif Rasul <kashif.rasul@gmail.com>
2024-01-25 12:56:15 +0100

    fix typos

--- trl/trainer/kto_trainer.py
+++ trl/trainer/kto_trainer.py
@@ -847,14 +847,14 @@ class KTOTrainer(Trainer):
 
         prefix = "eval_" if train_eval == "eval" else ""
         metrics[f"{prefix}rewards/chosen"] = chosen_rewards.mean().cpu()
-        metrics[f"{prefix}rewards/rejected"] = rejected_rewards.cpu()
+        metrics[f"{prefix}rewards/rejected"] = rejected_rewards.mean().cpu()
         metrics[f"{prefix}rewards/accuracies"] = reward_accuracies.cpu()
         metrics[f"{prefix}rewards/margins"] = (chosen_rewards.mean() - rejected_rewards.mean()).cpu()
         metrics[f"{prefix}logps/rejected"] = policy_rejected_logps.detach().mean().cpu()
         metrics[f"{prefix}logps/chosen"] = policy_chosen_logps.detach().mean().cpu()
         metrics[f"{prefix}logits/rejected"] = policy_rejected_logits.detach().mean().cpu()
         metrics[f"{prefix}logits/chosen"] = policy_chosen_logits.detach().mean().cpu()
-        metrics[f"{prefix}kl"] = kl.cpu()
+        metrics[f"{prefix}kl"] = kl.item()
 
         return losses.mean(), metrics
 
