f633d09272cab77e3cb2b70d352fe87e47b64457
Costa Huang <costa.huang@outlook.com>
2024-01-26 18:05:27 +0000

    Fix sft ci

--- commands/run_sft.sh
+++ commands/run_sft.sh
@@ -42,7 +42,7 @@ accelerate launch $EXTRA_ACCELERATE_ARGS \
     --output_dir $OUTPUT_DIR \
     --max_steps $MAX_STEPS \
     --per_device_train_batch_size $BATCH_SIZE \
-    --seq_length $SEQ_LEN \
+    --max_seq_length $SEQ_LEN \
     $EXTRA_TRAINING_ARGS
 """
 
