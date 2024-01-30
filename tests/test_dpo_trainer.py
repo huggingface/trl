207685dc01904d7f1ff216936df3c4745ca63fa2
imraviagrawal <raviagrawal@umass.edu>
2024-01-29 17:55:07 -0800

    Add multiprocessing in the DPO trainer. (#1286)
    * Update dpo_trainer.py
    
    Added support for num_proc to tokenize the training dataset.
    
    * Update dpo_trainer.py
    
    added type in the new num_proc variable
    
    * added test case
    
    * add test case
    
    * fix type
    
    ---------
    
    Co-authored-by: imraviagrawal <ravi.agrawal@umass.edu>
    Co-authored-by: Ravi Agrawal <raviagrawal@Ravis-MacBook-Pro.local>

--- tests/test_dpo_trainer.py
+++ tests/test_dpo_trainer.py
@@ -263,6 +263,42 @@ class DPOTrainerTester(unittest.TestCase):
 
                 trainer.train()
 
+    def test_dpo_trainer_w_dataset_num_proc(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            training_args = TrainingArguments(
+                output_dir=tmp_dir,
+                per_device_train_batch_size=2,
+                max_steps=3,
+                remove_unused_columns=False,
+                gradient_accumulation_steps=1,
+                learning_rate=9e-1,
+                evaluation_strategy="steps",
+            )
+
+            dummy_dataset = self._init_dummy_dataset()
+
+            tokenizer = AutoTokenizer.from_pretrained(self.model_id)
+            tokenizer.pad_token = None
+
+            with self.assertRaisesRegex(
+                ValueError,
+                expected_regex=r"Padding is enabled, but the tokenizer is not configured with a padding token."
+                r" Explicitly set `tokenizer.pad_token` \(e.g. `tokenizer.pad_token = tokenizer.eos_token`\)"
+                r" before calling the trainer.",
+            ):
+                trainer = DPOTrainer(
+                    model=self.model,
+                    ref_model=None,
+                    beta=0.1,
+                    args=training_args,
+                    tokenizer=tokenizer,
+                    train_dataset=dummy_dataset,
+                    eval_dataset=dummy_dataset,
+                    dataset_num_proc=5,
+                )
+
+                trainer.train()
+
     @require_no_wandb
     def test_dpo_trainer_generate_during_eval_no_wandb(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
