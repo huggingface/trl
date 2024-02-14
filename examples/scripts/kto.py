0bcfcbea84604e778998f1d56fb298b2429ce4c1
Kashif Rasul <kashif.rasul@gmail.com>
2024-02-14 13:38:36 +0100

    Update examples/scripts/kto.py
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>

--- examples/scripts/kto.py
+++ examples/scripts/kto.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Note: you need to install transformers from main to run this script. See https://huggingface.co/docs/transformers/installation#install-from-source
 
 # 0. imports
 from dataclasses import dataclass, field
