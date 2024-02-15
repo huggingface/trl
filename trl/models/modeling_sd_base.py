138bceaaf7b6bd2cdc2e982e5ee6c7644c9de880
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2024-02-15 04:45:27 +0100

    [`core` / `DDPO`] Fix diffusers import issue (#1314)
    * fix
    
    * more clean up

--- trl/models/modeling_sd_base.py
+++ trl/models/modeling_sd_base.py
@@ -22,10 +22,10 @@ import numpy as np
 import torch
 from diffusers import DDIMScheduler, StableDiffusionPipeline, UNet2DConditionModel
 from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion import rescale_noise_cfg
-from diffusers.utils import convert_state_dict_to_diffusers
 
 from ..core import randn_tensor
 from ..import_utils import is_peft_available
+from .sd_utils import convert_state_dict_to_diffusers
 
 
 if is_peft_available():
