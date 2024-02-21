6027f8a5af80ce093f95515bb848b0907f619b4c
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2024-02-21 09:29:43 +0000

    Fix import

--- trl/__init__.py
+++ trl/__init__.py
@@ -32,7 +32,6 @@ from .trainer import (
     SFTTrainer,
     SPINConfig,
     SPINTrainer,
-    TextGenerationCallback,
 )
 from .trainer.utils import get_kbit_device_map, get_peft_config, get_quantization_config
 
