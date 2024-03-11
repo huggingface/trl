64cd85f154d24efee6a13562f7c08260b538adbf
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2024-03-11 16:56:21 +0530

    revert

--- trl/trainer/sft_trainer.py
+++ trl/trainer/sft_trainer.py
@@ -188,7 +188,7 @@ class SFTTrainer(Trainer):
                         if param.__class__.__name__ == "Params4bit":
                             is_sharded_qlora = param.data.device.type == "cpu"
                             break
-                if getattr(model, "is_loaded_in_8bit", False) or (getattr(model, "is_loaded_in_4bit", False) and not is_sharded_qlora):
+                elif getattr(model, "is_loaded_in_8bit", False) or (getattr(model, "is_loaded_in_4bit", False) and not is_sharded_qlora):
                     preprare_model_kwargs = {
                         "use_gradient_checkpointing": getattr(args, "gradient_checkpointing", False)
                     }
