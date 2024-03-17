ef1cd23e93baff01dea236f26fe08567878ee535
Kashif Rasul <kashif.rasul@gmail.com>
2024-03-17 14:19:34 +0100

    fix typo in docstring

--- trl/trainer/kto_trainer.py
+++ trl/trainer/kto_trainer.py
@@ -1196,7 +1196,7 @@ class KTOTrainer(Trainer):
     @wraps(Trainer.push_to_hub)
     def push_to_hub(self, commit_message: Optional[str] = "End of training", blocking: bool = True, **kwargs) -> str:
         """
-        Overwrite the `push_to_hub` method in order to force-add the tag "sft" when pushing the
+        Overwrite the `push_to_hub` method in order to force-add the tag "kto" when pushing the
         model on the Hub. Please refer to `~transformers.Trainer.push_to_hub` for more details.
         """
         kwargs = trl_sanitze_kwargs_for_tagging(model=self.model, tag_names=self._tag_names, kwargs=kwargs)
