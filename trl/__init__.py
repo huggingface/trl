c5a7311b6d01509a0c4b7a3211d4e90447329eb8
Younes Belkada <younesbelkada@gmail.com>
2024-03-18 13:19:46 +0000

    fix doc build on main

--- trl/__init__.py
+++ trl/__init__.py
@@ -124,4 +124,10 @@ if TYPE_CHECKING:
 else:
     import sys
 
-    sys.modules[__name__] = _LazyModule(__name__, globals()["__file__"], _import_structure, module_spec=__spec__)
+    sys.modules[__name__] = _LazyModule(
+        __name__,
+        globals()["__file__"],
+        _import_structure,
+        module_spec=__spec__,
+        extra_objects={"__version__": __version__},
+    )
