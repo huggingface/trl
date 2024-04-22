85302ab0015d2606ad19ba7bea3ca61db181e485
Kashif Rasul <kashif.rasul@gmail.com>
2024-04-22 17:22:55 +0200

    add back options to SftScriptArguments

--- trl/commands/cli_utils.py
+++ trl/commands/cli_utils.py
@@ -142,6 +142,8 @@ def init_zero_verbose():
 @dataclass
 class SftScriptArguments:
     dataset_name: str = field(default="timdettmers/openassistant-guanaco", metadata={"help": "the dataset name"})
+    dataset_train_name: str = field(default="train", metadata={"help": "the name of the training set of the dataset"})
+    dataset_test_name: str = field(default="test", metadata={"help": "the name of the training set of the dataset"})
     config: str = field(default=None, metadata={"help": "Path to the optional config file"})
     gradient_checkpointing_use_reentrant: bool = field(
         default=False, metadata={"help": "Whether to apply `use_reentrant` for gradient_checkpointing"}
