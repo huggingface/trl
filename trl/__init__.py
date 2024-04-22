6d9254eaa34de5ff6edae2c6f770e4167fcf8d89
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2024-04-22 10:58:13 +0200

    Release: v0.8.6 (#1567)

--- trl/__init__.py
+++ trl/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 
-__version__ = "0.8.6.dev0"
+__version__ = "0.8.6"
 
 from typing import TYPE_CHECKING
 from .import_utils import _LazyModule, is_diffusers_available, OptionalDependencyNotAvailable
