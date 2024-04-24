38708c16177c09aac602f3e31b0824bb8db17378
Andrew Lapp <andrew@rew.la>
2024-04-24 01:33:27 -0500

    debug

--- trl/trainer/rloo_trainer.py
+++ trl/trainer/rloo_trainer.py
@@ -285,7 +285,10 @@ class RLOOTrainer(Trainer):
         model.train()
         inputs = self._prepare_inputs(inputs)
         queries = inputs["input_ids"].to(self.accelerator.device)
+        print(queries)
+        print(queies.shape)
         queries = queries.repeat(self.args.rloo_k, 1)
+        print("w/ repeats", queies.shape)
         context_length = queries.shape[0]
         query_responses = []
         responses = []
