62f8ec1c4ddb7f0b8269432978e80b326a5216b9
Andrew Lapp <andrew@rew.la>
2024-04-24 18:37:14 -0500

    fix ref model preparation

--- trl/trainer/rloo_trainer.py
+++ trl/trainer/rloo_trainer.py
@@ -37,6 +37,39 @@ if is_peft_available():
     from peft import PeftConfig, PeftModel, get_peft_model, prepare_model_for_kbit_training
 
 
+def _prepare_deepspeed(self, accelerator, model: PreTrainedModelWrapper, evaluation_mode):
+    # Adapted from accelerate: https://github.com/huggingface/accelerate/blob/739b135f8367becb67ffaada12fe76e3aa60fefd/src/accelerate/accelerator.py#L1473
+    deepspeed_plugin = accelerator.state.deepspeed_plugin
+    config_kwargs = deepcopy(deepspeed_plugin.deepspeed_config)
+
+    if model is not None:
+        if hasattr(model, "config"):
+            hidden_size = (
+                max(model.config.hidden_sizes)
+                if getattr(model.config, "hidden_sizes", None)
+                else getattr(model.config, "hidden_size", None)
+            )
+            if hidden_size is not None and config_kwargs["zero_optimization"]["stage"] == 3:
+                # Note that `stage3_prefetch_bucket_size` can produce DeepSpeed messages like: `Invalidate trace cache @ step 0: expected module 1, but got module 0`
+                # This is expected and is not an error, see: https://github.com/microsoft/DeepSpeed/discussions/4081
+                config_kwargs.update(
+                    {
+                        "zero_optimization.reduce_bucket_size": hidden_size * hidden_size,
+                        "zero_optimization.stage3_param_persistence_threshold": 10 * hidden_size,
+                        "zero_optimization.stage3_prefetch_bucket_size": 0.9 * hidden_size * hidden_size,
+                    }
+                )
+
+    # If ZeRO-3 is used, we shard both the active and reference model.
+    # Otherwise, we assume the reference model fits in memory and is initialized on each device with ZeRO disabled (stage 0)
+    if config_kwargs["zero_optimization"]["stage"] != 3:
+        config_kwargs["zero_optimization"]["stage"] = 0
+    model, *_ = deepspeed.initialize(model=model, config=config_kwargs)
+    if evaluation_mode:
+        model.eval()
+    return model
+
+
 class ReferenceModelManager:
     def __init__(
             self,
@@ -61,6 +94,23 @@ class ReferenceModelManager:
                 f"ref_model must be a PreTrainedModelWrapper or `None` "
                 f"got {type(ref_model)} "
                 f"- supported architectures are: {SUPPORTED_ARCHITECTURES} "
+            )
+
+        self._prepare_multigpu()
+
+    def _prepare_multigpu(self):
+        if self.ref_model is None:
+            return
+        elif self.is_deepspeed_enabled:
+            self.ref_model = _prepare_deepspeed(
+                self.accelerator,
+                self.ref_model,
+                evaluation_mode=True
+            )
+        else:
+            self.ref_model = self.accelerator.prepare_model(
+                self.ref_model,
+                evaluation_mode=True
             )
 
     def __enter__(self):
@@ -239,7 +289,6 @@ class RLOOTrainer(Trainer):
             **kwargs,
         )
 
-        ref_model = self._prepare_multigpu(ref_model)
         self.ref_model_mgr = ReferenceModelManager(
             self.accelerator,
             ref_model=ref_model,
@@ -259,49 +308,6 @@ class RLOOTrainer(Trainer):
         if self.reward_model is not None:
             disable_dropout(self.reward_model)
 
-
-    def _prepare_multigpu(self, model):
-        # Deepspeed Zero-3 does not support precompute_ref_log_probs
-        if model is None:
-            return model
-        elif self.is_deepspeed_enabled:
-            return self._prepare_deepspeed(model)
-        else:
-            return self.accelerator.prepare_model(
-                model,
-                evaluation_mode=True
-            )
-
-    def _prepare_deepspeed(self, model: PreTrainedModelWrapper):
-        # Adapted from accelerate: https://github.com/huggingface/accelerate/blob/739b135f8367becb67ffaada12fe76e3aa60fefd/src/accelerate/accelerator.py#L1473
-        deepspeed_plugin = self.accelerator.state.deepspeed_plugin
-        config_kwargs = deepcopy(deepspeed_plugin.deepspeed_config)
-
-        if model is not None:
-            if hasattr(model, "config"):
-                hidden_size = (
-                    max(model.config.hidden_sizes)
-                    if getattr(model.config, "hidden_sizes", None)
-                    else getattr(model.config, "hidden_size", None)
-                )
-                if hidden_size is not None and config_kwargs["zero_optimization"]["stage"] == 3:
-                    # Note that `stage3_prefetch_bucket_size` can produce DeepSpeed messages like: `Invalidate trace cache @ step 0: expected module 1, but got module 0`
-                    # This is expected and is not an error, see: https://github.com/microsoft/DeepSpeed/discussions/4081
-                    config_kwargs.update(
-                        {
-                            "zero_optimization.reduce_bucket_size": hidden_size * hidden_size,
-                            "zero_optimization.stage3_param_persistence_threshold": 10 * hidden_size,
-                            "zero_optimization.stage3_prefetch_bucket_size": 0.9 * hidden_size * hidden_size,
-                        }
-                    )
-
-        # If ZeRO-3 is used, we shard both the active and reference model.
-        # Otherwise, we assume the reference model fits in memory and is initialized on each device with ZeRO disabled (stage 0)
-        if config_kwargs["zero_optimization"]["stage"] != 3:
-            config_kwargs["zero_optimization"]["stage"] = 0
-        model, *_ = deepspeed.initialize(model=model, config=config_kwargs)
-        model.eval()
-        return model
 
     def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
         """
