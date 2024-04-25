663dc39b0ef668cb79fc824230bacc85035e0eec
Andrew Lapp <andrew@rew.la>
2024-04-25 00:44:35 -0500

    debug log

--- trl/trainer/rloo_trainer.py
+++ trl/trainer/rloo_trainer.py
@@ -228,10 +228,16 @@ class RLOOTrainer(PolicyTrainerBase):
                 new_logprobs, padding_mask, INVALID_LOGPROB
             )
             new_ratio = (new_logprobs - logprobs).exp()
+            print("new_ratio.shape", new_ratio.shape)
             new_logprobs = new_logprobs.sum(1)
+            print("new_logprobs.shape", new_logprobs.shape)
+            print("logprobs.shape 0", logprobs.shape)
             logprobs = logprobs.sum(1)
+            print("logprobs.shape 1", logprobs.shape)
             logprobs_diff = new_logprobs - logprobs
+            print("logprobs_diff.shape", logprobs_diff.shape)
             ratio = torch.exp(logprobs_diff)
+            print("ratio.shape", ratio.shape)
             # print(f"{ratio=}")
             print("advantages.shape", advantages.shape)
             print("ratio.shape", ratio.shape)
