678b21bed2fd31afab20cba411de735465a20c1d
Andrew Lapp <andrew@rew.la>
2024-04-24 21:25:15 -0500

    fix syntax

--- trl/trainer/rloo_trainer.py
+++ trl/trainer/rloo_trainer.py
@@ -136,7 +136,7 @@ class RLOOTrainer(PolicyTrainerBase):
         model.train()
 
         inputs = self._prepare_inputs(inputs)
-1        queries = inputs["input_ids"].to(self.accelerator.device)
+        queries = inputs["input_ids"].to(self.accelerator.device)
         print("queries.shape 0", queries.shape)
         queries = queries.repeat(self.args.rloo_k, 1)
         print("queries.shape 1", queries.shape)
