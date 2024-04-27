bug fix: I was always calling no_grad, even when running forward pass for loss calculation in model

trl/trainer/policy_trainer_base.py
@@ -341,7 +341,7 @@ class PolicyTrainerBase(Trainer):
         attention_mask = query_responses != self.tokenizer.pad_token_id
         # position_ids = attention_mask.cumsum(1) - attention_mask.long()
         input_ids = torch.masked_fill(query_responses, ~attention_mask, 0)
-        with eval_mode(model), torch.no_grad():
+        with eval_mode(model):
             return model(
                 input_ids=input_ids,
                 attention_mask=attention_mask,