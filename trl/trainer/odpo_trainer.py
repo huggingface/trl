cbdaa682360a696c7e012b5729c767c8ab491b9d
edbeeching <edbeeching@aol.com>
2024-04-30 08:52:51 +0000

    testing multi gpu

--- trl/trainer/odpo_trainer.py
+++ trl/trainer/odpo_trainer.py
@@ -127,28 +127,6 @@ class OnlineDPOTrainer(DPOTrainer):
             annotation_batch["prompts"].append(prompt)
             annotation_batch["completions"].append([response0, response1])
         results = self.annotator.judge_batch(annotation_batch["prompts"], annotation_batch["completions"])
-        # TODO: Make this is a bit cleaner
-        # if type(self.annotator) is FutureAnnotator:
-        #     # annotate the responses with the GPT3.5 annotator
-        #     results = self.annotator.judge_batch(annotation_batch["prompts"], annotation_batch["completions"])
-
-        # elif type(self.annotator) is PairRMAnnotator:
-        # convs_a = []
-        # convs_b = []
-        # for i in range(self._train_batch_size):
-        #     response0 = inputs["messages"][i][:-1]
-        #     response1 = inputs["messages"][i][:-1]
-        #     response0.append({"role": "assistant", "content": text_generations[i * 2]})
-        #     response1.append({"role": "assistant", "content": text_generations[i * 2 + 1]})
-        #     if response0[0]["role"] == "system":  #  PAIRRM is not compatible with system prompts
-        #         response0 = response0[1:]
-        #         response1 = response1[1:]
-
-        #     convs_a.append(response0)
-        #     convs_b.append(response1)
-        # results = self.annotator.judge_batch(convs_a, convs_b)
-        # else:
-        #     raise ValueError("Unknown Annotator", self.annotator)
 
         optimization_batch = []
         for chosen_index, prompt, completion in zip(
@@ -158,9 +136,7 @@ class OnlineDPOTrainer(DPOTrainer):
             rejected = completion[1 - chosen_index]
 
             sample = {
-                "prompt": prompt.removeprefix(
-                    self.tokenizer.bos_token
-                ),  # the BOS token was added again in the tokenize_row method
+                "prompt": prompt,
                 "chosen": chosen,
                 "rejected": rejected,
             }
