c3ef2560bf7c96f5d448947b74cdcbe2be711540
Andrew Lapp <andrew@rew.la>
2024-05-02 11:45:53 -0500

    add breakpoint

--- trl/trainer/rloo_trainer.py
+++ trl/trainer/rloo_trainer.py
@@ -201,6 +201,7 @@ class RLOOTrainer(PolicyTrainerBase):
                 print("Gradient Calc - pg_losses Min:", pg_losses.min().item(), "Max:", pg_losses.max().item(), "Contains NaN or Inf:", torch.isnan(pg_losses).any().item() or torch.isinf(pg_losses).any().item())
                 print("Gradient Calc - pg_loss Value:", pg_loss.item(), "Contains NaN or Inf:", torch.isnan(pg_loss).any().item() or torch.isinf(pg_loss).any().item())
 
+                import pdb;pdb.set_trace()
 
             # log metrics
             with torch.no_grad():
