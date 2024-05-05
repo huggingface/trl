a909ba8bf7b27b36e2eccce083cc62907c318b48
Michael Noukhovitch <mnoukhov@gmail.com>
2024-05-05 06:11:47 +0000

    fix preferred not in completion

--- examples/summarization/generate_and_llm_judge.py
+++ examples/summarization/generate_and_llm_judge.py
@@ -260,7 +260,10 @@ def llm_as_a_judge(args, prompts, reference, generations, model_name=None):
         comparisons, preferred = [], []
         for llm_judge_completion in llm_judge_texts:
             comparisons.append(llm_judge_completion.split("Comparison:")[1].split("Preferred:")[0].strip())
-            preferred.append(llm_judge_completion.split("Preferred:")[1].strip())
+            if "Preferred:" in llm_judge_completion:
+                preferred.append(llm_judge_completion.split("Preferred:")[1].strip())
+            else:
+                preferred.append("X")
 
         full_convo = [prompt + text for prompt, text in zip(llm_judge_prompts, llm_judge_texts)]
 
