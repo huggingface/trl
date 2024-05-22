13454d2f4b243b7260fa4ec828297812c3f975fc
Costa Huang <costa.huang@outlook.com>
2024-05-22 08:31:10 -0400

    PPO / Reinforce Trainers (#1540)
    * Add ppov2 trainer
    
    * make eos trick optional, remove unused args
    
    * quick fix
    
    * precommit
    
    * update debugging script
    
    * fix out of bound `drop_last=True`; use built-in scheduler
    
    * Add PPO examples
    
    * push changes
    
    * quick change
    
    * quick change
    
    * various bug fixes
    
    * remove unnecessary grad accumulation setting
    
    * push new changes
    
    * fix DS3 model saving
    
    * update ppo.py
    
    * refactor
    
    * quick change
    
    * refactor
    
    * update ppo trainer
    
    * refactor
    
    * quick test
    
    * add ds2 /ds3 7 processes config
    
    * add vllm trainer
    
    * quick change
    
    * experiment with reward normalization
    
    * push changes
    
    * quick push
    
    * push changes
    
    * push various changes
    
    * refactor to use ModelConfig
    
    * quick change
    
    * refactor
    
    * refactor
    
    * Simplify DS logic
    
    * quick update
    
    * remove unnecessary files
    
    * precommit
    
    * deepspeed fix; handle edge case when eos_token_id = 0
    
    * add PPO tldr example
    
    * add TL;DR example
    
    * fix undefined var
    
    * utilize all samples in rloo
    
    * quick setting
    
    * remove the unnecessary `value_model`
    
    * use exact_div
    
    * allow saving the deepspeed model
    
    * refactor
    
    * remove dead code
    
    * Use some shared utilities
    
    * add some end-to-end test cases
    
    * add PPOv2 docs and RLOO docs / tests
    
    * update docs
    
    * quikc push
    
    * fix ci
    
    * fix type annotation for ci
    
    * quick update
    
    * update trainer docs

--- examples/datasets/tldr_preference.py
+++ examples/datasets/tldr_preference.py
@@ -27,6 +27,9 @@ class ScriptArguments:
     hf_repo_id: Optional[str] = field(
         default="tldr-preference-trl-style", metadata={"help": "The Hugging Face repository ID"}
     )
+    sft_hf_repo_id: Optional[str] = field(
+        default="tldr-preference-sft-trl-style", metadata={"help": "The Hugging Face repository ID"}
+    )
     revision: Optional[str] = field(default="0.1.0", metadata={"help": "The revision of the repository"})
     update_main_revision: Optional[bool] = field(
         default=True, metadata={"help": "Update the main revision of the repository"}
@@ -39,7 +42,11 @@ if __name__ == "__main__":
     if args.hf_entity is None:
         args.hf_entity = api.whoami()["name"]
     full_repo_id = f"{args.hf_entity}/{args.hf_repo_id}"
+    full_sft_repo_id = f"{args.hf_entity}/{args.sft_hf_repo_id}"
 
+    ################
+    # Preference dataset
+    ################
     ds = load_dataset("openai/summarize_from_feedback", "comparisons")
     if args.debug:
         for key in ds:
@@ -92,22 +99,88 @@ if __name__ == "__main__":
                 repo_type="dataset",
             )
 
-        sft_card = RepoCard.load(
+        preference_card = RepoCard.load(
             full_repo_id,
             repo_type="dataset",
         )
-        sft_card.text = f"""\
+        preference_card.text = f"""\
 # TRL's TL;DR Preference Dataset
 
 We preprocess the dataset using our standard `prompt, chosen, rejected` format.
 
+## Source of the dataset
+
+We take the dataset from https://huggingface.co/datasets/openai/summarize_from_feedback.
 
 ## Reproduce this dataset
 
 1. Download the `{file_name}` from the {repo_full_url}.
 2. Run `{run_command}`
 """
-        sft_card.push_to_hub(
+        preference_card.push_to_hub(
             full_repo_id,
             repo_type="dataset",
         )
+
+    ################
+    # SFT dataset
+    ################
+    sft_ds = load_dataset("vwxyzjn/summarize_from_feedback_tldr_3_filtered")
+    if args.debug:
+        for key in sft_ds:
+            sft_ds[key] = sft_ds[key].select(range(50))
+
+    def sft_process(row):
+        row["prompt"] = tldr_format_str.format(**row)
+        row["messages"] = [
+            {"role": "user", "content": row["prompt"]},
+            {"role": "assistant", "content": row["summary"]},
+        ]
+        return row
+
+    sft_ds = sft_ds.map(
+        sft_process,
+        num_proc=1 if args.debug else multiprocessing.cpu_count(),
+        load_from_cache_file=False,
+    )
+    for key in sft_ds:  # reorder columns
+        sft_ds[key] = sft_ds[key].select_columns(["prompt", "messages", "id", "subreddit", "title", "post", "summary"])
+    if args.push_to_hub:
+        revisions = ["main"] if args.update_main_revision else []
+        revisions.append(args.revision)
+
+        # get the commnad used to run the script
+        run_command = " ".join(["python"] + sys.argv)
+
+        for revision in revisions:
+            sft_ds.push_to_hub(full_sft_repo_id, revision=revision)
+            repo_full_url = f"https://huggingface.co/datasets/{full_sft_repo_id}/tree/{revision}"
+
+            # get the name of the current file
+            file_name = __file__.split("/")[-1]
+            api.upload_file(
+                path_or_fileobj=__file__,
+                path_in_repo=file_name,
+                revision=revision,
+                repo_id=full_sft_repo_id,
+                repo_type="dataset",
+            )
+
+        sft_card = RepoCard.load(
+            full_sft_repo_id,
+            repo_type="dataset",
+        )
+        sft_card.text = f"""\
+# TRL's TL;DR SFT Dataset
+
+We preprocess the dataset using our standard `prompt, messages` format.
+
+## Source of the dataset
+
+We take the dataset from https://huggingface.co/datasets/vwxyzjn/summarize_from_feedback_tldr_3_filtered.
+
+## Reproduce this dataset
+
+1. Download the `{file_name}` from the {repo_full_url}.
+2. Run `{run_command}`
+"""
