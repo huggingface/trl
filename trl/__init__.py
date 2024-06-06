3eb9ccb104e2c46360adb937f3f25871c167eb90
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2024-06-06 19:33:20 +0200

    set dev version (#1710)
    * Update setup.py
    
    * Update __init__.py

--- trl/__init__.py
+++ trl/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 
-__version__ = "0.9.4"
+__version__ = "0.9.5.dev0"
 
 from typing import TYPE_CHECKING
 from .import_utils import _LazyModule, is_diffusers_available, OptionalDependencyNotAvailable
