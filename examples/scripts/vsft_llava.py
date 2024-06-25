d6dc3ba7e91f97f7a5e63c2293bf173e5efd9ce0
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-06-25 13:43:58 +0000

    revert vsft change (oos)

--- examples/scripts/vsft_llava.py
+++ examples/scripts/vsft_llava.py
@@ -18,9 +18,9 @@ python examples/scripts/vsft_llava.py \
     --dataset_name="HuggingFaceH4/llava-instruct-mix-vsft" \
     --model_name_or_path="llava-hf/llava-1.5-7b-hf" \
     --report_to="wandb" \
-    --learning_rate=1.4e-7 \
-    --per_device_train_batch_size=2 \
-    --gradient_accumulation_steps=32 \
+    --learning_rate=1.4e-5 \
+    --per_device_train_batch_size=8 \
+    --gradient_accumulation_steps=1 \
     --output_dir="data/vsft-llava-1.5-7b-hf" \
     --logging_steps=5 \
     --num_train_epochs=1 \
@@ -28,15 +28,16 @@ python examples/scripts/vsft_llava.py \
     --gradient_checkpointing \
     --remove_unused_columns=False \
     --torch_dtype=float16 \
+    --fp16=True
     
 # peft:
 python examples/scripts/vsft_llava.py \
-    --dataset_name="HuggingFaceH4/llava-instruct-mix" \
+    --dataset_name="HuggingFaceH4/llava-instruct-mix-vsft" \    
     --model_name_or_path="llava-hf/llava-1.5-7b-hf" \
     --report_to="wandb" \
     --learning_rate=1.4e-5 \
     --per_device_train_batch_size=8 \
-    --gradient_accumulation_steps=128 \
+    --gradient_accumulation_steps=1 \
     --output_dir="data/vsft-llava-1.5-7b-hf" \
     --logging_steps=5 \
     --num_train_epochs=1 \
@@ -44,10 +45,11 @@ python examples/scripts/vsft_llava.py \
     --gradient_checkpointing \
     --remove_unused_columns=False \
     --torch_dtype=float16 \
+    --fp16=True \ 
     --use_peft=True \
     --lora_r=64 \
     --lora_alpha=16 \
-    --lora_target_modules=all-linear
+    --lora_target_modules=all-linear"
 
 # evaluation:
  
