59b6ef309107932e744f6fdf00bf744aafb7ba19
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-07-17 16:44:13 +0000

    remove openai and pairrm judge from test

--- tests/test_judges.py
+++ tests/test_judges.py
@@ -1,6 +1,6 @@
 import unittest
 
-from trl import HuggingFaceJudge, MockAPIJudge, MockJudge, OpenAIJudge, PairRMJudge
+from trl import HuggingFaceJudge, MockAPIJudge, MockJudge
 
 
 class TestJudges(unittest.TestCase):
@@ -8,14 +8,6 @@ class TestJudges(unittest.TestCase):
         prompts = ["What is the capital of France?", "What is the biggest planet in the solar system?"]
         completion_pairs = [["Paris", "Marseille"], ["Saturn", "Jupiter"]]
         return prompts, completion_pairs
-
-    def test_pair_rm_judge(self):
-        judge = PairRMJudge()
-        prompts, completion_pairs = self._get_prompts_and_completion_pairs()
-        ranks = judge.judge(prompts=prompts, completion_pairs=completion_pairs)
-        self.assertEqual(len(ranks), 2)
-        self.assertTrue(all(isinstance(rank, int) for rank in ranks))
-        self.assertEqual(ranks, [0, 1])
 
     def test_mock_judge(self):
         judge = MockJudge()
@@ -33,14 +25,6 @@ class TestJudges(unittest.TestCase):
 
     def test_hugging_face_judge(self):
         judge = HuggingFaceJudge()
-        prompts, completion_pairs = self._get_prompts_and_completion_pairs()
-        ranks = judge.judge(prompts=prompts, completion_pairs=completion_pairs)
-        self.assertEqual(len(ranks), 2)
-        self.assertTrue(all(isinstance(rank, int) for rank in ranks))
-        self.assertEqual(ranks, [0, 1])
-
-    def test_open_ai_judge(self):
-        judge = OpenAIJudge()
         prompts, completion_pairs = self._get_prompts_and_completion_pairs()
         ranks = judge.judge(prompts=prompts, completion_pairs=completion_pairs)
         self.assertEqual(len(ranks), 2)
