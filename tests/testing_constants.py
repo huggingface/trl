1f5f9f02ea1fc50e86252b46c40336d4f230e523
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-07-18 13:23:36 +0000

    rm token

--- tests/testing_constants.py
+++ tests/testing_constants.py
@@ -14,6 +14,5 @@
 
 CI_HUB_USER = "__DUMMY_TRANSFORMERS_USER__"
 CI_HUB_USER_FULL_NAME = "Dummy User"
-CI_HUB_USER_TOKEN = "hf_94wBhPGp6KrrTH3KDchhKpRxZwd6dmHWLL"
 
 CI_HUB_ENDPOINT = "https://hub-ci.huggingface.co"
