9e892dd1c0015c1e8cf633c35afbfbb547bbdddc
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-07-23 19:06:27 +0000

    fix vsft example commands

--- examples/scripts/vsft_llava.py
+++ examples/scripts/vsft_llava.py
@@ -13,55 +13,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-# regular:
 python examples/scripts/vsft_llava.py \
-    --dataset_name="HuggingFaceH4/llava-instruct-mix-vsft" \
-    --model_name_or_path="llava-hf/llava-1.5-7b-hf" \
-    --report_to="wandb" \
-    --learning_rate=1.4e-5 \
-    --per_device_train_batch_size=8 \
-    --gradient_accumulation_steps=1 \
-    --output_dir="data/vsft-llava-1.5-7b-hf" \
-    --logging_steps=5 \
-    --num_train_epochs=1 \
-    --push_to_hub \
-    --gradient_checkpointing \
+    --dataset_name HuggingFaceH4/llava-instruct-mix-vsft \
+    --model_name_or_path llava-hf/llava-1.5-7b-hf \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 8 \
+    --output_dir sft-llava-1.5-7b-hf \
     --remove_unused_columns=False \
-    --torch_dtype=float16 \
-    --fp16=True
-    
-# peft:
-python examples/scripts/vsft_llava.py \
-    --dataset_name="HuggingFaceH4/llava-instruct-mix-vsft" \    
-    --model_name_or_path="llava-hf/llava-1.5-7b-hf" \
-    --report_to="wandb" \
-    --learning_rate=1.4e-5 \
-    --per_device_train_batch_size=8 \
-    --gradient_accumulation_steps=1 \
-    --output_dir="data/vsft-llava-1.5-7b-hf" \
-    --logging_steps=5 \
-    --num_train_epochs=1 \
-    --push_to_hub \
+    --bf16 \
+    --torch_dtype bfloat16 \
     --gradient_checkpointing \
-    --remove_unused_columns=False \
-    --torch_dtype=float16 \
-    --fp16=True \ 
-    --use_peft=True \
-    --lora_r=64 \
-    --lora_alpha=16 \
-    --lora_target_modules=all-linear"
+    --use_peft \
+    --dataloader_num_workers 32 \
+    --lora_target_modules=all-linear
 
-# evaluation:
- 
+## Evaluation:
+
 To evaluate, first install the lmms-eval framework: pip install git+https://github.com/EvolvingLMMs-Lab/lmms-eval.git
 then run:
 accelerate launch --num_processes=8 -m lmms_eval \
-        --model llava_hf \
-        --model_args pretrained=llava-hf/llava-1.5-7b-hf \
-        --tasks mmbench \
-        --batch_size 1 \
-        --output_path ./logs/ \
-        --log_sample    
+    --model llava_hf \
+    --model_args pretrained llava-hf/llava-1.5-7b-hf \
+    --tasks mmbench \
+    --batch_size 1 \
+    --output_path ./logs/ \
+    --log_sample
 """
 
 import logging
@@ -127,6 +103,7 @@ if __name__ == "__main__":
         revision=model_config.model_revision,
         attn_implementation=model_config.attn_implementation,
         torch_dtype=torch_dtype,
+        use_cache=False if training_args.gradient_checkpointing else True,
         device_map=get_kbit_device_map() if quantization_config is not None else None,
         quantization_config=quantization_config,
     )
