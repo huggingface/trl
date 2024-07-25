1dae55f90f6e929500df4fc4ee5bbc0146e35574
elie <97572401+eliebak@users.noreply.github.com>
2024-07-25 10:27:34 +0200

    add fsdp_qlora config and bnb_4bit_quant_storage (#1863)

--- trl/trainer/utils.py
+++ trl/trainer/utils.py
@@ -754,6 +754,7 @@ def get_quantization_config(model_config: ModelConfig) -> Optional[BitsAndBytesC
             bnb_4bit_compute_dtype=model_config.torch_dtype,  # For consistency with model weights, we use the same value as `torch_dtype`
             bnb_4bit_quant_type=model_config.bnb_4bit_quant_type,
             bnb_4bit_use_double_quant=model_config.use_bnb_nested_quant,
+            bnb_4bit_quant_storage=model_config.torch_dtype,
         )
     elif model_config.load_in_8bit:
         quantization_config = BitsAndBytesConfig(
