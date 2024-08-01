d858b2876836373e56a5c39794fc273cf53f713e
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-08-01 10:12:06 +0000

    revert change

--- trl/trainer/rloo_trainer.py
+++ trl/trainer/rloo_trainer.py
@@ -436,8 +436,8 @@ class RLOOTrainer(Trainer):
                 metrics["lr"] = self.lr_scheduler.get_last_lr()[0]
                 metrics["episode"] = self.state.episode
                 self.state.epoch = self.state.episode / self.train_dataset_len  # used by self.log
-                self.log(metrics)
                 self.state.global_step += 1
+                self.log(metrics)
             del kl, mean_kl, mean_entropy, scores
 
             self.control = self.callback_handler.on_step_end(args, self.state, self.control)
