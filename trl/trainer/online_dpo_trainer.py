4020f41f1aee180ad71978cb17eb17e1e5029c75
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-08-01 10:13:54 +0000

    policy->model

--- trl/trainer/online_dpo_trainer.py
+++ trl/trainer/online_dpo_trainer.py
@@ -191,12 +191,10 @@ class OnlineDPOTrainer(Trainer):
                 self.reward_model = prepare_deepspeed(
                     self.reward_model, args.per_device_train_batch_size, args.fp16, args.bf16
                 )
-            self.ref_policy = prepare_deepspeed(
-                self.ref_policy, args.per_device_train_batch_size, args.fp16, args.bf16
-            )
+            self.ref_model = prepare_deepspeed(self.ref_model, args.per_device_train_batch_size, args.fp16, args.bf16)
             self.deepspeed = self.model
         else:
-            self.ref_policy = self.ref_policy.to(self.accelerator.device)
+            self.ref_model = self.ref_model.to(self.accelerator.device)
             if reward_model is not None:
                 self.reward_model = self.reward_model.to(self.accelerator.device)
 
