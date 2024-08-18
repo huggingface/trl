1bde84109c1131940a4df729223ee66bdf6ba47c
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-08-18 14:58:45 +0000

    update simpo config

--- trl/trainer/simpo_config.py
+++ trl/trainer/simpo_config.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from dataclasses import dataclass
-from typing import Dict, Literal, Optional
+from typing import Any, Dict, Literal, Optional
 
 from transformers import TrainingArguments
 
@@ -27,58 +27,63 @@ class SimPOConfig(TrainingArguments):
     command line.
 
     Parameters:
-        max_length (`int`, defaults to `None`):
-            The maximum length of the sequences in the batch. This argument is required if you want to use the default data collator.
-        max_prompt_length (`int`, defaults to `None`):
-            The maximum length of the prompt. This argument is required if you want to use the default data collator.
-        max_target_length (`int`, defaults to `None`):
-            The maximum length of the target. This argument is required if you want to use the default data collator and your model is an encoder-decoder.
-        beta (`float`, defaults to 2.0):
-            The beta factor in SimPO loss.
-        gamma_beta_ratio (`float`, defaults to 0.25):
-            The ratio between the target reward margin (gamma) and beta in SimPO loss.
-        sft_weight (`float`, defaults to 0.0):
+        beta (`float`, *optional*, defaults to `2.0`):
+            Beta factor in SimPO loss.
+        gamma_beta_ratio (`float`, *optional*, defaults to `0.25`):
+            Ratio between the target reward margin (gamma) and beta in SimPO loss.
+        sft_weight (`float`, *optional*, defaults to `0.0`):
             SFT loss weight added to the SimPO loss (0.0 is not using SFT).
-        label_smoothing (`float`, defaults to 0):
-            The label smoothing factor. This argument is required if you want to use the default data collator.
-        loss_type (`str`, defaults to `sigmoid`):
-            The type of loss to use. This argument is required if you want to use the default data collator.
-        label_pad_token_id (`int`, defaults to `-100`):
-            The label pad token id. This argument is required if you want to use the default data collator.
-        padding_value (`int`, defaults to `None`):
-            The padding value if it is different to the tokenizer's pad_token_id.
-        truncation_mode (`str`, defaults to `keep_end`):
-            The truncation mode to use, either `keep_end` or `keep_start`. This argument is required if you want to use the default data collator.
-        generate_during_eval (`bool`, defaults to `False`):
-            Whether to sample and log generations during evaluation step.
-        is_encoder_decoder (`Optional[bool]`, `optional`, defaults to `None`):
-            If no model is provided, we need to know if the model_init returns an encoder-decoder.
-        disable_dropout (`bool`, defaults to `True`):
-            Whether or not to disable dropouts in `model`.
-        model_init_kwargs (`Optional[Dict]`, *optional*):
-            Dict of Optional kwargs to pass when instantiating the model from a string
-        dataset_num_proc (`Optional[int]`, *optional*):
-            The number of workers to use to tokenize the data. Defaults to None.
-    """
+        label_smoothing (`float`, *optional*, defaults to `0.0`):
+            Label smoothing factor.
+        loss_type (`str`, *optional*, defaults to `"sigmoid"`):
+            Type of loss to use. Possible values are:
 
-    max_length: Optional[int] = None
-    max_prompt_length: Optional[int] = None
-    max_completion_length: Optional[int] = None
-    max_target_length: Optional[int] = None
+                - `"sigmoid"`: sigmoid loss from the original [DPO](https://huggingface.co/papers/2305.18290) paper.
+                - `"hinge"`: hinge loss on the normalized likelihood from the [SLiC](https://huggingface.co/papers/2305.10425) paper.
+
+        label_pad_token_id (`int`, *optional*, defaults to `-100`):
+            Label pad token id. This argument is required if you want to use the default data collator.
+        padding_value (`Optional[int]`, *optional*, defaults to `None`):
+            Padding value if it is different to the tokenizer's pad_token_id.
+        truncation_mode (`str`, *optional*, defaults to `"keep_end"`):
+            Truncation mode to use, either `"keep_end"` or `"keep_start"`. This argument is required if you want to use the default data collator.
+        max_length (`Optional[int]`, *optional*, defaults to `None`):
+            Maximum length of the sequences in the batch. This argument is required if you want to use the default
+            data collator.
+        max_prompt_length (`Optional[int]`, *optional*, defaults to `None`):
+            Maximum length of the prompt. This argument is required if you want to use the default data collator.
+        max_completion_length (`Optional[int]`, *optional*, defaults to `None`):
+            Maximum length of the target. This argument is required if you want to use the default data collator
+            and your model is an encoder-decoder.
+        max_target_length (`Optional[int]`, *optional*, defaults to `None`):
+            Maximum length of the target. This argument is required if you want to use the default data collator
+            and your model is an encoder-decoder.
+        is_encoder_decoder (`Optional[bool]`, *optional*, defaults to `None`):
+            If no model is provided, we need to know if the model_init returns an encoder-decoder.
+        disable_dropout (`bool`, *optional*, defaults to `True`):
+            Whether or not to disable dropouts in `model`.
+        generate_during_eval (`bool`, *optional*, defaults to `False`):
+            Whether to sample and log generations during evaluation step.
+        dataset_num_proc (`Optional[int]`, *optional*, defaults to `None`):
+            Number of processes to use for processing the datasets.
+        model_init_kwargs (`Optional[Dict[str, Any]]`, *optional*, defaults to `None`):
+            Dict of optional kwargs to pass when instantiating the model from a string
+    """
 
     beta: float = 2.0
     gamma_beta_ratio: float = 0.25
     sft_weight: float = 0.0
-    label_smoothing: float = 0
+    label_smoothing: float = 0.0
     loss_type: Literal["sigmoid", "hinge"] = "sigmoid"
-    disable_dropout: bool = True
-
     label_pad_token_id: int = -100
-    padding_value: int = None
+    padding_value: Optional[int] = None
     truncation_mode: str = "keep_end"
-    generate_during_eval: bool = False
+    max_length: Optional[int] = None
+    max_prompt_length: Optional[int] = None
+    max_completion_length: Optional[int] = None
+    max_target_length: Optional[int] = None
     is_encoder_decoder: Optional[bool] = None
-
-    model_init_kwargs: Optional[Dict] = None
-
+    disable_dropout: bool = True
+    generate_during_eval: bool = False
     dataset_num_proc: Optional[int] = None
+    model_init_kwargs: Optional[Dict[str, Any]] = None
