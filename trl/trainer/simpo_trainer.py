88be87386fb37a301126372e42ae1922cdacdf6d
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-08-18 16:22:53 +0000

    simplify

--- trl/trainer/simpo_trainer.py
+++ trl/trainer/simpo_trainer.py
@@ -651,23 +651,6 @@ class SimPOTrainer(Trainer):
             label_pad_token_id=self.label_pad_token_id,
         )
 
-        def cross_entropy_loss(logits, labels):
-            if not self.is_encoder_decoder:
-                # Shift so that tokens < n predict n
-                logits = logits[..., :-1, :].contiguous()
-                labels = labels[..., 1:].contiguous()
-            # Flatten the tokens
-            loss_fct = nn.CrossEntropyLoss()
-            logits = logits.view(-1, logits.shape[-1])
-            labels = labels.view(-1)
-            # Enable model parallelism
-            labels = labels.to(logits.device)
-            loss = loss_fct(logits, labels)
-            return loss
-
-        labels = concatenated_batch["concatenated_labels"].clone()
-        nll_loss = cross_entropy_loss(all_logits[:len_chosen], labels[:len_chosen])
-
         chosen_logps = all_logps[:len_chosen]
         rejected_logps = all_logps[len_chosen:]
 
@@ -677,17 +660,9 @@ class SimPOTrainer(Trainer):
         chosen_labels = concatenated_batch["concatenated_labels"][:len_chosen]
 
         if self.aux_loss_enabled:
-            return (
-                chosen_logps,
-                rejected_logps,
-                chosen_logits,
-                rejected_logits,
-                chosen_labels,
-                nll_loss,
-                outputs.aux_loss,
-            )
+            return (chosen_logps, rejected_logps, chosen_logits, rejected_logits, chosen_labels, outputs.aux_loss)
 
-        return (chosen_logps, rejected_logps, chosen_logits, rejected_logits, chosen_labels, nll_loss)
+        return (chosen_logps, rejected_logps, chosen_logits, rejected_logits, chosen_labels)
 
     @staticmethod
     def get_batch_logps(
@@ -741,7 +716,7 @@ class SimPOTrainer(Trainer):
             chosen_labels,
         ) = forward_output[:5]
         if self.aux_loss_enabled:
-            aux_loss = forward_output[6]
+            aux_loss = forward_output[5]
 
         losses, chosen_rewards, rejected_rewards = self.simpo_loss(
             policy_chosen_logps,
