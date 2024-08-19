4694d5e0051dab2fc58ff4b90f3a24847508f2c8
Kashif Rasul <kashif.rasul@gmail.com>
2024-08-19 20:02:36 +0200

    Update examples/scripts/gkd.py
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>

--- examples/scripts/gkd.py
+++ examples/scripts/gkd.py
@@ -36,7 +36,7 @@ python examples/scripts/gkd.py \
     --dataset_name="andito/chatbot_arena_completions" \
     --report_to="wandb" \
     --learning_rate=2e-4 \
-    --per_device_train_batch_size=64 \
+    --per_device_train_batch_size=16 \
     --gradient_accumulation_steps=2 \
     --output_dir="gkd-model" \
     --logging_steps=1 \
