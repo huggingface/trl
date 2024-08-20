dbbf268243b6dad3d781b1adec7eaaba76fb0e2d
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-08-20 08:26:45 +0000

    don't mark issues as stale if nobody answered

--- scripts/stale.py
+++ scripts/stale.py
@@ -36,18 +36,19 @@ def main():
 
     for issue in open_issues:
         comments = sorted(issue.get_comments(), key=lambda i: i.created_at, reverse=True)
-        last_comment = comments[0] if len(comments) > 0 else None
+        involved_users = [comment.user.login for comment in comments if comment.user.login != "github-actions[bot]"]
         if (
-            last_comment is not None
-            and last_comment.user.login == "github-actions[bot]"
-            and (dt.now(timezone.utc) - issue.updated_at).days > 7
-            and (dt.now(timezone.utc) - issue.created_at).days >= 30
+            len(involved_users) >= 0  # somebody has commented
+            and involved_users[0].user.login == "github-actions[bot]"  # last comment from this bot
+            and (dt.now(timezone.utc) - issue.updated_at).days > 7  # stale for 7 days
+            and (dt.now(timezone.utc) - issue.created_at).days >= 30  # created for 30 days
             and not any(label.name.lower() in LABELS_TO_EXEMPT for label in issue.get_labels())
         ):
             issue.edit(state="closed")
         elif (
-            (dt.now(timezone.utc) - issue.updated_at).days > 23
-            and (dt.now(timezone.utc) - issue.created_at).days >= 30
+            len(involved_users) >= 0  # somebody has commented
+            and (dt.now(timezone.utc) - issue.updated_at).days > 23  # inactive for 23 days
+            and (dt.now(timezone.utc) - issue.created_at).days >= 30  # created for 30 days
             and not any(label.name.lower() in LABELS_TO_EXEMPT for label in issue.get_labels())
         ):
             issue.create_comment(
