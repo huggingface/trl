c73f43a9d4d51f8d924254b6c46a1899339da643
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-08-21 20:35:46 +0000

    style

--- trl/trainer/online_dpo_config.py
+++ trl/trainer/online_dpo_config.py
@@ -14,6 +14,7 @@ class OnlineDPOConfig(OnPolicyConfig):
     [argparse](https://docs.python.org/3/library/argparse#module-argparse) arguments that can be specified on the
     command line.
     """
+
     exp_name: str = os.path.basename(__file__)[: -len(".py")]
     """the name of this experiment"""
     reward_model_path: str = "EleutherAI/pythia-160m"
