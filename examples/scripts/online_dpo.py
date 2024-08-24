3d280b9135580b62630979eef4e6c118004d92f2
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-08-24 20:49:20 +0000

    fix example

--- examples/scripts/online_dpo.py
+++ examples/scripts/online_dpo.py
@@ -1,7 +1,30 @@
+# flake8: noqa
+# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
 from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoModelForSequenceClassification, AutoTokenizer
 
-from trl import ModelConfig, OnlineDPOConfig, OnlineDPOTrainer
+from trl import (
+    DPOScriptArguments,
+    ModelConfig,
+    OnlineDPOConfig,
+    OnlineDPOTrainer,
+    get_kbit_device_map,
+    get_quantization_config,
+)
 from trl.commands.cli_utils import TrlParser
 from trl.trainer.callbacks import LogCompletionsCallback
 
@@ -11,25 +34,51 @@ python examples/scripts/online_dpo.py --output_dir online_dpo
 """
 
 if __name__ == "__main__":
-    parser = TrlParser((OnlineDPOConfig, ModelConfig))
-    training_args, model_config = parser.parse_args_and_config()
+    parser = TrlParser((DPOScriptArguments, OnlineDPOConfig, ModelConfig))
+    args, training_args, model_config = parser.parse_args_and_config()
 
-    model = AutoModelForCausalLM.from_pretrained("cleanrl/EleutherAI_pythia-1b-deduped__sft__tldr")
-    ref_model = AutoModelForCausalLM.from_pretrained("cleanrl/EleutherAI_pythia-1b-deduped__sft__tldr")
-    reward_model = AutoModelForSequenceClassification.from_pretrained(
-        "cleanrl/EleutherAI_pythia-1b-deduped__reward__tldr"
+    torch_dtype = (
+        model_config.torch_dtype
+        if model_config.torch_dtype in ["auto", None]
+        else getattr(torch, model_config.torch_dtype)
     )
-    tokenizer = AutoTokenizer.from_pretrained("cleanrl/EleutherAI_pythia-1b-deduped__sft__tldr", padding_side="left")
+    quantization_config = get_quantization_config(model_config)
+    model_kwargs = dict(
+        revision=model_config.model_revision,
+        attn_implementation=model_config.attn_implementation,
+        torch_dtype=torch_dtype,
+        use_cache=False if training_args.gradient_checkpointing else True,
+        device_map=get_kbit_device_map() if quantization_config is not None else None,
+        quantization_config=quantization_config,
+    )
 
-    dataset = load_dataset("trl-internal-testing/tldr-preference-sft-trl-style")
+    model = AutoModelForCausalLM.from_pretrained(
+        model_config.model_name_or_path, trust_remote_code=model_config.trust_remote_code, **model_kwargs
+    )
+    ref_model = AutoModelForCausalLM.from_pretrained(
+        model_config.model_name_or_path, trust_remote_code=model_config.trust_remote_code, **model_kwargs
+    )
+    reward_model = AutoModelForSequenceClassification.from_pretrained(
+        training_args.reward_model_path, num_labels=1, trust_remote_code=model_config.trust_remote_code
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_config.model_name_or_path,
+        padding_side="left",
+        trust_remote_code=model_config.trust_remote_code,
+    )
+    dataset = load_dataset(args.dataset_name)
+
+    prompts = dataset[args.dataset_test_split]["prompt"][:8]
+    log_completions_callback = LogCompletionsCallback(prompts)
 
     trainer = OnlineDPOTrainer(
         model=model,
         ref_model=ref_model,
         reward_model=reward_model,
         args=training_args,
-        train_dataset=dataset["train"],
+        train_dataset=dataset[args.dataset_train_split],
+        eval_dataset=dataset[args.dataset_test_split],
         tokenizer=tokenizer,
-        callbacks=[LogCompletionsCallback(dataset["test"]["prompt"][:8])],
+        callbacks=[log_completions_callback],
     )
     trainer.train()
