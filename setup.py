34a8840031c2dc1410cdf31fef8410e011b007e1
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2024-08-28 14:37:58 +0200

    Update setup.py
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>

--- setup.py
+++ setup.py
@@ -107,7 +107,6 @@ try:
             "Natural Language :: English",
             "Operating System :: OS Independent",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
