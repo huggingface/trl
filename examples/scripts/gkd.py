dedc894bb9fee074fba5ad39aca6437eda897e30
Kashif Rasul <kashif.rasul@gmail.com>
2024-09-09 09:46:52 +0200

    fix prompts

--- examples/scripts/gkd.py
+++ examples/scripts/gkd.py
@@ -127,8 +127,9 @@ if __name__ == "__main__":
         eval_dataset = None
         prompts = train_dataset["messages"][:8]
 
-    # apply chat template to the prompts if tokenizer is a chat model
-    prompts = tokenizer.apply_chat_template(prompts, tokenize=False, add_generation_prompt=True)
+    # remove the last assistant message from the prompts messages and then apply chat template to the prompts
+    prompts = [prompts[i][:-1] for i in range(len(prompts))]
+    prompts = [tokenizer.apply_chat_template(prompt, tokenize=False) for prompt in prompts]
 
     ################
     # Optional rich context managers
