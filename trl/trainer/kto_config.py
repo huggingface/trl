cdafc9333cb31b367ba4e776467858c31fc2050c
Kashif Rasul <kashif.rasul@gmail.com>
2024-09-16 12:24:43 +0200

    [KTO] Overrides default `learning_rate` in `KTOConfig`  (#2070)
    * learning rate recomentations for kto
    
    * update from suggestion
    
    * override default lr
    
    * add tip tag
    
    * Update trl/trainer/kto_config.py
    
    ---------
    
    Co-authored-by: Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>

--- trl/trainer/kto_config.py
+++ trl/trainer/kto_config.py
@@ -27,6 +27,8 @@ class KTOConfig(TrainingArguments):
     command line.
 
     Parameters:
+        learning_rate (`float`, *optional*, defaults to `5e-7`):
+            Initial learning rate for [`AdamW`] optimizer. The default value replaces that of [`~transformers.TrainingArguments`].
         max_length (`Optional[int]`, *optional*, defaults to `None`):
             Maximum length of the sequences (prompt + completion) in the batch. This argument is required if you want
             to use the default data collator.
@@ -74,6 +76,7 @@ class KTOConfig(TrainingArguments):
             Number of processes to use for processing the dataset.
     """
 
+    learning_rate: float = 5e-7
     max_length: Optional[int] = None
     max_prompt_length: Optional[int] = None
     max_completion_length: Optional[int] = None
