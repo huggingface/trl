c926045df4f28777a8426830c020c9e7262ad99f
Kashif Rasul <kashif.rasul@gmail.com>
2024-09-24 15:23:40 +0200

    Update trl/trainer/nash_md_config.py
    Co-authored-by: Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>

--- trl/trainer/nash_md_config.py
+++ trl/trainer/nash_md_config.py
@@ -26,9 +26,10 @@ class NashMDConfig(OnlineDPOConfig):
     Subclass of [`OnlineDPOConfig`] we can use all its arguments and add the following:
 
     Parameters:
-        mixture_coef (`float` or `List[float]`, *optional*, defaults to `[0.5]`):
-            Logit mixture coefficient for the model and reference model. If a list of floats is provided then the mixture
-            coefficient is selected for each new epoch and the last coefficient is used for the rest of the epochs.
+        mixture_coef (`float` or `list[float]`, *optional*, defaults to `0.5`):
+             Logit mixture coefficient for the model and reference model. If a list of floats is provided then the
+             mixture coefficient is selected for each new epoch and the last coefficient is used for the rest of the
+             epochs.
     """
 
     mixture_coef: List[float] = field(default_factory=lambda: [0.5])
