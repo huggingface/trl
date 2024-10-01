54ef4ca84cab332c2fc99816ec311f729cef8c86
August Moharrami <moh.murr@gmail.com>
2024-10-01 16:56:09 +0000

    removing model_init_kwargs

--- examples/scripts/sft.py
+++ examples/scripts/sft.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
         device_map="auto",
         quantization_config=quantization_config,
     )
-    training_args.model_init_kwargs = model_kwargs
+
     tokenizer = AutoTokenizer.from_pretrained(
         model_config.model_name_or_path, trust_remote_code=model_config.trust_remote_code, use_fast=True
     )
