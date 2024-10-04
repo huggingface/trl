a8fba851c3eb1feb6e70509941f96872ad03e7a9
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-10-04 10:21:45 +0000

    orpo

--- tests/test_trainers_args.py
+++ tests/test_trainers_args.py
@@ -306,7 +306,7 @@ class TrainerArgTester(unittest.TestCase):
                 dataset_num_proc=4,
             )
 
-            trainer = ORPOTrainer(model="gpt2", args=training_args, train_dataset=dataset, tokenizer=tokenizer)
+            trainer = ORPOTrainer(model="gpt2", args=training_args, train_dataset=dataset, processing_class=tokenizer)
             self.assertEqual(trainer.args.max_length, 256)
             self.assertEqual(trainer.args.max_prompt_length, 64)
             self.assertEqual(trainer.args.max_completion_length, 64)
