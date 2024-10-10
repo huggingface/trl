fdbcaaea3d646c5b1e13083175b9e5985bb0d5d6
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-10-10 12:30:46 +0000

    Version `0.11.2` -> `0.11.3`

--- trl/__init__.py
+++ trl/__init__.py
@@ -14,7 +14,7 @@
 
 # flake8: noqa
 
-__version__ = "0.11.2"
+__version__ = "0.11.3"
 
 from typing import TYPE_CHECKING
 from .import_utils import _LazyModule, is_diffusers_available, OptionalDependencyNotAvailable
