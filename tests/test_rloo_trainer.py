7160f9ff81a2bd145732b208ea3f44f94a807e6e
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-10-11 17:18:07 +0000

    update test commands

--- tests/test_rloo_trainer.py
+++ tests/test_rloo_trainer.py
@@ -26,6 +26,7 @@ from trl import RLOOConfig, RLOOTrainer
 def test():
     command = """\
 python examples/scripts/rloo/rloo.py \
+    --dataset_name trl-lib/chatbot_arena_completions \
     --learning_rate 3e-6 \
     --output_dir models/minimal/rloo \
     --per_device_train_batch_size 4 \
