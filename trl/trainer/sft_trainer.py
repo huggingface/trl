53e8d8a4e0c77cab4273cc48319c08aa5a39464c
Paul Dufour <pdufour@users.noreply.github.com>
2024-11-03 13:49:38 +0000

    Update sft_trainer.py

--- trl/trainer/sft_trainer.py
+++ trl/trainer/sft_trainer.py
@@ -651,7 +651,7 @@ class SFTTrainer(Trainer):
         context_manager = amp.autocast("cuda") if self._peft_has_been_casted_to_bf16 else nullcontext()
 
         with context_manager:
-            loss = super().compute_loss(model, inputs, return_outputs)
+            loss, metrics = super().compute_loss(model, inputs, return_outputs)
         
         # Make sure to move the loss to the device the original accumulating loss is at back in the `Trainer` class:
         loss = loss.to(self.args.device)
