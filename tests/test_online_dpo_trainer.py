7eef63a5b4e99c37a44309a61f6194ac21bb6ecf
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-11-19 17:41:13 +0000

    Remove unused imports in test_online_dpo_trainer.py

--- tests/test_online_dpo_trainer.py
+++ tests/test_online_dpo_trainer.py
@@ -20,8 +20,7 @@ from transformers import AutoModelForCausalLM, AutoModelForSequenceClassificatio
 from transformers.testing_utils import require_peft
 from transformers.utils import is_peft_available
 
-from trl import OnlineDPOConfig, OnlineDPOTrainer, RandomPairwiseJudge, is_llm_blender_available
-from trl.trainer.utils import SIMPLE_CHAT_TEMPLATE
+from trl import OnlineDPOConfig, OnlineDPOTrainer
 
 from .testing_utils import RandomPairwiseJudge
 
