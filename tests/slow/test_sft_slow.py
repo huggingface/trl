5626806aef04203c484ee5dcd0ff97b6097a01e5
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2024-11-20 11:51:05 +0100

    🧲 Use our own `require_bitsandbytes` (#2370)
    * use our own require_bitsandbytes
    
    * rephrase

--- tests/slow/test_sft_slow.py
+++ tests/slow/test_sft_slow.py
@@ -22,7 +22,6 @@ from datasets import load_dataset
 from parameterized import parameterized
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from transformers.testing_utils import (
-    require_bitsandbytes,
     require_liger_kernel,
     require_peft,
     require_torch_accelerator,
@@ -33,6 +32,7 @@ from transformers.utils import is_peft_available
 from trl import SFTConfig, SFTTrainer
 from trl.models.utils import setup_chat_format
 
+from ..testing_utils import require_bitsandbytes
 from .testing_constants import DEVICE_MAP_OPTIONS, GRADIENT_CHECKPOINTING_KWARGS, MODELS_TO_TEST, PACKING_OPTIONS
 
 
