35ee4e60c77660c2b77eb35789446af24ffbbec4
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-11-21 18:55:41 +0000

    style

--- trl/cli.py
+++ trl/cli.py
@@ -1,8 +1,23 @@
-import argparse
+# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from transformers import HfArgumentParser, TrainingArguments
+import argparse
 import sys
 from pathlib import Path
+
+from transformers import HfArgumentParser, TrainingArguments
+
 
 # Add the root of the project to the python path so that we can import examples scripts
 path = Path(__file__).parent.parent
@@ -24,4 +39,3 @@ def main():
 
         (training_args,) = dpo_parser.parse_args_into_dataclasses()
         dpo_main(training_args)
-
