852ef2a1d668fa83992c2989548201b144f8f0c5
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2024-11-26 13:05:25 +0100

    🖋️ Fix warning message formatting in `KTOTrainer` (#2394)

--- trl/trainer/kto_trainer.py
+++ trl/trainer/kto_trainer.py
@@ -729,12 +729,12 @@ class KTOTrainer(Trainer):
 
                 if not (des_weight_in_range or und_weight_in_range):
                     warnings.warn(
-                        f"""
-                        You have different amounts of desirable/positive and undesirable/negative examples but the
-                        weights on the desirable and undesirable losses don't seem to be in an ideal range. Based
-                        on your data, we recommend EITHER desirable_weight in [{des_weight_lower_bound}, {des_weight_upper_bound}]
-                        or undesirable_weight in [{und_weight_lower_bound}, {und_weight_upper_bound}] (but NOT BOTH).
-                        See the documentation on how to optimally set these weights.""",
+                        "You have different amounts of desirable/positive and undesirable/negative examples but the "
+                        "weights on the desirable and undesirable losses don't seem to be in an ideal range. Based "
+                        f"on your data, we recommend EITHER "
+                        f"desirable_weight in [{des_weight_lower_bound}, {des_weight_upper_bound}] or "
+                        f"undesirable_weight in [{und_weight_lower_bound}, {und_weight_upper_bound}] (but NOT BOTH). "
+                        "See the documentation on how to optimally set these weights.",
                         UserWarning,
                     )
 
