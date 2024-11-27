a4c36646cf9dd23657e7b5e4b670cba67dc489c2
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-11-27 15:32:17 +0000

    Add community notebooks to API documentation

---
+++ docs/source/community_notebooks.py
@@ -0,0 +1,13 @@
+# Community Notebooks
+
+Blah, blah, blah to explain that the following notebooks are contributed by the community, and we don't maintain them. And everyone can contribute their own notebooks.
+
+## Supervised
+
+
+
+
+## Vision Language Models
+
+- 👁️ [Fine-Tuning a Vision Language Model (Qwen2-VL-7B) with the Hugging Face Ecosystem (TRL)](https://huggingface.co/learn/cookbook/fine_tuning_vlm_trl), by   [Sergio Paniego](https://github.com/sergiopaniego)
+
