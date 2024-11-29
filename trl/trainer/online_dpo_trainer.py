c7b81f44751eef55c817b2fb917779d423f593b5
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-11-29 19:31:21 +0000

    add model prep back

--- trl/trainer/online_dpo_trainer.py
+++ trl/trainer/online_dpo_trainer.py
@@ -277,6 +277,11 @@ class OnlineDPOTrainer(Trainer):
 
         self._beta = args.beta
 
+        if self.ref_model is not None:
+            self.ref_model = self.ref_model.to(self.accelerator.device)
+        if self.reward_model is not None:
+            self.reward_model = self.reward_model.to(self.accelerator.device)
+
     @property
     def beta(self):
         if isinstance(self._beta, list):
