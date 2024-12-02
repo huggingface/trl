c0ed5dd3540967662b5f131791e19168382ea48b
Swayam <74960567+SwayamInSync@users.noreply.github.com>
2024-12-02 20:34:58 +0530

    Update trl/trainer/dpo_config.py
    Co-authored-by: Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>

--- trl/trainer/dpo_config.py
+++ trl/trainer/dpo_config.py
@@ -95,7 +95,9 @@ class DPOConfig(TrainingArguments):
             Whether to precompute reference model log probabilities for training and evaluation datasets. This is
             useful when training without the reference model to reduce the total GPU memory needed.
         precompute_ref_batch_size (`int`, *optional*, defaults to `None`):
-            Batch size to use when precomputing reference model log probabilities. This can be set higher than the training batch size to speed up preprocessing. If None, defaults to per_device_train_batch_size for training and per_device_eval_batch_size for evaluation.
+            Batch size to use when precomputing reference model log probabilities. This can be set higher than the
+            training batch size to speed up preprocessing. If `None``, defaults to `per_device_train_batch_size` for
+            training and `per_device_eval_batch_size` for evaluation.
         dataset_num_proc (`Optional[int]`, *optional*, defaults to `None`):
             Number of processes to use for processing the dataset.
         model_init_kwargs (`Optional[dict[str, Any]]`, *optional*, defaults to `None`):
