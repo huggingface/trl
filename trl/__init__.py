7ff70463cd0f7052e02edc5e40b14abdac3e13d0
Kashif Rasul <kashif.rasul@gmail.com>
2024-12-06 13:25:41 +0100

    update version

--- trl/__init__.py
+++ trl/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.12.1"
+__version__ = "0.12.2"
 
 from typing import TYPE_CHECKING
 
