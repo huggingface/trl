2fc0b6f5e3da0c68c57f6dd618f40f4394701938
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2024-12-10 10:40:00 +0100

    ©️ Copyrights update (#2454)
    * First changes
    
    * Other files
    
    * Finally
    
    * rm comment
    
    * fix nashmd
    
    * Fix example
    
    * Fix example [ci skip]

--- examples/scripts/chat.py
+++ examples/scripts/chat.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 ################################################################################################
 # This file has been moved to https://github.com/huggingface/trl/blob/main/trl/scripts/chat.py #
 ################################################################################################
