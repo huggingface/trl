5239b9462dccd8751936f6f4e181a6db0c2bef7d
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2024-12-22 12:19:17 +0100

    💧 Generalize `disable_dropout` (#2511)

--- trl/trainer/online_dpo_config.py
+++ trl/trainer/online_dpo_config.py
@@ -57,7 +57,7 @@ class OnlineDPOConfig(TrainingArguments):
         dataset_num_proc (`Optional[int]`, *optional*, defaults to `None`):
             Number of processes to use for processing the dataset.
         disable_dropout (`bool`, *optional*, defaults to `True`):
-            Whether to disable dropout in the model.
+            Whether to disable dropout in the model and reference model.
     """
 
     learning_rate: float = 5e-7
