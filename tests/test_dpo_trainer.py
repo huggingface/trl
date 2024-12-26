72df715df7f3b98d7cbe0ff7e9fe77cbd8dfd463
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-12-26 23:48:14 +0000

    drop require flash-attn

--- tests/test_dpo_trainer.py
+++ tests/test_dpo_trainer.py
@@ -29,12 +29,7 @@ from transformers import (
     PreTrainedTokenizerBase,
     is_vision_available,
 )
-from transformers.testing_utils import (
-    require_flash_attn,
-    require_peft,
-    require_torch_gpu_if_bnb_not_multi_backend_enabled,
-    require_vision,
-)
+from transformers.testing_utils import require_peft, require_torch_gpu_if_bnb_not_multi_backend_enabled, require_vision
 
 from trl import DPOConfig, DPOTrainer, FDivergenceType
 
@@ -1170,14 +1165,15 @@ class DPOTrainerTester(unittest.TestCase):
 
             trainer.train()
 
-    @require_flash_attn
     def test_padding_free(self):
         model_id = "trl-internal-testing/tiny-LlamaForCausalLM-3.2"
         tokenizer = AutoTokenizer.from_pretrained(model_id)
         tokenizer.pad_token = tokenizer.eos_token
-        model = AutoModelForCausalLM.from_pretrained(
-            model_id, torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2"
-        )
+
+        # Normally, we need `attn_implementation="flash_attention_2"` to that the model returns correct logits.
+        # Without it, the logits may be incorrect, but that's fine here. This test focuses only on the inner logic
+        # of padding_free.
+        model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.bfloat16)
 
         with tempfile.TemporaryDirectory() as tmp_dir:
             training_args = DPOConfig(
