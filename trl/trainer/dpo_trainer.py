bec9b43d4025cd6c8974e833700cdd2d9854c690
Lin, Fanli <fanli.lin@intel.com>
2025-01-02 22:40:45 -0800

    bug fix

--- trl/trainer/dpo_trainer.py
+++ trl/trainer/dpo_trainer.py
@@ -1,3 +1,17 @@
+# Copyright 2025 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Copyright 2024 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -731,7 +745,7 @@ class DPOTrainer(Trainer):
                 # Unnecessary cache clearing to avoid OOM
                 if self.args.device.type == "cuda":
                     torch.cuda.empty_cache()
-                else:
+                elif self.args.device.type == "xpu":
                     torch.xpu.empty_cache()
                 self.accelerator.free_memory()
 
