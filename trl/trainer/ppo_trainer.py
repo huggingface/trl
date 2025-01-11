909b605186dce19a09cbd242168f8f34ffe9baf4
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2025-01-11 14:05:15 +0000

    feat(PPOTrainer): add `_compute_score` method
    - Refactors the score computation logic by decoupling it from the `train()` method.
    - Enables easier customization of reward logic by overriding the `_compute_score` method.

--- trl/trainer/ppo_trainer.py
+++ trl/trainer/ppo_trainer.py
@@ -337,6 +337,16 @@ class PPOTrainer(Trainer):
         if self.is_deepspeed_enabled:
             self.deepspeed = backup_deepspeed
 
+    def _compute_score(self, query_reponse: torch.Tensor, context_length: int) -> torch.Tensor:
+        '''
+        This methods decoples the score computing from the training method.
+        Override it to implement your custom reward function.
+        '''
+        _, score, _ = get_reward(
+            self.reward_model, query_reponse, self.processing_class.pad_token_id, context_length
+        )
+        return score
+
     def train(self):
         args = self.args
         accelerator = self.accelerator
@@ -460,9 +470,7 @@ class PPOTrainer(Trainer):
                         unwrapped_value_model, query_response, processing_class.pad_token_id, context_length
                     )
                     value = full_value[:, context_length - 1 : -1].squeeze(-1)
-                    _, score, _ = get_reward(
-                        reward_model, postprocessed_query_response, processing_class.pad_token_id, context_length
-                    )
+                    score = self._compute_score(postprocessed_query_response, context_length)
 
                     responses.append(response)
                     postprocessed_responses.append(postprocessed_response)
@@ -712,9 +720,7 @@ class PPOTrainer(Trainer):
                     )
 
                     postprocessed_query_response = torch.cat((query, postprocessed_response), 1)
-                    _, score, _ = get_reward(
-                        self.reward_model, postprocessed_query_response, processing_class.pad_token_id, context_length
-                    )
+                    score = self._compute_score(postprocessed_query_response, context_length)
                     table["score"].extend(self.accelerator.gather(score).float().cpu().numpy())
 
                 if sampling:
