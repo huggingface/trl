b39f54afbc5015da58ac2167242428fe29dc708a
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2025-01-15 18:37:06 +0000

    proper require_torch_accelerator

--- tests/test_online_dpo_trainer.py
+++ tests/test_online_dpo_trainer.py
@@ -241,8 +241,8 @@ class TestOnlineDPOTrainer(unittest.TestCase):
             self.assertIn("train_loss", trainer.state.log_history[-1])
 
     @unittest.skipIf(not is_vllm_available(), "vllm is not available")
-    @require_torch_accelerator
     @parameterized.expand([("standard_prompt_only",), ("conversational_prompt_only",)])
+    @require_torch_accelerator
     def test_training_with_vllm(self, config_name):
         model_id = "trl-internal-testing/small-Qwen2ForCausalLM-2.5"  # We neeed a bigger model
         model = AutoModelForCausalLM.from_pretrained(model_id)
