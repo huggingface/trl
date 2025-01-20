5fd78367aef83d3666737d29e50a0d88eb6d807f
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2025-01-20 21:26:11 +0100

    🫣 Ignore CLI test for Python 3.9 (#2592)
    * ignore cli test for python 3.9
    
    * move import inside tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,16 +13,22 @@
 # limitations under the License.
 
 
+import sys
 import tempfile
 import unittest
 from io import StringIO
 from unittest.mock import patch
 
-from trl.cli import main
 
-
+@unittest.skipIf(
+    sys.version_info < (3, 10),
+    "Transformers' generation codebase uses a Python >3.10 syntax (`str | None`), which seems to cause the CLI tests "
+    "to fail on Python <3.10.",  # let's say it's a known issue, but not expected to be fixed, because too niche
+)
 class TestCLI(unittest.TestCase):
     def test_dpo(self):
+        from trl.cli import main
+
         with tempfile.TemporaryDirectory() as tmp_dir:  # Create a temporary directory
             command = f"trl dpo --output_dir {tmp_dir} --model_name_or_path trl-internal-testing/tiny-Qwen2ForCausalLM-2.5 --dataset_name trl-internal-testing/zen --dataset_config standard_preference --report_to none"
             with patch("sys.argv", command.split(" ")):
@@ -30,6 +36,8 @@ class TestCLI(unittest.TestCase):
 
     @patch("sys.stdout", new_callable=StringIO)
     def test_env(self, mock_stdout):
+        from trl.cli import main
+
         command = "trl env"
         with patch("sys.argv", command.split(" ")):
             main()
@@ -42,12 +50,16 @@ class TestCLI(unittest.TestCase):
                 main()
 
     def test_kto(self):
+        from trl.cli import main
+
         with tempfile.TemporaryDirectory() as tmp_dir:  # Create a temporary directory
             command = f"trl kto --output_dir {tmp_dir} --model_name_or_path trl-internal-testing/tiny-Qwen2ForCausalLM-2.5 --dataset_name trl-internal-testing/zen --dataset_config standard_unpaired_preference --report_to none"
             with patch("sys.argv", command.split(" ")):
                 main()
 
     def test_sft(self):
+        from trl.cli import main
+
         with tempfile.TemporaryDirectory() as tmp_dir:  # Create a temporary directory
             command = f"trl sft --output_dir {tmp_dir} --model_name_or_path trl-internal-testing/tiny-Qwen2ForCausalLM-2.5 --dataset_name trl-internal-testing/zen --dataset_config standard_language_modeling --report_to none"
             with patch("sys.argv", command.split(" ")):
