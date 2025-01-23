b0ba01adbab8adb8f8a212740b17940e3e97946e
August Moharrami <moh.murr@gmail.com>
2025-01-23 10:28:10 +0000

    rloo custom reward function and test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -30,6 +30,7 @@ from trl.trainer.utils import (
     flush_left,
     generate_model_card,
     get_peft_config,
+    get_reward_custom,
     pad,
 )
 
@@ -451,3 +452,20 @@ class TestFlushLeft(unittest.TestCase):
         expected_mask = torch.tensor([[1, 1, 1], [1, 1, 0]])
 
         self.assertTrue(torch.equal(new_mask, expected_mask))
+
+
+class TestReward(unittest.TestCase):
+    def test_get_reward_custom(self):
+        tokenizer = AutoTokenizer.from_pretrained("trl-internal-testing/tiny-LlamaForCausalLM-3.2")
+        tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+        text = ["this text is extactly 37 letters long"]
+
+        # defining dummy reward function
+        def reward_function(texts):
+            # based on length of text
+            rewards = [len(text) for text in texts]
+            return rewards
+
+        tokenized_text = tokenizer(text, return_tensors="pt")["input_ids"]
+        reward = get_reward_custom(reward_function, tokenizer, tokenized_text)
+        self.assertTrue(float(reward[0]) == 37)
