2d956c7b90fd912a84a5f087d232470288b02db2
Kashif Rasul <kashif.rasul@gmail.com>
2025-01-25 12:12:02 +0100

    formatting

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -528,7 +528,9 @@ class GRPOTrainer(Trainer):
                         # Repeat each value in the column for `num_generations` times
                         reward_kwargs[key].extend([example[key]] * self.num_generations)
                 output_reward_func = reward_func(prompts=prompts, completions=completions, **reward_kwargs)
-                rewards_per_func[:, i] = torch.tensor(output_reward_func, dtype=torch.float32, device=self.accelerator.device)
+                rewards_per_func[:, i] = torch.tensor(
+                    output_reward_func, dtype=torch.float32, device=self.accelerator.device
+                )
 
         # Sum the rewards from all reward functions
         rewards = rewards_per_func.sum(dim=1)
