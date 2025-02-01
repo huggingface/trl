6abda349c0f5f5256ebd893e6fa893d93cebcedd
Mircea Pricop <mircea.a.pricop@gmail.com>
2025-02-01 16:58:41 +0100

    Set max_model_len when initializing vLLM instance

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -314,6 +314,7 @@ class GRPOTrainer(Trainer):
                         model=model.name_or_path,
                         device=vllm_device,
                         gpu_memory_utilization=self.args.vllm_gpu_memory_utilization,
+                        max_model_len=(self.max_prompt_length + self.max_completion_length),
                     )
                 self.sampling_params = SamplingParams(
                     n=self.num_generations,
