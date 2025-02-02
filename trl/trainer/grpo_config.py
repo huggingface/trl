e26d7fc0653c90841a9595901a4e544c5ddd9ac2
Mircea Pricop <mircea.a.pricop@gmail.com>
2025-02-02 21:07:35 +0000

    Add missing import

--- trl/trainer/grpo_config.py
+++ trl/trainer/grpo_config.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import warnings
 from dataclasses import dataclass, field
 from typing import Optional
 
