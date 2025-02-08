76dffbc8c1848d5afacc74694c5bfe0976062b5d
William Brown <williambrown97@gmail.com>
2025-02-07 23:49:16 -0500

    llm.engine

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -477,7 +477,7 @@ class GRPOTrainer(Trainer):
                     else:
                         state_dict = unwrapped_model.state_dict()
                 if self.accelerator.is_main_process:
-                    llm_model = self.llm.llm_engine.model_executor.driver_worker.model_runner.model
+                    llm_model = self.llm.engine.model_executor.driver_worker.model_runner.model
                     llm_model.load_weights(state_dict.items())
                 self._last_loaded_step = self.state.global_step
 
