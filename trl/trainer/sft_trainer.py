f9a642bb8e40047c8f05f300524519010d23000b
Kashif Rasul <kashif.rasul@gmail.com>
2025-02-14 14:51:21 +0100

    add back get_formatting_func_from_dataset

--- trl/trainer/sft_trainer.py
+++ trl/trainer/sft_trainer.py
@@ -44,6 +44,7 @@ from transformers.utils import is_liger_kernel_available, is_peft_available
 from transformers.utils.deprecation import deprecate_kwarg
 
 from ..data_utils import is_conversational, maybe_apply_chat_template, pack_examples
+from ..extras.dataset_formatting import get_formatting_func_from_dataset
 from .sft_config import SFTConfig
 from .utils import (
     ConstantLengthDataset,
@@ -193,6 +194,11 @@ class SFTTrainer(Trainer):
                 processing_class.pad_token = processing_class.eos_token  # required for padding when collating data
 
         # Dataset
+        if formatting_func is None:
+            # check if dataset has ChatML format or instruction format and is supported
+            # if not stays None
+            formatting_func = get_formatting_func_from_dataset(train_dataset, processing_class)
+
         preprocess_dataset = args.dataset_kwargs is None or not args.dataset_kwargs.get("skip_prepare_dataset", False)
         if preprocess_dataset:
             train_dataset = self._prepare_dataset(
