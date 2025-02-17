7596db89da9a728d1635271bf1f49bf03eb89b80
Kashif Rasul <kashif.rasul@gmail.com>
2025-02-17 07:50:55 +0100

    [SFT] fix check for AutoLigerKernelForCausalLM (#2874)
    * fix check for AutoLigerKernelForCausalLM
    
    * fix case where AutoLigerKernelForCausalLM is not defined
    
    * update min liger version
    
    * formatting
    
    * fix win CI

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ EXTRAS = {
     "diffusers": ["diffusers>=0.18.0"],
     "judges": ["openai>=1.23.2", "llm-blender>=0.0.2"],
     # liger-kernel depends on triton, which is only available on Linux https://github.com/triton-lang/triton#compatibility
-    "liger": ["liger-kernel>=0.4.0; sys_platform != 'win32'"],
+    "liger": ["liger-kernel>=0.5.3; sys_platform != 'win32'"],
     "mergekit": ["mergekit>=0.0.5.1"],
     "peft": ["peft>=0.8.0"],
     "quantization": ["bitsandbytes"],
