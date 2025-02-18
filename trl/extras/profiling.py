fa665a363c35f4d772b5354364de9f0570a9e150
edbeeching <edbeeching@aol.com>
2025-02-18 10:40:47 +0000

    naming + precommit

--- trl/extras/profiling.py
+++ trl/extras/profiling.py
@@ -12,25 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
 import functools
+import time
 
 from transformers import is_wandb_available
+
 
 if is_wandb_available():
     import wandb
 
-def profiler_decorator(func):
+
+def profiling_decorator(func):
     """
     Decorator to profile a function and log the time taken to execute it.
     """
+
     @functools.wraps(func)
     def wrapper(self, *args, **kwargs):
         start_time = time.perf_counter()
         result = func(self, *args, **kwargs)
         end_time = time.perf_counter()
         duration = end_time - start_time
-        
+
         if "wandb" in self.args.report_to and wandb.run is not None and self.accelerator.is_main_process:
             wandb.log({f"profiling/Time taken: {self.__class__.__name__}.{func.__name__}": duration})
         return result
