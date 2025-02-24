69ad852e5654a77f1695eb4c608906fe0c7e8624
Huazhong Ji <hzji210@gmail.com>
2025-02-25 07:40:09 +0800

    ⏪ Parameterize `enable_prefix_caching` (#2900)
    * parameterize enable_prefix_caching
    
    * apply review suggestion
    
    ---------
    
    Co-authored-by: Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -481,7 +481,7 @@ class GRPOTrainer(Trainer):
                         # Automatic Prefix Caching caches the KV cache of existing queries, so that a new query can
                         # directly reuse the KV cache if it shares the same prefix with one of the existing queries.
                         # This is particularly useful here because we generate completions from the same prompts.
-                        enable_prefix_caching=True,
+                        enable_prefix_caching=self.args.vllm_enable_prefix_caching,
                         max_model_len=self.args.vllm_max_model_len,
                     )
 
