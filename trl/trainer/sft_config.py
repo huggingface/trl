55a9753a072138102d1bae9eb377153646f76295
DanFosing <danfoss12340@gmail.com>
2025-02-24 18:34:16 +0100

    Update sft_config.py

--- trl/trainer/sft_config.py
+++ trl/trainer/sft_config.py
@@ -170,7 +170,7 @@ class SFTConfig(TrainingArguments):
 
         if self.max_seq_length is not None:
             warnings.warn(
-                "`max_seq_length` is deprecated and will be remove in version 0.20.0. Use `max_length` instead.", # Shouldn't this warning message be changed as max_seq_lenght doesn't seem to work currently?
+                "`max_seq_length` is deprecated and will be remove in version 0.20.0. Use `max_length` instead.",
                 DeprecationWarning,
             )
             self.max_length = self.max_seq_length
