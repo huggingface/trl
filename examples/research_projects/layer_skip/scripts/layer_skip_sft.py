c63e48d8bfd63545e0af5031de56428f63253792
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-03-24 18:01:26 +0000

    precommits

--- examples/research_projects/layer_skip/scripts/layer_skip_sft.py
+++ examples/research_projects/layer_skip/scripts/layer_skip_sft.py
@@ -1,11 +1,24 @@
+# Copyright 2025 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import config
 import torch
 from custom_trainer import LayerSkipSFTTrainer
-
 from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from trl import SFTConfig, DataCollatorForCompletionOnlyLM
+from trl import DataCollatorForCompletionOnlyLM, SFTConfig
 
 
 def formatting_prompts_func(example):
