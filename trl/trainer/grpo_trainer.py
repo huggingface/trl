2295aa5a1ffc5ed568ef0b220177b6288e054cca
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-04-09 22:57:33 +0000

    style and finish merge main

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -20,6 +20,7 @@ from collections.abc import Sized
 from contextlib import nullcontext
 from typing import Any, Callable, Optional, Union
 
+import datasets
 import torch
 import torch.utils.data
 import transformers
@@ -59,9 +60,6 @@ from .utils import (
     selective_log_softmax,
 )
 
-
-if is_datasets_available():
-    import datasets
 
 if is_deepspeed_available():
     import deepspeed
@@ -1068,21 +1066,21 @@ class GRPOTrainer(Trainer):
 
         # log completion lengths, mean, min, max
         agg_completion_mask = self.accelerator.gather_for_metrics(completion_mask.sum(1))
-        self._metrics[mode]["mean_completion_length"].append(agg_completion_mask.float().mean().item())
-        self._metrics[mode]["min_completion_length"].append(agg_completion_mask.float().min().item())
-        self._metrics[mode]["max_completion_length"].append(agg_completion_mask.float().max().item())
+        self._metrics[mode]["completions/mean_length"].append(agg_completion_mask.float().mean().item())
+        self._metrics[mode]["completions/min_length"].append(agg_completion_mask.float().min().item())
+        self._metrics[mode]["completions/max_length"].append(agg_completion_mask.float().max().item())
 
         # identify sequences that terminated with EOS and log their lengths
         agg_terminated_with_eos = self.accelerator.gather_for_metrics(is_eos.any(dim=1))
         term_completion_mask = agg_completion_mask[agg_terminated_with_eos]
         clipped_completions_ratio = 1 - len(term_completion_mask) / len(agg_completion_mask)
-        self._metrics[mode]["clipped_completions_ratio"].append(clipped_completions_ratio)
+        self._metrics[mode]["completions/clipped_ratio"].append(clipped_completions_ratio)
         if len(term_completion_mask) == 0:
             # edge case where no completed sequences are found
             term_completion_mask = torch.zeros(1, device=device)
-        self._metrics[mode]["mean_terminated_completion_length"].append(term_completion_mask.float().mean().item())
-        self._metrics[mode]["min_terminated_completion_length"].append(term_completion_mask.float().min().item())
-        self._metrics[mode]["max_terminated_completion_length"].append(term_completion_mask.float().max().item())
+        self._metrics[mode]["completions/mean_terminated_length"].append(term_completion_mask.float().mean().item())
+        self._metrics[mode]["completions/min_terminated_length"].append(term_completion_mask.float().min().item())
+        self._metrics[mode]["completions/max_terminated_length"].append(term_completion_mask.float().max().item())
 
         # Get the names of the reward functions
         reward_func_names = []
