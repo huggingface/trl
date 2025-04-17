11b497bf2f53a2c07ac7067d7781df670516649e
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2025-04-17 09:59:59 -0700

    Update trl/scripts/vllm_serve.py
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>

--- trl/scripts/vllm_serve.py
+++ trl/scripts/vllm_serve.py
@@ -237,7 +237,7 @@ class ScriptArguments:
     )
 
 
-def llm_worker(script_args, data_parallel_rank, connection):
+def llm_worker(script_args : ScriptArguments, data_parallel_rank : int, connection : Pipe) -> None:
     # Set required environment variables for DP to work with vLLM
     os.environ["VLLM_DP_RANK"] = str(data_parallel_rank)
     os.environ["VLLM_DP_RANK_LOCAL"] = str(data_parallel_rank)
