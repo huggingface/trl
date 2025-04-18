e481bacf494c9ca642efb1bf5032f721bf0a7d63
edbeeching <edbeeching@gmail.com>
2025-04-18 14:58:36 +0000

    train grpo file for testing

--- train_grpo.py
+++ train_grpo.py
@@ -1,11 +1,29 @@
+# Copyright 2020-2025 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from datasets import load_dataset
+
 from trl import GRPOConfig, GRPOTrainer
 
+
 dataset = load_dataset("trl-lib/tldr", split="train")
+
 
 # Define the reward function, which rewards completions that are close to 20 characters
 def reward_len(completions, **kwargs):
     return [-abs(20 - len(completion)) for completion in completions]
+
 
 training_args = GRPOConfig(output_dir="Qwen2-0.5B-GRPO", logging_steps=1, replay_buffer_class="SSRReplayBuffer")
 trainer = GRPOTrainer(
@@ -15,4 +33,3 @@ trainer = GRPOTrainer(
     train_dataset=dataset,
 )
 trainer.train()
-
