3a8f5094648aa2abe9c4fcf2f2db26d6c09bef37
shirinyamani <yamani.shirin@ucalgary.ca>
2025-05-05 23:22:15 +0000

    qwen3 added for tests

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -102,6 +102,7 @@ class ApplyChatTemplateTester(unittest.TestCase):
         "trl-internal-testing/tiny-MistralForCausalLM-0.2",
         "trl-internal-testing/tiny-Phi3ForCausalLM",
         "trl-internal-testing/tiny-Qwen2ForCausalLM-2.5",
+        "trl-internal-testing/tiny-Qwen3ForCausalLM",
     ]
 
     conversational_examples = [
