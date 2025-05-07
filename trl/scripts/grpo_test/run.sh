a6158fa5ff039da1013a03377322865285a8136c
renxinx <renxinx2021@gmail.com>
2025-05-06 23:24:48 -0700

    delete test scripts

--- trl/scripts/grpo_test/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export CUDA_VISIBLE_DEVICES=5,6,7
-export PYTHONPATH="/sgl-workspace/ryang/trl:$PYTHONPATH"
-accelerate launch --config_file=trl/scripts/grpo_test/grpo_sgl_test.yaml trl/scripts/grpo_test/grpo_sgl_test.py
