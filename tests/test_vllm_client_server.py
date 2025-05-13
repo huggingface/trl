d3c3b9e6421fafd22f0bb1965c325c2fe26c7c79
Yao Matrix <matrix.yao@intel.com>
2025-05-14 07:19:44 +0800

    Update test_vllm_client_server.py

--- tests/test_vllm_client_server.py
+++ tests/test_vllm_client_server.py
@@ -142,7 +142,7 @@ class TestVLLMClientServerTP(unittest.TestCase):
         # We want the server to run on accelerator 1 and 2, so we set VISIBLE_DEVICES to "1,2"
         env = os.environ.copy()
         VISIBLE_DEVICES = "ZE_AFFINITY_MASK" if torch_device == "xpu" else "CUDA_VISIBLE_DEVICES"
-        env[VISIBLE_DEVICES] = "1,2"
+        env[VISIBLE_DEVICES] = "1,2"  # Restrict to accelerator 1 and 2
 
         # Start the server process
         cls.server_process = subprocess.Popen(
