ee23a56c55073d25af3012da9f737dace427c838
HERIUN <ehdrndd@naver.com>
2025-05-15 09:58:11 +0000

    add warning if dataset's input_ids exceed max_length

--- trl/trainer/sft_trainer.py
+++ trl/trainer/sft_trainer.py
@@ -635,7 +635,7 @@ class SFTTrainer(Trainer):
 
                         # Create a completion mask
                         completion_mask = [0] * len(prompt_ids) + [1] * (len(prompt_completion_ids) - len(prompt_ids))
-                        processed = {**processed, "completion_mask": completion_mask}
+                        processed = {**processed, "completion_mask": completion_mask, "length":len(processed["input_ids"])}
 
                     else:  # language modeling case
                         processed = processing_class(
@@ -663,7 +663,9 @@ class SFTTrainer(Trainer):
                 dataset = pack_dataset(dataset, args.max_length, map_kwargs)
             elif args.max_length is not None:
                 if isinstance(dataset, Dataset):  # `IterableDataset.map` does not support `desc`
-                    map_kwargs["desc"] = f"Truncating {dataset_name} dataset"
+                    if max(dataset["length"]) > args.max_length:
+                        warnings.warn(f"Dataset max sequence length is {max(dataset["length"])} which longer than max_length(={args.max_length}).")
+                    map_kwargs["desc"] = f"Truncating {dataset_name} dataset with max_length={args.max_length}"
                 dataset = truncate_dataset(dataset, args.max_length, map_kwargs)
             # For Liger kernel, ensure only input_ids is present
             if args.use_liger_kernel:
