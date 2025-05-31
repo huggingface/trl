809aa5d9a46736bb465b52ee06bfd1dff8ccefd2
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-05-31 18:49:27 +0000

    test

--- tests/test_dataset_formatting.py
+++ tests/test_dataset_formatting.py
@@ -19,7 +19,7 @@ from datasets import Dataset, load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from trl.extras.dataset_formatting import get_formatting_func_from_dataset
-from trl.models.utils import setup_chat_format
+from trl.models.utils import ChatMlSpecialTokens, setup_chat_format, setup_chat_template
 
 
 class DatasetFormattingTestCase(unittest.TestCase):
@@ -119,24 +119,29 @@ class SetupChatFormatTestCase(unittest.TestCase):
     def setUp(self):
         self.tokenizer = AutoTokenizer.from_pretrained("trl-internal-testing/tiny-Qwen2ForCausalLM-2.5")
         self.model = AutoModelForCausalLM.from_pretrained("trl-internal-testing/tiny-Qwen2ForCausalLM-2.5")
-        # Remove built-in chat_template to simulate a model having no chat_template
+        # remove built-in chat_template to simulate a model having no chat_template
         self.tokenizer.chat_template = None
 
     def test_setup_chat_format(self):
-        _, modified_tokenizer = setup_chat_format(self.model, self.tokenizer, format="chatml")
+        modified_model, modified_tokenizer = setup_chat_format(
+            self.model, self.tokenizer, format="chatml", resize_to_multiple_of=123
+        )
 
+        _chatml = ChatMlSpecialTokens()
         # Check if special tokens are correctly set
         self.assertEqual(modified_tokenizer.eos_token, "<|im_end|>")
+        self.assertEqual(modified_tokenizer.pad_token, "<|im_end|>")
         self.assertEqual(modified_tokenizer.bos_token, "<|im_start|>")
-
-    def test_setup_chat_format_with_resize(self):
-        modified_model, _ = setup_chat_format(self.model, self.tokenizer, format="chatml", resize_to_multiple_of=123)
-
-        # Check that the input embeddings have been resized to a multiple of 123
-        self.assertEqual((modified_model.get_input_embeddings().weight.shape[0] % 123), 0)
+        self.assertEqual(modified_tokenizer.eos_token, _chatml.eos_token)
+        self.assertEqual(modified_tokenizer.pad_token, _chatml.pad_token)
+        self.assertEqual(modified_tokenizer.bos_token, _chatml.bos_token)
+        self.assertEqual((self.model.get_input_embeddings().weight.shape[0] % 123), 0)
 
     def test_example_with_setup_model(self):
-        _, modified_tokenizer = setup_chat_format(self.model, self.tokenizer)
+        modified_model, modified_tokenizer = setup_chat_format(
+            self.model,
+            self.tokenizer,
+        )
         messages = [
             {"role": "system", "content": "You are helpful"},
             {"role": "user", "content": "Hello"},
@@ -147,4 +152,39 @@ class SetupChatFormatTestCase(unittest.TestCase):
         self.assertEqual(
             prompt,
             "<|im_start|>system\nYou are helpful<|im_end|>\n<|im_start|>user\nHello<|im_end|>\n<|im_start|>assistant\nHi, how can I help you?<|im_end|>\n",
+        )
+
+
+class SetupChatTemplateTestCase(unittest.TestCase):
+    def setUp(self):
+        # This tokenizer doesn't have a chat_template by default
+        self.tokenizer = AutoTokenizer.from_pretrained("trl-internal-testing/tiny-BloomForCausalLM")
+        self.model = AutoModelForCausalLM.from_pretrained("trl-internal-testing/tiny-BloomForCausalLM")
+        # This one has a chat_template by default
+        self.source = "trl-internal-testing/tiny-Qwen3ForCausalLM"
+
+    def test_setup(self):
+        _, modified_tokenizer = setup_chat_template(self.model, self.tokenizer, self.source)
+
+        # Check if special tokens are correctly set
+        self.assertEqual(modified_tokenizer.eos_token, "<|im_end|>")
+
+    def test_setup_with_resize(self):
+        modified_model, _ = setup_chat_template(self.model, self.tokenizer, self.source, resize_to_multiple_of=123)
+
+        # Check that the input embeddings have been resized to a multiple of 123
+        self.assertEqual((modified_model.get_input_embeddings().weight.shape[0] % 123), 0)
+
+    def test_apply_new_chat_template(self):
+        _, modified_tokenizer = setup_chat_template(self.model, self.tokenizer, self.source)
+        messages = [
+            {"role": "system", "content": "You are helpful"},
+            {"role": "user", "content": "Hello"},
+            {"role": "assistant", "content": "Hi, how can I help you?"},
+        ]
+        prompt = modified_tokenizer.apply_chat_template(messages, tokenize=False)
+
+        self.assertEqual(
+            prompt,
+            "<|im_start|>system\nYou are helpful<|im_end|>\n<|im_start|>user\nHello<|im_end|>\n<|im_start|>assistant\n<think>\n\n</think>\n\nHi, how can I help you?<|im_end|>\n",
         )
