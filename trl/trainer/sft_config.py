ce1d6fc8d65115ec8f640a9ec3fc5f7abf037fd3
Edward Beeching <edbeeching@users.noreply.github.com>
2025-06-04 21:33:08 +0200

    Update trl/trainer/sft_config.py
    Co-authored-by: Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>

--- trl/trainer/sft_config.py
+++ trl/trainer/sft_config.py
@@ -81,11 +81,6 @@ class SFTConfig(TrainingArguments):
             the full sequence for [language modeling](#language-modeling) datasets.
         activation_offloading (`bool`, *optional*, defaults to `False`):
             Whether to offload the activations to the CPU.
-        average_tokens_across_devices (`bool`, *optional*, defaults to `True`):
-            Whether or not to average tokens across devices. If enabled, will use all_reduce to synchronize
-            num_tokens_in_batch for precise loss calculation. Reference:
-            https://github.com/huggingface/transformers/issues/34242
-            Overrides the `average_tokens_across_devices` parameter in [`~transformers.TrainingArguments`].
     """
 
     # Parameters whose default values are overridden from TrainingArguments
