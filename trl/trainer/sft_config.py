90ccb833ebb6ff7cc8f8bb9d1c0cad10695dd004
edbeeching <edbeeching@gmail.com>
2025-06-04 18:29:11 +0000

    set average_tokens_across_devices to true in SFTConfig

--- trl/trainer/sft_config.py
+++ trl/trainer/sft_config.py
@@ -80,7 +80,11 @@ class SFTConfig(TrainingArguments):
             the full sequence for [language modeling](#language-modeling) datasets.
         activation_offloading (`bool`, *optional*, defaults to `False`):
             Whether to offload the activations to the CPU.
-
+        average_tokens_across_devices (`bool`, *optional*, defaults to `True`):
+            Whether or not to average tokens across devices. If enabled, will use all_reduce to synchronize
+            num_tokens_in_batch for precise loss calculation. Reference:
+            https://github.com/huggingface/transformers/issues/34242
+            Overrides the `average_tokens_across_devices` parameter in [`~transformers.TrainingArguments`].
     """
 
     # Parameters that control the model
@@ -184,6 +188,14 @@ class SFTConfig(TrainingArguments):
         default=None,
         metadata={
             "help": "This parameter is deprecated and will be removed in version 0.20.0. Use `max_length` instead."
+        },
+    )
+    average_tokens_across_devices: bool = field(
+        default=True,
+        metadata={
+            "help": "Whether or not to average tokens across devices. If enabled, will use all_reduce to synchronize "
+            "num_tokens_in_batch for precise loss calculation. Reference: https://github.com/huggingface/transformers/issues/34242 "
+            "Overrides the `average_tokens_across_devices` parameter in [`~transformers.TrainingArguments`]."
         },
     )
 
