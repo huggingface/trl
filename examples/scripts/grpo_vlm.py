1935a02253c978daaf9ce8a3c94475a3f1b91fcc
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-07-22 02:32:15 +0000

    simplify example script

--- examples/scripts/grpo_vlm.py
+++ examples/scripts/grpo_vlm.py
@@ -16,14 +16,18 @@
 pip install math_verify
 
 accelerate launch \
-    --config_file=examples/accelerate_configs/deepspeed_zero3.yaml \
-    examples/scripts/grpo_vlm.py \
-    --dataset_name lmms-lab/multimodal-open-r1-8k-verified \
-    --model_name_or_path Qwen/Qwen2-VL-2B-Instruct \
-    --output_dir grpo-Qwen2-VL-2B-Instruct \
-    --torch_dtype bfloat16 \
-    --use_peft \
-    --lora_target_modules "q_proj", "v_proj"
+  --config_file=examples/accelerate_configs/deepspeed_zero3.yaml \
+  examples/scripts/grpo_vlm.py \
+  --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
+  --output_dir grpo-Qwen2.5-VL-3B-Instruct \
+  --gradient_checkpointing \
+  --torch_dtype bfloat16 \
+  --max_prompt_length 2048 \
+  --use_vllm \
+  --vllm_mode colocate \
+  --use_peft \
+  --lora_target_modules "q_proj", "v_proj" \
+  --log_completions
 """
 
 import re
@@ -32,7 +36,6 @@ import torch
 from datasets import load_dataset
 from latex2sympy2_extended import NormalizationConfig
 from math_verify import LatexExtractionConfig, parse, verify
-from transformers import AutoModelForImageTextToText, AutoProcessor
 
 from trl import (
     GRPOConfig,
@@ -49,9 +52,6 @@ from trl import (
 if __name__ == "__main__":
     parser = TrlParser((ScriptArguments, GRPOConfig, ModelConfig))
     script_args, training_args, model_args = parser.parse_args_and_config()
-    training_args.remove_unused_columns = False
-    training_args.max_prompt_length = None  # needs the full length to not cut off image tokens
-
     ################
     # Model & Processor
     ################
@@ -66,23 +66,12 @@ if __name__ == "__main__":
         device_map=get_kbit_device_map() if quantization_config is not None else None,
         quantization_config=quantization_config,
     )
-    processor = AutoProcessor.from_pretrained(
-        model_args.model_name_or_path,
-        use_fast=True,
-        trust_remote_code=model_args.trust_remote_code,
-        padding_side="left",
-    )
-
-    model = AutoModelForImageTextToText.from_pretrained(
-        model_args.model_name_or_path, trust_remote_code=model_args.trust_remote_code, **model_kwargs
-    )
 
     ################
     # Dataset
     ################
-    dataset = load_dataset(script_args.dataset_name, name=script_args.dataset_config, split="train[:5%]")
-
-    split_dataset = dataset.train_test_split(test_size=0.2, seed=42)
+    dataset = load_dataset("lmms-lab/multimodal-open-r1-8k-verified", split="train[:5%]")
+    dataset = dataset.train_test_split(test_size=0.2, seed=42)
 
     SYSTEM_PROMPT = (
         "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant "
@@ -92,39 +81,25 @@ if __name__ == "__main__":
     )
 
     def make_conversation(example):
-        conversation = [
-            {"role": "system", "content": SYSTEM_PROMPT},
-            {
-                "role": "user",
-                "content": [
-                    {"type": "image"},
-                    {"type": "text", "text": example["problem"]},
-                ],
-            },
+        prompt = [
+            # {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": example["problem"]},
         ]
-        prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
-        return {
-            "prompt": prompt,
-            "image": example["image"],
-        }
+        return {"prompt": prompt}
 
-    train_dataset = split_dataset[script_args.dataset_train_split]
-    eval_dataset = split_dataset[script_args.dataset_test_split] if training_args.eval_strategy != "no" else None
+    dataset = dataset.map(make_conversation)
 
-    # Convert the dataset to the required format
-    train_dataset = train_dataset.map(make_conversation)
-    train_dataset = train_dataset.remove_columns(["problem", "original_question", "original_answer"])
-    if eval_dataset:
-        eval_dataset = eval_dataset.map(make_conversation)
-        eval_dataset = eval_dataset.remove_columns(["problem", "original_question", "original_answer"])
+    train_dataset = dataset["train"]
+    eval_dataset = dataset["test"] if training_args.eval_strategy != "no" else None
 
     ################
     # Reward Function for Training
     ################
     def format_reward(completions, **kwargs):
         """Reward function that checks if the completion has a specific format."""
+        contents = [completion[0]["content"] for completion in completions]
         pattern = r"^<think>\n.*?\n</think>\n<answer>\n.*?\n</answer>$"
-        matches = [re.match(pattern, content, re.DOTALL | re.MULTILINE) for content in completions]
+        matches = [re.match(pattern, content, re.DOTALL | re.MULTILINE) for content in contents]
         rewards = [1.0 if match else 0.0 for match in matches]
         return rewards
 
@@ -134,8 +109,8 @@ if __name__ == "__main__":
         - If not parseable → compare as normalized text.
         """
         rewards = []
-
-        for completion, sol in zip(completions, solution):
+        contents = [completion[0]["content"] for completion in completions]
+        for content, sol in zip(contents, solution):
             try:
                 gold_parsed = parse(sol, extraction_mode="first_match")
             except Exception:
@@ -145,7 +120,7 @@ if __name__ == "__main__":
                 # Try parsing predicted answer too
                 try:
                     answer_parsed = parse(
-                        completion,
+                        content,
                         extraction_config=[
                             LatexExtractionConfig(
                                 normalization_config=NormalizationConfig(
@@ -163,11 +138,11 @@ if __name__ == "__main__":
                     )
                     reward = float(verify(gold_parsed, answer_parsed))
                 except Exception as e:
-                    print(f"verify failed: {e}, answer: {completion}, gold: {sol}")
+                    print(f"verify failed: {e}, answer: {content}, gold: {sol}")
                     reward = None
             else:
                 # fallback to text match
-                reward = float(completion.strip().lower() == sol.strip().lower())
+                reward = float(content.strip().lower() == sol.strip().lower())
 
             rewards.append(reward)
 
@@ -177,12 +152,11 @@ if __name__ == "__main__":
     # Training
     ################
     trainer = GRPOTrainer(
-        model=model,
+        model=model_args.model_name_or_path,
         args=training_args,
         reward_funcs=[format_reward, accuracy_reward],
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
-        processing_class=processor,
         peft_config=get_peft_config(model_args),
     )
 
@@ -192,5 +166,3 @@ if __name__ == "__main__":
     trainer.save_model(training_args.output_dir)
     if training_args.push_to_hub:
         trainer.push_to_hub(dataset_name=script_args.dataset_name)
-        if trainer.accelerator.is_main_process:
-            processor.push_to_hub(training_args.hub_model_id)
