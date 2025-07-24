70987b7c6889c2aa6fd14e21bbbb6984a2cac819
Quentin Lhoest <lhoest.q@gmail.com>
2025-07-24 18:16:57 +0200

    add uv scripts headers

--- trl/scripts/kto.py
+++ trl/scripts/kto.py
@@ -12,6 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# /// script
+# dependencies = [
+#     "trl @ git+https://github.com/huggingface/trl.git",
+# ]
+# ///
+
 """
 Run the KTO training script with the commands below. In general, the optimal configuration for KTO will be similar to
 that of DPO.
