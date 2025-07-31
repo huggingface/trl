d48f18f35489e7a2d3bf780a9dd681f50e3ec386
hjh <hujinghan.hjh@alibaba-inc.com>
2025-07-31 11:43:41 +0800

    support gspo-token

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -1837,6 +1837,11 @@ class GRPOTrainer(Trainer):
         elif self.importance_sampling_level == "sequence":
             log_importance_weights = (log_ratio * completion_mask).sum(-1) / completion_mask.sum(-1).clamp(min=1.0)
             log_importance_weights = log_importance_weights.unsqueeze(-1)
+        elif self.importance_sampling_level == 'sequence_token':
+            # GSPO-token: sg[si(θ)] * πθ(yi,t)/sg[πθ(yi,t)]
+            seq_level_log_weight = (log_ratio * completion_mask).sum(-1) / completion_mask.sum(-1).clamp(min=1.0)
+            seq_level_log_weight = seq_level_log_weight.unsqueeze(-1).detach()  # Stop gradient
+            log_importance_weights = seq_level_log_weight + per_token_logps - per_token_logps.detach()
         else:
             raise ValueError(
                 f"Unknown importance sampling level: {self.importance_sampling_level}. Possible values are 'token' "
