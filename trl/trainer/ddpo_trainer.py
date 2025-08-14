80399fe9cfa46bc9bb09427a5299eb056872947b
Yao, Matrix <matrix.yao@intel.com>
2025-08-14 17:11:42 +0000

    validate examples on xpu
    Signed-off-by: Yao, Matrix <matrix.yao@intel.com>

--- trl/trainer/ddpo_trainer.py
+++ trl/trainer/ddpo_trainer.py
@@ -159,7 +159,7 @@ class DDPOTrainer(PyTorchModelHubMixin):
 
         # Enable TF32 for faster training on Ampere GPUs,
         # cf https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices
-        if self.config.allow_tf32:
+        if self.config.allow_tf32 and torch.cuda.is_available():
             torch.backends.cuda.matmul.allow_tf32 = True
 
         self.optimizer = self._setup_optimizer(
