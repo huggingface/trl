6e33db5503f036674ace03989439097baf70f627
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-08-19 00:08:00 +0000

    conversational_prompt_completion_dataset

--- scripts/generate_zen_multi_image_dataset.py
+++ scripts/generate_zen_multi_image_dataset.py
@@ -19,7 +19,7 @@ from datasets import Dataset, Features, Image, List, Value
 from transformers import HfArgumentParser
 
 
-Message = List({'content': List({'text': Value('string'), 'type': Value('string')}), 'role': Value('string')})
+Message = List({"content": List({"text": Value("string"), "type": Value("string")}), "role": Value("string")})
 
 
 @dataclass
@@ -82,7 +82,7 @@ def main(test_size, push_to_hub, repo_id):
     if push_to_hub:
         conversational_language_modeling_dataset.push_to_hub(repo_id, config_name="conversational_language_modeling")
 
-    prompts = [
+    prompt = [
         [{"role": "user", "content": [{"type": "image"}, {"type": "text", "text": "What is better than ugly?"}]}],
         [{"role": "user", "content": [{"type": "text", "text": "What is better than implicit?"}, {"type": "image"}]}],
         [{"role": "user", "content": [{"type": "image"}, {"type": "text", "text":  "What is better than complex?"}, {"type": "image"}]}],
@@ -104,13 +104,64 @@ def main(test_size, push_to_hub, repo_id):
         [{"role": "user", "content": [{"type": "text", "text": "Any great ideas?"}]}],
     ]
     # Create the images
-    number_of_images = [sum(1 for part in row[0]["content"] if part.get("type") == "image") for row in prompts]
+    number_of_images = [sum(1 for part in row[0]["content"] if part.get("type") == "image") for row in prompt]
     sizes = [np.random.randint(32, 64, size=(num_images, 2)) for num_images in number_of_images]
     images = [[np.random.uniform(low=0.0, high=255.0, size=(h, w, 3)).astype(np.uint8) for h, w in s] for s in sizes]
-    conversational_prompt_only_dataset = Dataset.from_dict({"prompt": messages, "images": images}, features=Features(prompt=Message, images=List(Image())))
+    conversational_prompt_only_dataset = Dataset.from_dict({"prompt": prompt, "images": images}, features=Features(prompt=Message, images=List(Image())))
     conversational_prompt_only_dataset = conversational_prompt_only_dataset.train_test_split(test_size=test_size, shuffle=False)
     if push_to_hub:
         conversational_prompt_only_dataset.push_to_hub(repo_id, config_name="conversational_prompt_only")
+
+    prompt = [
+        [{"role": "user", "content": [{"type": "image"}, {"type": "text", "text": "What is better than ugly?"}]}],
+        [{"role": "user", "content": [{"type": "text", "text": "What is better than implicit?"}, {"type": "image"}]}],
+        [{"role": "user", "content": [{"type": "image"}, {"type": "text", "text":  "What is better than complex?"}, {"type": "image"}]}],
+        [{"role": "user", "content": [{"type": "image"}, {"type": "image"}, {"type": "text", "text": "What is better than complicated?"}]}],
+        [{"role": "user", "content": [{"type": "image"}, {"type": "text", "text": "What is better than nested?"}]}],
+        [{"role": "user", "content": [{"type": "text", "text": "What is better than dense?"}, {"type": "image"}]}],
+        [{"role": "user", "content": [{"type": "image"}, {"type": "text", "text": "What counts?"}, {"type": "image"}]}],
+        [{"role": "user", "content": [{"type": "text", "text": "Are special cases enough to break the rules?"}]}],
+        [{"role": "user", "content": [{"type": "text", "text": "What beats purity?"}, {"type": "image"}, {"type": "image"}]}],
+        [{"role": "user", "content": [{"type": "image"}, {"type": "image"}, {"type": "text", "text": "What should never pass silently?"}, {"type": "image"}]}],
+        [{"role": "user", "content": [{"type": "image"}, {"type": "text", "text": "When can errors pass silently?"}]}],
+        [{"role": "user", "content": [{"type": "text", "text": "What should you do in the face of ambiguity?"}, {"type": "image"}]}],
+        [{"role": "user", "content": [{"type": "image"}, {"type": "text", "text": "How many ways should there be to do it?"}, {"type": "image"}, {"type": "image"}]}],
+        [{"role": "user", "content": [{"type": "text", "text": "For whom may the way not be obvious at first?"}]}],
+        [{"role": "user", "content": [{"type": "text", "text": "What is better than never?"}, {"type": "image"}]}],
+        [{"role": "user", "content": [{"type": "text", "text": "Is"}, {"type": "image"}, {"type": "text", "text": " never better than *right* now?"}]}],
+        [{"role": "user", "content": [{"type": "image"}, {"type": "text", "text": "What does it mean if the implementation is hard to explain?"}]}],
+        [{"role": "user", "content": [{"type": "text", "text": "What does it mean if the implementation is easy to explain?"}, {"type": "image"}]}],
+        [{"role": "user", "content": [{"type": "text", "text": "Any great ideas?"}]}],
+    ]
+    completion = [
+        [{"role": "assistant", "content": [{"type": "text", "text": "Beautiful."}]},],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Explicit."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Explicit."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Complex."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Flat."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Sparse."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Readability."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "No, special cases aren't special enough to break the rules."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Practicality."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Errors."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "When explicitly silenced."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Refuse the temptation to guess."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "One, and preferably only one."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Dutch."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Now is better than never."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Yes, often."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "It means it's a bad idea."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "It means it may be a good idea."}]}],
+        [{"role": "assistant", "content": [{"type": "text", "text": "Namespaces are one honking great idea."}]}],
+    ]
+    # Create the images
+    number_of_images = [sum(1 for part in row[0]["content"] if part.get("type") == "image") for row in prompt]
+    sizes = [np.random.randint(32, 64, size=(num_images, 2)) for num_images in number_of_images]
+    images = [[np.random.uniform(low=0.0, high=255.0, size=(h, w, 3)).astype(np.uint8) for h, w in s] for s in sizes]
+    conversational_prompt_completion_dataset = Dataset.from_dict({"prompt": prompt, "completion": completion, "images": images}, features=Features(prompt=Message, completion=Message, images=List(Image())))
+    conversational_prompt_completion_dataset = conversational_prompt_completion_dataset.train_test_split(test_size=test_size, shuffle=False)
+    if push_to_hub:
+        conversational_prompt_completion_dataset.push_to_hub(repo_id, config_name="conversational_prompt_completion")
     # fmt: on
 
 
