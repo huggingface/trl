654e97d0f5f2e81ea7f4431f21fefbc3d79ab87f
edbeeching <edbeeching@gmail.com>
2025-08-28 12:43:20 +0000

    precommit

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -1431,9 +1431,9 @@ class GRPOTrainer(Trainer):
         if self.use_vllm:
             if self.vllm_mode == "colocate" and self.args.vllm_sleep_enabled:
                 # wake up colocated vLLM instances if needed
-                torch.cuda.empty_cache() # required to avoid OOM in some cases
+                torch.cuda.empty_cache()  # required to avoid OOM in some cases
                 self.llm.wake_up()
-            
+
             # First, update the vLLM weights if needed
             if self.state.global_step != self._last_loaded_step:
                 self._move_model_to_vllm()
@@ -1541,10 +1541,10 @@ class GRPOTrainer(Trainer):
                     local_rank_in_group = torch.distributed.get_rank(group=self.tp_group)
                     tp_slice = slice(local_rank_in_group * orig_size, (local_rank_in_group + 1) * orig_size)
                     completion_ids = completion_ids[tp_slice]
-                    
+
                 if self.args.vllm_sleep_enabled:
                     self.llm.sleep(level=1)
-                    
+
             # Pad the completions, and concatenate them with the prompts
             completion_ids = [torch.tensor(ids, device=device) for ids in completion_ids]
             completion_ids = pad(completion_ids, padding_value=self.pad_token_id)
