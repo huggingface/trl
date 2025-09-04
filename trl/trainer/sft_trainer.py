6292f41e476dbea9b64ae38bffbc6dfd8cd3c134
Pramodith Ballapuram <16939722+pramodith@users.noreply.github.com>
2025-09-04 11:45:03 +0000

    Add aux loss in SFT loss.

--- trl/trainer/sft_trainer.py
+++ trl/trainer/sft_trainer.py
@@ -499,7 +499,9 @@ class SFTTrainer(Trainer):
               [`~transformers.PreTrainedModel.save_pretrained`], e.g., `'./my_model_directory/'`. The model is loaded
               using [`~transformers.AutoModelForCausalLM.from_pretrained`] with the keyword arguments in
               `args.model_init_kwargs`.
-            - A [`~transformers.PreTrainedModel`] object. Only causal language models are supported.
+            - A [`~transformers.PreTrainedModel`] object. Only causal language models are supported. If you're training
+            a MoE model and want to include the load balancing/auxilliary loss as a part of the final loss, remember to set
+            the `output_router_logits` config of the model to `True`.
         args ([`SFTConfig`], *optional*, defaults to `None`):
             Configuration for this trainer. If `None`, a default configuration is used.
         data_collator (`DataCollator`, *optional*):
@@ -844,6 +846,16 @@ class SFTTrainer(Trainer):
         if hasattr(self.model, "add_model_tags"):
             self.model.add_model_tags(self._tag_names)
 
+        self.aux_loss_enabled = getattr(model.config, "output_router_logits", False)
+        self.aux_loss_coef = getattr(model.config, "router_aux_loss_coef", 0.0)
+        if self.aux_loss_enabled and self.aux_loss_coef == 0.0:
+            logger.warning(
+                "You set `output_router_logits` to `True` in the model config, but `router_aux_loss_coef` is set to "
+                "`0.0`, meaning the auxiliary loss will not be used. Either set `router_aux_loss_coef` to a value "
+                "greater than `0.0`, or set `output_router_logits` to `False` if you don't want to use the auxiliary "
+                "loss.",
+            )
+
     def _prepare_dataset(
         self,
         dataset: Union[Dataset, IterableDataset],
@@ -1042,6 +1054,11 @@ class SFTTrainer(Trainer):
             model, inputs, return_outputs=True, num_items_in_batch=num_items_in_batch
         )
 
+        # Add auxiliary loss if available
+        if self.aux_loss_enabled and self.aux_loss_coef:
+            aux_loss = self.aux_loss_coef * outputs.aux_loss
+            loss += aux_loss
+
         # Compute entropy
         if not self.args.use_liger_kernel:  # liger doesn't return logits
             with torch.no_grad():
@@ -1103,6 +1120,8 @@ class SFTTrainer(Trainer):
                 total_sum = total_tokens.sum()
                 accuracy = (correct_tokens.sum() / total_sum).item() if total_sum > 0 else 0.0
                 self._metrics[mode]["mean_token_accuracy"].append(accuracy)
+                if self.aux_loss_enabled:
+                    self._metrics[mode]["aux_loss"].append(aux_loss.item())
 
         return (loss, outputs) if return_outputs else loss
 
