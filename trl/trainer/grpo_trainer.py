d657de496c299c26c7588b1bc86a3241c42a4045
Minhao Chou <zhou_00009@126.com>
2025-09-14 09:43:21 +0800

    Update grpo_trainer.py
    minor fix

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -382,7 +382,7 @@ class GRPOTrainer(Trainer):
             )
 
         # Multi-step
-        self.num_iterations = args.num_iterations  # = ?? in the GRPO paper
+        self.num_iterations = args.num_iterations  # = 𝜇 in the GRPO paper
         self.epsilon_low = args.epsilon
         self.epsilon_high = args.epsilon_high if args.epsilon_high is not None else args.epsilon
         # Tracks the number of iterations (forward + backward passes), including those within a grad accum cycle
