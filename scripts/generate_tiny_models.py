fd4b0a04b920bf83869e88debd84e85f322ad98d
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-09-16 15:56:25 +0000

    tiny GPTNoeX

--- scripts/generate_tiny_models.py
+++ scripts/generate_tiny_models.py
@@ -236,6 +236,7 @@ push_to_hub(model, tokenizer, "small")
 
 # Reward models
 for model_id, model_class, suffix in [
+    ("EleutherAI/pythia-14m", GPTNeoXConfig, GPTNeoXForCausalLM, None),
     ("meta-llama/Llama-3.2-1B-Instruct", LlamaForSequenceClassification, "3.2"),
     ("Qwen/Qwen2.5-32B-Instruct", Qwen2ForSequenceClassification, "2.5"),
     ("Qwen/Qwen3-4B", Qwen3ForSequenceClassification, None),
