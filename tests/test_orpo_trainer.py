a5ca7d4ba7768e6275fed853f7aa91f26bc6620f
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-10-06 20:18:41 +0000

    style

--- tests/test_orpo_trainer.py
+++ tests/test_orpo_trainer.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import torch
 from datasets import load_dataset
 from parameterized import parameterized
--- tests/test_xpo_trainer.py
+++ tests/test_xpo_trainer.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from datasets import load_dataset
 from parameterized import parameterized
 from transformers import AutoModelForCausalLM, AutoModelForSequenceClassification, AutoTokenizer
