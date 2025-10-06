45ee98b05e979c817ee06c81122a82c42a352f87
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2025-10-06 11:14:54 +0200

    Replace unittest with pytest (#4188)

--- tests/test_core.py
+++ tests/test_core.py
@@ -20,22 +20,21 @@ from trl.core import masked_mean, masked_var, masked_whiten
 from .testing_utils import TrlTestCase
 
 
-class CoreTester(TrlTestCase):
+class TestCore(TrlTestCase):
     """
     A wrapper class for testing core utils functions
     """
 
-    def setUp(self):
-        super().setUp()
+    def setup_method(self):
         self.test_input = torch.Tensor([1, 2, 3, 4])
         self.test_mask = torch.Tensor([0, 1, 1, 0])
         self.test_input_unmasked = self.test_input[1:3]
 
     def test_masked_mean(self):
-        self.assertEqual(torch.mean(self.test_input_unmasked), masked_mean(self.test_input, self.test_mask))
+        assert torch.mean(self.test_input_unmasked) == masked_mean(self.test_input, self.test_mask)
 
     def test_masked_var(self):
-        self.assertEqual(torch.var(self.test_input_unmasked), masked_var(self.test_input, self.test_mask))
+        assert torch.var(self.test_input_unmasked) == masked_var(self.test_input, self.test_mask)
 
     def test_masked_whiten(self):
         def whiten(values: torch.Tensor) -> torch.Tensor:
@@ -45,4 +44,4 @@ class CoreTester(TrlTestCase):
         whiten_unmasked = whiten(self.test_input_unmasked)
         whiten_masked = masked_whiten(self.test_input, self.test_mask)[1:3]
         diffs = (whiten_unmasked - whiten_masked).sum()
-        self.assertLess(abs(diffs.item()), 0.00001)
+        assert abs(diffs.item()) < 0.00001
