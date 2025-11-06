baa0b0698c56cdac30a6204bd5738bfa1bafab40
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-11-06 23:01:59 +0000

    consistency: relative imports

--- trl/experimental/grpo_with_replay_buffer/grpo_with_replay_buffer_trainer.py
+++ trl/experimental/grpo_with_replay_buffer/grpo_with_replay_buffer_trainer.py
@@ -18,14 +18,9 @@ from typing import Any
 import torch
 from accelerate.utils import gather_object
 
-from trl.data_utils import (
-    apply_chat_template,
-    is_conversational,
-    prepare_multimodal_messages,
-)
-from trl.trainer.grpo_trainer import GRPOTrainer
-from trl.trainer.utils import nanmax, nanmin, nanstd, pad
-
+from ...data_utils import apply_chat_template, is_conversational, prepare_multimodal_messages
+from ...trainer.grpo_trainer import GRPOTrainer
+from ...trainer.utils import nanmax, nanmin, nanstd, pad
 from .grpo_with_replay_buffer_config import GRPOWithReplayBufferConfig
 
 
