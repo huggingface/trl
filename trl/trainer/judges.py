d6c1c1045a18c04a8ea06076ad1347cf2aab3792
Behrooz <ermiaazarkhalili@gmail.com>
2025-11-05 20:25:37 -0800

    Add deprecation stub for judges module
    Judges were moved to trl.experimental.judges but the deprecation stub
    at trl/trainer/judges.py was missing, causing import errors.
    
    This adds the stub file that re-exports all judge classes from
    experimental with a FutureWarning.

---
+++ trl/trainer/judges.py
@@ -0,0 +1,47 @@
+# Copyright 2020-2025 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import warnings
+
+from ..experimental.judges import (
+    AllTrueJudge,
+    BaseBinaryJudge,
+    BaseJudge,
+    BasePairwiseJudge,
+    BaseRankJudge,
+    HfPairwiseJudge,
+    OpenAIPairwiseJudge,
+    PairRMJudge,
+)
+
+
+# Deprecation warning for all judge classes
+warnings.warn(
+    "Importing judges from 'trl.trainer' is deprecated and will be removed in v1.2.0. "
+    "Please use 'from trl.experimental.judges import ...' instead.",
+    FutureWarning,
+    stacklevel=2,
+)
+
+
+__all__ = [
+    "AllTrueJudge",
+    "BaseBinaryJudge",
+    "BaseJudge",
+    "BasePairwiseJudge",
+    "BaseRankJudge",
+    "HfPairwiseJudge",
+    "OpenAIPairwiseJudge",
+    "PairRMJudge",
+]
