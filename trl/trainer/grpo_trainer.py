21792daad569d4eaf4ccf32a77eb0d8b0d04e310
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-11-20 01:52:32 +0000

    style

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -64,7 +64,7 @@ from ..data_utils import (
 )
 from ..extras.profiling import profiling_context, profiling_decorator
 from ..extras.vllm_client import VLLMClient
-from ..import_utils import is_liger_kernel_available, is_vllm_available, is_jmespath_available
+from ..import_utils import is_jmespath_available, is_liger_kernel_available, is_vllm_available
 from ..models import prepare_deepspeed, prepare_fsdp, prepare_peft_model, unwrap_model_for_generation
 from ..models.utils import _ForwardRedirection
 from .base_trainer import BaseTrainer
