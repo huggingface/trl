fce5dfdc06c841d6ddddc62ff97a5690188c11cd
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2025-12-05 07:34:04 +0100

    Raise warnings at 2nd stack level (#4621)

--- trl/trainer/rloo_trainer.py
+++ trl/trainer/rloo_trainer.py
@@ -247,7 +247,8 @@ class RLOOTrainer(BaseTrainer):
                 "This trainer will soon be moved to trl.experimental and is a candidate for removal. If you rely on "
                 "it and want it to remain, please share your comments here: "
                 "https://github.com/huggingface/trl/issues/4223. Silence this warning by setting environment variable "
-                "TRL_EXPERIMENTAL_SILENCE=1."
+                "TRL_EXPERIMENTAL_SILENCE=1.",
+                stacklevel=2,
             )
 
         # Args
