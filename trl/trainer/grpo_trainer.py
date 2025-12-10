af60de9ea520c3a8edf914276141ed03b633a433
pramodith <pramodith1@gmail.com>
2025-12-10 17:05:17 +0000

    Merge

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -12,11 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import asyncio
-import inspect
-import multiprocessing
 import copy
 import inspect
 import json
+import multiprocessing
 import os
 import textwrap
 import time
