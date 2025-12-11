7339a927ce00991b16a2222153f1ef7505c7cd3e
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-12-11 05:17:52 +0000

    Move `GeometricMixtureWrapper` to `nash_md_trainer`

--- trl/models/modeling_base.py
+++ trl/models/modeling_base.py
@@ -14,9 +14,6 @@
 
 import warnings
 
-import torch
-from transformers import GenerationMixin
-
 from ..import_utils import suppress_experimental_warning
 
 
@@ -43,61 +40,3 @@ class PreTrainedModelWrapper(_PreTrainedModelWrapper):
             stacklevel=2,
         )
         super().__init__(*args, **kwargs)
-
-
-class GeometricMixtureWrapper(GenerationMixin):
-    """
-    Geometric Mixture generation wrapper that samples from the logits of two model's geometric mixture.
-
-    Args:
-        model ([`~transformers.PreTrainedModel`]): The model to be wrapped.
-        ref_model ([`~transformers.PreTrainedModel`]): The reference model.
-        generation_config ([`~transformers.GenerationConfig`]): The generation config.
-        mixture_coef (`float`, *optional* - default: 0.5): The mixture coefficient.
-    """
-
-    main_input_name = "input_ids"
-    _supports_cache_class = False
-    _supports_static_cache = False
-    _is_stateful = False
-
-    def __init__(self, model, ref_model, generation_config, mixture_coef=0.5, device=None):
-        super().__init__()
-
-        self.model = model
-        self.config = model.config
-        self.ref_model = ref_model
-        self.generation_config = generation_config
-        self.mixture_coef = mixture_coef
-        self.device = device
-        if hasattr(self.model, "_is_stateful"):
-            self._is_stateful = self.model._is_stateful
-
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
-
-    @torch.inference_mode()
-    def forward(self, *args, **kwargs):
-        model_outputs = self.model(*args, **kwargs)
-        model_logits = model_outputs.logits
-        ref_model_logits = self.ref_model(*args, **kwargs).logits
-
-        model_outputs.logits = torch.nn.functional.log_softmax(
-            self.mixture_coef * ref_model_logits + (1 - self.mixture_coef) * model_logits, dim=-1
-        )
-
-        return model_outputs
-
-    def prepare_inputs_for_generation(self, *args, **kwargs):
-        # turn off cache in the generation config
-        kwargs["use_cache"] = False
-        model_inputs = self.model.prepare_inputs_for_generation(*args, **kwargs)
-        _ = self.ref_model.prepare_inputs_for_generation(*args, **kwargs)
-
-        return model_inputs
-
-    def _validate_model_class(self):
-        self.model._validate_model_class()
-
-    def _validate_model_kwargs(self, model_kwargs):
-        return self.model._validate_model_kwargs(model_kwargs)
