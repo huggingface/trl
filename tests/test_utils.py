65b57167b42b22d6f554eb93760e32ad4d376207
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-12-22 04:10:22 +0000

    llavanext only

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -830,8 +830,8 @@ class TestForwardMaskedLogits:
             "trl-internal-testing/tiny-Gemma3ForConditionalGeneration",
             "trl-internal-testing/tiny-Idefics2ForConditionalGeneration",
             # "trl-internal-testing/tiny-Idefics3ForConditionalGeneration",
-            "trl-internal-testing/tiny-LlavaForConditionalGeneration",
-            # "trl-internal-testing/tiny-LlavaNextForConditionalGeneration",
+            # "trl-internal-testing/tiny-LlavaForConditionalGeneration",
+            "trl-internal-testing/tiny-LlavaNextForConditionalGeneration",
             # "trl-internal-testing/tiny-Qwen2VLForConditionalGeneration",
             # "trl-internal-testing/tiny-Qwen2_5_VLForConditionalGeneration",
             # # "trl-internal-testing/tiny-SmolVLMForConditionalGeneration", seems not to support bf16 properly
