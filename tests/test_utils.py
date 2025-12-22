be330f6874dacb85f854ea3bd4539b3aada18ba6
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-12-22 04:55:29 +0000

    no idefics

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -828,12 +828,12 @@ class TestForwardMaskedLogits:
         "model_id",
         [
             "trl-internal-testing/tiny-Gemma3ForConditionalGeneration",
-            "trl-internal-testing/tiny-Idefics2ForConditionalGeneration",
+            # "trl-internal-testing/tiny-Idefics2ForConditionalGeneration",
             # "trl-internal-testing/tiny-Idefics3ForConditionalGeneration",
-            # "trl-internal-testing/tiny-LlavaForConditionalGeneration",
-            # "trl-internal-testing/tiny-LlavaNextForConditionalGeneration",
-            # "trl-internal-testing/tiny-Qwen2VLForConditionalGeneration",
-            # "trl-internal-testing/tiny-Qwen2_5_VLForConditionalGeneration",
+            "trl-internal-testing/tiny-LlavaForConditionalGeneration",
+            "trl-internal-testing/tiny-LlavaNextForConditionalGeneration",
+            "trl-internal-testing/tiny-Qwen2VLForConditionalGeneration",
+            "trl-internal-testing/tiny-Qwen2_5_VLForConditionalGeneration",
             # # "trl-internal-testing/tiny-SmolVLMForConditionalGeneration", seems not to support bf16 properly
             "trl-internal-testing/tiny-Qwen3VLForConditionalGeneration",
         ],
