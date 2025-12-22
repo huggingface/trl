c180ae4ac731c93e83c86e811a81055fd69f6d34
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-12-22 04:09:55 +0000

    llava only

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -830,7 +830,7 @@ class TestForwardMaskedLogits:
             "trl-internal-testing/tiny-Gemma3ForConditionalGeneration",
             "trl-internal-testing/tiny-Idefics2ForConditionalGeneration",
             # "trl-internal-testing/tiny-Idefics3ForConditionalGeneration",
-            # "trl-internal-testing/tiny-LlavaForConditionalGeneration",
+            "trl-internal-testing/tiny-LlavaForConditionalGeneration",
             # "trl-internal-testing/tiny-LlavaNextForConditionalGeneration",
             # "trl-internal-testing/tiny-Qwen2VLForConditionalGeneration",
             # "trl-internal-testing/tiny-Qwen2_5_VLForConditionalGeneration",
