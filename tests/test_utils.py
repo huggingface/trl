7fe01aa2cad14634504c1e0b5892536bbb316420
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-12-22 02:14:39 +0000

    idefics3

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -829,7 +829,7 @@ class TestForwardMaskedLogits:
         [
             "trl-internal-testing/tiny-Gemma3ForConditionalGeneration",
             "trl-internal-testing/tiny-Idefics2ForConditionalGeneration",
-            # "trl-internal-testing/tiny-Idefics3ForConditionalGeneration",
+            "trl-internal-testing/tiny-Idefics3ForConditionalGeneration",
             # "trl-internal-testing/tiny-LlavaForConditionalGeneration",
             # "trl-internal-testing/tiny-LlavaNextForConditionalGeneration",
             # "trl-internal-testing/tiny-Qwen2VLForConditionalGeneration",
