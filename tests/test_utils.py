1bbec04969625c75a00d6a03bb248c7795ba941e
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2025-12-22 03:31:33 +0000

    qwen2_5

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -833,7 +833,7 @@ class TestForwardMaskedLogits:
             "trl-internal-testing/tiny-LlavaForConditionalGeneration",
             # "trl-internal-testing/tiny-LlavaNextForConditionalGeneration",
             # "trl-internal-testing/tiny-Qwen2VLForConditionalGeneration",
-            # "trl-internal-testing/tiny-Qwen2_5_VLForConditionalGeneration",
+            "trl-internal-testing/tiny-Qwen2_5_VLForConditionalGeneration",
             # # "trl-internal-testing/tiny-SmolVLMForConditionalGeneration", seems not to support bf16 properly
             "trl-internal-testing/tiny-Qwen3VLForConditionalGeneration",
         ],
