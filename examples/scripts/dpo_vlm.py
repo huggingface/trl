348d4330b3f4bfef4762704ceea4e9bc6e0a2e59
Quentin Gallouédec <gallouedec.quentin@gmail.com>
2026-01-07 14:39:30 +0000

    Remove fix for "marked as ready twice"

--- examples/scripts/dpo_vlm.py
+++ examples/scripts/dpo_vlm.py
@@ -84,10 +84,6 @@ if __name__ == "__main__":
     parser = TrlParser((ScriptArguments, DPOConfig, ModelConfig))
     script_args, training_args, model_args = parser.parse_args_and_config()
 
-    # Fix for "marked as ready twice" error with gradient checkpointing + LoRA
-    if training_args.gradient_checkpointing:
-        training_args.gradient_checkpointing_kwargs = {"use_reentrant": False}
-
     ################
     # Model & Processor
     ################
