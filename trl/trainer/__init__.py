618d2ca6a905746c63b5575a78df3a42f8cbcf5e
Srijan Upadhyay <srjnupadhyay@gmail.com>
2026-01-16 16:51:19 +0530

    fix: remove stray dict entry and indentation error in __init__.py

--- trl/trainer/__init__.py
+++ trl/trainer/__init__.py
@@ -1,4 +1,4 @@
-    "sft_trainer": ["SFTTrainer", "GenerativeSFTTrainer"],
+# ...existing code...
 # Copyright 2020-2026 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
