c3ead5b556d9ea588b4a95cae1775913118ddbc6
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2026-02-20 11:09:02 +0100

    Fix NameError: name 'importlib' is not defined (#5134)

--- trl/import_utils.py
+++ trl/import_utils.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import importlib
+import importlib.metadata
+import importlib.util
 import warnings
 from contextlib import contextmanager
 
