2c4285f9f8151637143617f44dc47d1706f445a8
casinca <47400729+casinca@users.noreply.github.com>
2026-03-05 15:24:57 +0100

    bumped deprecation to 0.30

--- trl/trainer/grpo_config.py
+++ trl/trainer/grpo_config.py
@@ -910,7 +910,7 @@ class GRPOConfig(_BaseConfig):
 
         if self.vllm_importance_sampling_cap is not None:
             warnings.warn(
-                "The `vllm_importance_sampling_cap` argument is deprecated and will be removed in version 0.29.0. You "
+                "The `vllm_importance_sampling_cap` argument is deprecated and will be removed in version 0.30.0. You "
                 "should instead use `vllm_importance_sampling_max`.",
                 FutureWarning,
                 stacklevel=2,
