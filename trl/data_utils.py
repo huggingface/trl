39ba6919250d25cca938881c6dac42fede1bf25e
mariosasko <mariosasko777@gmail.com>
2026-03-13 21:17:53 +0100

    Don't mutate in-place

--- trl/data_utils.py
+++ trl/data_utils.py
@@ -895,7 +895,7 @@ def pack_dataset(
     else:  # PackingStrategy.WRAPPED
         dataset = dataset.map(_pack_wrapped, batched=True, fn_kwargs={"seq_length": seq_length}, **map_kwargs)
     if strategy in {PackingStrategy.BFD, PackingStrategy.BFD_SPLIT} and "columns" in format:
-        format["columns"].append("seq_lengths")
+        format["columns"] = format["columns"] + ["seq_lengths"]
     dataset = dataset.with_format(**format)
     return dataset
 
