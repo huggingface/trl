88856bdfc09ad2d0a0fe0c9e9cfe6421f370183c
Liu, Kaixuan <kaixuan.liu@intel.com>
2026-05-15 05:14:54 +0000

    skip vision parts of the model for test_train_vlm_multi_image as well
    Signed-off-by: Liu, Kaixuan <kaixuan.liu@intel.com>

--- tests/test_grpo_trainer.py
+++ tests/test_grpo_trainer.py
@@ -2315,7 +2315,18 @@ class TestGRPOTrainer(TrlTestCase):
 
         assert trainer.state.log_history[-1]["train_loss"] is not None
 
+        # Check that the params have changed
+        # Because of the way the tiny models are initialized, the gradient does not flow properly through the
+        # vision parts of the model, so we skip them. Ideally, we should fix the init of these models.
+        params_to_skip = (
+            "model.vision_tower.",
+            "model.multi_modal_projector.",
+            "model.visual.",
+            "model.image_newline",
+        )
         for n, param in previous_trainable_params.items():
+            if n.startswith(params_to_skip):
+                continue
             new_param = trainer.model.get_parameter(n)
             assert not torch.equal(param, new_param), f"Parameter {n} has not changed."
 
