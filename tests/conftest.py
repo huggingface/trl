ec5ac9b192b98aac62f2ab540c2bb9f73fc69f50
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2026-05-20 10:54:16 +0200

    Revert "Run CI tests with tiny model PR"
    This reverts commit 329c7793721bfebe4da84e1ae83d522c319e0d2d.

--- tests/conftest.py
+++ tests/conftest.py
@@ -69,7 +69,6 @@ def pytest_runtest_makereport(item, call):
 
 MODEL_REVISIONS = {
     # Add model_id: revision mappings here to test PRs
-    "trl-internal-testing/tiny-Qwen3VLForConditionalGeneration": "refs/pr/6",
 }
 
 
