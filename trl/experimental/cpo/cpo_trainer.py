e69b7a1c5e163e91fd9535fd25504bc3df58cae5
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2026-06-22 13:53:24 -0400

    Harmonize logger imports (#6142)

--- trl/experimental/cpo/cpo_trainer.py
+++ trl/experimental/cpo/cpo_trainer.py
@@ -27,7 +27,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import transformers
-from accelerate import PartialState, logging
+from accelerate import PartialState
+from accelerate.logging import get_logger
 from accelerate.utils import is_peft_model
 from datasets import Dataset
 from packaging.version import Version
@@ -69,7 +70,7 @@ if is_wandb_available():
     import wandb
 
 
-logger = logging.get_logger(__name__)
+logger = get_logger(__name__)
 
 
 class CPOTrainer(_BaseTrainer):
