c457d97f71215091426867fa86aa7c1e5a2027fc
FAN YUCHEN <2994114386@qq.com>
2026-07-10 07:46:30 +0800

    Fix FileNotFoundError in DPO/KTO precompute_ref_log_probs (#6348)
    Signed-off-by: Yuchen Fan <functionhx@gmail.com>

--- trl/trainer/dpo_trainer.py
+++ trl/trainer/dpo_trainer.py
@@ -1120,6 +1120,7 @@ class DPOTrainer(_BaseTrainer):
                 batched=True,
                 remove_columns=dataset.column_names,
                 new_fingerprint=fingerprint,
+                cache_file_name=cache_file,
                 desc=f"Caching reference log probs for {name} dataset",
             )
         self.accelerator.wait_for_everyone()
