bc6e8007bd685a61e2d9714717ecd6d754cd6b38
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2026-07-13 20:24:03 -0400

    Fix crash when reward function is a `functools.partial` or callable instance (#6313)

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -81,6 +81,7 @@ from .utils import (
     create_model_from_path,
     disable_dropout_in_model,
     entropy_from_logits,
+    get_callable_name,
     get_config_model_id,
     identity,
     nanmax,
@@ -492,7 +493,7 @@ class GRPOTrainer(_BaseTrainer):
             if isinstance(reward_funcs[i], nn.Module):  # Use Module over PretrainedModel for compat w/ compiled models
                 self.reward_func_names.append(get_config_model_id(reward_funcs[i].config).split("/")[-1])
             else:
-                self.reward_func_names.append(reward_funcs[i].__name__)
+                self.reward_func_names.append(get_callable_name(reward_funcs[i]))
         self.reward_funcs = reward_funcs
 
         # Reward weights
