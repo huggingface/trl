5f266986f90b5db30c2b1ee0096e14d972b160d0
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2026-08-13 19:21:06 +0000

    Add Muse Glimmer support and testing

--- tests/test_kto_trainer.py
+++ tests/test_kto_trainer.py
@@ -1410,6 +1410,13 @@ class TestKTOTrainerVLM(TrlTestCase):
             ),
             "trl-internal-testing/tiny-LlavaForConditionalGeneration",
             "trl-internal-testing/tiny-LlavaNextForConditionalGeneration",
+            pytest.param(
+                "trl-internal-testing/tiny-MuseGlimmerForConditionalGeneration",
+                marks=pytest.mark.skipif(
+                    Version(transformers.__version__) < Version("5.15.0"),
+                    reason="Muse Glimmer was introduced in transformers-5.15.0",
+                ),
+            ),
             "trl-internal-testing/tiny-Qwen2VLForConditionalGeneration",
             "trl-internal-testing/tiny-Qwen2_5_VLForConditionalGeneration",
             # "trl-internal-testing/tiny-SmolVLMForConditionalGeneration", seems not to support bf16 properly
