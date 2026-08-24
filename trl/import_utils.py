d6b969de28016e3c10e0c8ecb0bd6ff9186a648a
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2026-08-24 09:35:03 +0200

    Revert

--- trl/import_utils.py
+++ trl/import_utils.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import warnings
 import importlib
 import importlib.metadata
 import importlib.util
+import warnings
 from contextlib import contextmanager
 
 from packaging.version import Version
