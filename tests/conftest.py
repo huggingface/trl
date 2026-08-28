1308ae7d62126943793041e912a969edad34637c
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2026-08-28 16:13:24 +0200

    Pin tiny model revision

--- tests/conftest.py
+++ tests/conftest.py
@@ -88,6 +88,7 @@ def pytest_runtest_makereport(item, call):
 
 MODEL_REVISIONS = {
     # Add model_id: revision mappings here to test PRs
+    "trl-internal-testing/tiny-Cohere2ForCausalLM": "refs/pr/3",
 }
 
 
