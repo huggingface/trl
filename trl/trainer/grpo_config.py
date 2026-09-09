c33adaef02b55aec3f8365ebc3930d9275b2cec1
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2026-09-09 22:38:41 +0000

    Remove the stray quote at the end of the log_completions_hub_repo docstring
    Leftover from the field's help string; it renders in the docs.

--- trl/trainer/grpo_config.py
+++ trl/trainer/grpo_config.py
@@ -381,7 +381,7 @@ class GRPOConfig(_BaseConfig):
             Hugging Face Hub repository to save the completions. Should be a complete repository name like
             `'username/reponame'` or `'orgname/reponame'`, or just `'reponame'` in which case the repository will be
             created in the currently-logged-in Hugging Face user's namespace. Note that this repository will be public
-            unless you set `hub_private_repo=True` or your organization's default is to create private repositories."
+            unless you set `hub_private_repo=True` or your organization's default is to create private repositories.
 
         > Deprecated parameters
 
