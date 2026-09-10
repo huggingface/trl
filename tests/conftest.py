c7d2362af9217fffc167ce5cb47d7f073bc4d996
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2026-09-10 11:58:41 +0200

    Update CI MODEL_REVISIONS

--- tests/conftest.py
+++ tests/conftest.py
@@ -88,6 +88,7 @@ def pytest_runtest_makereport(item, call):
 
 MODEL_REVISIONS = {
     # Add model_id: revision mappings here to test PRs
+    "trl-internal-testing/tiny-Qwen2ForCausalLM-R1-Distill": "refs/pr/1",
 }
 
 
