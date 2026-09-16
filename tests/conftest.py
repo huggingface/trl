a253a0fcda31dc7f649cf1375644e59195dd499d
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2026-09-16 10:04:36 +0000

    Test tiny Gemma2 model PR

--- tests/conftest.py
+++ tests/conftest.py
@@ -87,7 +87,7 @@ def pytest_runtest_makereport(item, call):
 # ============================================================================
 
 MODEL_REVISIONS = {
-    # Add model_id: revision mappings here to test PRs
+    "trl-internal-testing/tiny-Gemma2ForCausalLM": "refs/pr/1",
 }
 
 
