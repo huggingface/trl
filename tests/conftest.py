59206d3a00022a6fed82767a980f65f6ed87e26e
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2026-09-17 10:46:54 +0000

    Test tiny Phi-3 model PR

--- tests/conftest.py
+++ tests/conftest.py
@@ -87,7 +87,7 @@ def pytest_runtest_makereport(item, call):
 # ============================================================================
 
 MODEL_REVISIONS = {
-    # Add model_id: revision mappings here to test PRs
+    "trl-internal-testing/tiny-Phi3ForCausalLM-3": "refs/pr/1",
 }
 
 
