fe31f4a48168e6518b83ac82bca617002c29b5e6
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2026-09-25 12:25:47 +0000

    Test tiny LFM2 model PR

--- tests/conftest.py
+++ tests/conftest.py
@@ -88,7 +88,7 @@ def pytest_runtest_makereport(item, call):
 # ============================================================================
 
 MODEL_REVISIONS = {
-    # Add model_id: revision mappings here to test PRs
+    "trl-internal-testing/tiny-Lfm2ForCausalLM": "refs/pr/1",
 }
 
 
